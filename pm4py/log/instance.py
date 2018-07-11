a4e2d2434f2fa9250b587374e8e7298d2993fe25
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2018-07-11 09:49:46 +0200

    temporal commit before integrating pandas

--- pm4py/log/instance.py
+++ pm4py/log/instance.py
@@ -1,4 +1,3 @@
-import functools
 from collections.abc import Mapping, Sequence
 
 
@@ -19,17 +18,11 @@ class Event(Mapping):
     def __str__(self):
         return str(self._dict)
 
-    def project_on_attributes(self, keys):
-        return Event({k: self[k] for k in keys})
-
-    @staticmethod
-    def project_on_attributes_static(event, keys):
-        return Event({k: event[k] for k in keys})
-
 
 class EventLog(Sequence):
 
-    def __init__(self, *args):
+    def __init__(self, attributes, *args):
+        self._attributes = attributes
         self._list = list(*args)
 
     def __getitem__(self, key):
@@ -59,17 +52,58 @@ class EventLog(Sequence):
     def append(self, x):
         self._list.append(x)
 
-    def project_events_on_attributes(self, keys):
-        return EventLog(map(functools.partial(Event.project_on_attributes_static, keys=keys), self.events))
+    def _set_attributes(self, attributes):
+        self._attributes = attributes
 
-    def trace_view(self, key):
-        trace_view = {}
-        for event in self.events:
-            if key in event.attributes:
-                if event.attributes[key] in trace_view:
-                    trace_view[event.attributes[key]].append(event)
-                else:
-                    trace_view[event.attributes[key]] = [event]
-            else:
-                raise KeyError('key %s not present in event %s' % (key, event))
-        return trace_view
+    def _get_attributes(self):
+        return self._attributes
+
+    attributes = property(_get_attributes, _set_attributes)
+
+
+class Trace(Sequence):
+
+    def __init__(self, attributes, *args):
+        self._set_attributes(attributes)
+        self._list = list(*args)
+
+    def __getitem__(self, key):
+        return self._list[key]
+
+    def __iter__(self):
+        return iter(self._list)
+
+    def __len__(self):
+        return len(self._list)
+
+    def __contains__(self, item):
+        return item in self._list
+
+    def __reversed__(self):
+        return reversed(self._list)
+
+    def index(self, x, start: int = ..., end: int = ...):
+        return self._list.index(x, start, end)
+
+    def count(self, x):
+        return self._list.count(x)
+
+    def __str__(self):
+        return str(self._list)
+
+    def append(self, x):
+        self._list.append(x)
+
+    def _set_attributes(self, attributes):
+        self._attributes = attributes
+
+    def _get_attributes(self):
+        return self._attributes
+
+    attributes = property(_get_attributes, _set_attributes)
+    
+
+class TraceLog(EventLog):
+    def __init__(self, attributes, *args):
+        super(TraceLog, self).__init__(attributes, *args)
+
--- test.py
+++ test.py
@@ -2,26 +2,34 @@ from pm4py.log import instance as log_instance
 from pm4py.log.importer import csv as csv_importer
 import time
 
-e1 = log_instance.Event({'concept:name': 'b', 'trace:concept:name': 't1'})
+e1 = log_instance.Event({'concept:name': 'a', 'trace:concept:name': 't1'})
 e2 = log_instance.Event({'concept:name': 'b', 'trace:concept:name': 't1'})
-e3 = log_instance.Event({'concept_name': 'b', 'trace:concept:name': 't1'})
+e3 = log_instance.Event({'concept:name': 'c', 'trace:concept:name': 't1'})
+t1 = log_instance.Trace({'concept:name': 't1'}, [e1, e2, e3])
+
+
+t2 = log_instance.Trace(t1.attributes, filter(lambda event: event['concept:name'] in {'a', 'b'}, t1))
+t3 = log_instance.Trace(t1.attributes, filter(lambda event: event['concept:name'] in {'c'}, t1))
+log = log_instance.TraceLog({'concept:name': ''})
 
 for key, value in e1.items():
     print(key, value)
 
 log = log_instance.EventLog([e1, e2, e3])
-log.it_is_me_mario()
 print(log)
 log = log_instance.EventLog(filter(lambda e: 'concept:name' in e, log))
-log.it_is_me_mario()
 print(log)
 
 for event in log:
     print(event)
 
 start = time.time()
-log = csv_importer.import_from_file('C:/Users/bas/Documents/tue/svn/private/logs/road_traffic.csv')
+importer = csv_importer.CSVImporter()
+log = importer.import_from_file('C:/Users/bas/Documents/tue/svn/private/logs/road_traffic.csv', ',', False)
 print(time.time() - start)
-
-print(log)
+print(len(log))
+start = time.time()
+log2 = log_instance.EventLog(log.attributes, filter(lambda e: float(e['amount']) > 50.0, log))
+print(time.time() - start)
+print(len(log2))
 
