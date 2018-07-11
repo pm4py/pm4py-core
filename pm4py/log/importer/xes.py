57b87dfc4e5fbf4a516c7d43a1248ca4f25672cf
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2018-07-11 17:30:33 +0200

    add (incomplete) .xes support

--- pm4py/log/importer/xes.py
+++ pm4py/log/importer/xes.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as xml_parser
 import re
 import pm4py.log.instance as log_instance
-from dateutil import parser as date_parser
+import pandas as pd
 
 
 def import_from_path_xes(path):
@@ -18,17 +18,25 @@ def import_from_path_xes(path):
         trace_attr = __parse_attribute(xes_trace, ns+'string', str, trace_attr)
         trace_attr = __parse_attribute(xes_trace, ns+'float', float, trace_attr)
         trace_attr = __parse_attribute(xes_trace, ns+'boolean', bool, trace_attr)
-        trace_attr = __parse_attribute(xes_trace, ns+'date', date_parser.parse, trace_attr)
-        trace = log_instance.Trace(trace_attr,[])
+        trace_attr = __parse_date_attribute(xes_trace, ns+'date', trace_attr)
+        trace = log_instance.Trace(trace_attr, [])
         for xes_event in xes_trace.findall(ns+'event'):
             event = log_instance.Event()
             event = __parse_attribute(xes_event, ns+'int', int, event)
             event = __parse_attribute(xes_event, ns+'string', str, event)
             event = __parse_attribute(xes_event, ns+'float', float, event)
             event = __parse_attribute(xes_event, ns+'boolean', bool, event)
-            event = __parse_attribute(xes_event, ns+'date', date_parser.parse, event)
+            event = __parse_date_attribute(xes_event, ns+'date', event)
             trace.append(event)
         log.append(trace)
+
+    return log
+
+
+def __parse_date_attribute(elem, ns_date, res_attr):
+    for a in elem.findall(ns_date):
+        res_attr[a.attrib['key']] = pd.Timestamp(a.attrib['value']).to_pydatetime()
+    return res_attr
 
 
 def __parse_attribute(elem, child, type, res_attr):
