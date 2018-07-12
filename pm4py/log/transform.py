8b61c0d0f8b31d4e15880703e47e0baaf66daf13
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2018-07-12 17:45:18 +0200

    add iterative parser that can handle big(ger) logs without running out of memory. current performance: BPI 2017 ~ 1 minute to parse into internal data structure.

--- pm4py/log/transform.py
+++ pm4py/log/transform.py
@@ -1,9 +1,9 @@
 from pm4py.log import instance as log_instance
 
 
-def transform_event_log_to_trace_log(event_log, case_glue='case:concept:name', includes_case_attributes=True, case_attribute_prefix='case:'):
+def transform_event_log_to_trace_log(log, case_glue='case:concept:name', includes_case_attributes=True, case_attribute_prefix='case:'):
     traces = {}
-    for event in event_log:
+    for event in log:
         glue = event[case_glue]
         if glue not in traces:
             if includes_case_attributes:
@@ -11,7 +11,7 @@ def transform_event_log_to_trace_log(event_log, case_glue='case:concept:name', i
                 for k in event.keys():
                     if k.startswith(case_attribute_prefix):
                         trace_attr[k.replace(case_attribute_prefix, '')] = event[k]
-            traces[glue] = log_instance.Trace(trace_attr)
+            traces[glue] = log_instance.Trace(attributes=trace_attr)
 
         if includes_case_attributes:
             for k in list(event.keys()):
@@ -19,7 +19,7 @@ def transform_event_log_to_trace_log(event_log, case_glue='case:concept:name', i
                     del event[k]
 
         traces[glue].append(event)
-    return log_instance.TraceLog(event_log.attributes, traces.values())
+    return log_instance.TraceLog(traces.values(), attributes=log.attributes, classifiers=log.classifiers, omni_present=log.omni_present, extensions=log.extensions)
 
 
 def transform_trace_log_to_event_log(trace_log, include_case_attributes=True, case_attribute_prefix='case:'):
