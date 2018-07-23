5f2276422c2ccf1e04256e83aa71400c13a90907
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2018-07-23 16:05:13 +0200

    add synchronous product net

--- pm4py/models/petri/utils.py
+++ pm4py/models/petri/utils.py
@@ -1,7 +1,7 @@
 from pm4py.models.petri import instance as pn_instance
 
 
-def add_arc_from_to_in(fr, to, net):
+def add_arc_from_to(fr, to, net, weight=1):
     '''
     Adds an arc from a specific element to another element in some net. Assumes from and to are in the net!
 
@@ -10,7 +10,7 @@ def add_arc_from_to_in(fr, to, net):
     :param net: net to use
     :return: pass
     '''
-    a = pn_instance.PetriNet.Arc(fr, to)
+    a = pn_instance.PetriNet.Arc(fr, to, weight)
     net.arcs.add(a)
     fr.out_arcs.add(a)
     to.in_arcs.add(a)
