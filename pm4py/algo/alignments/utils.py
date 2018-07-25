cc6b3f2824323efaaa132ecd8ca441057fc782f1
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2018-07-25 14:15:09 +0200

    fix for the tau model moves

--- pm4py/algo/alignments/utils.py
+++ pm4py/algo/alignments/utils.py
@@ -6,7 +6,7 @@ SKIP = '>>'
 def construct_standard_cost_function(synchronous_product_net, skip):
     costs = {}
     for t in synchronous_product_net.transitions:
-        if skip in t.label:
+        if skip in t.label and None not in t.label:
             costs[t] = 1
         else:
             costs[t] = 0
