e865a68c7435776feacb16314ef9370b93a0fc60
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-07-27 13:47:38 +0200

    Tests for alignments

--- tests/executeTests.py
+++ tests/executeTests.py
@@ -3,6 +3,7 @@ from xesImportExportTest import XesImportExportTest
 from csvImportExportTest import CsvImportExportTest
 from alphaMinerTest import AlphaMinerTest
 from imdfTest import InductiveMinerDFTest
+from alignmentTest import AlignmentTest
 
 if __name__ == "__main__":
 	unittest.main()
