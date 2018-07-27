4560e95bffe68db1110c2a92801c874089394679
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-07-27 15:13:37 +0200

    Added tests for importing/exporting the Petri net

--- tests/executeTests.py
+++ tests/executeTests.py
@@ -4,6 +4,7 @@ from csvImportExportTest import CsvImportExportTest
 from alphaMinerTest import AlphaMinerTest
 from imdfTest import InductiveMinerDFTest
 from alignmentTest import AlignmentTest
+from petriImportTest import PetriImportTest
 
 if __name__ == "__main__":
 	unittest.main()
