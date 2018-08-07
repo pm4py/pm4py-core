b74fd0f6449b41e4c544d0e5e1907b964efd3fc8
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2018-08-07 16:31:38 +0200

    improving the alignment performance (still need to be much faster). TODO's: - careful code inspection - variants map - multi threading - postfix cache

--- pm4py/algo/alignments/utils.py
+++ pm4py/algo/alignments/utils.py
@@ -7,7 +7,10 @@ def construct_standard_cost_function(synchronous_product_net, skip):
     costs = {}
     for t in synchronous_product_net.transitions:
         if (skip == t.label[0] or skip == t.label[1]) and (t.label[0] is not None and t.label[1] is not None):
-            costs[t] = 1
+            costs[t] = 10000
         else:
-            costs[t] = 0
+            if skip == t.label[0] and t.label[1] is None:
+                costs[t] = 1
+            else:
+                costs[t] = 0
     return costs
