bc4ec2d59db5dedbbcbafaf58cae3df9362dbf55
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-08-15 13:58:16 +0200

    Revised example for IMDF

--- imdf_example.py
+++ imdf_example.py
@@ -8,63 +8,56 @@ import traceback
 from pm4py.models.petri import exporter as petri_exporter
 from pm4py.models.petri import importer as petri_importer
 
-#log = xes_importer.import_from_path_xes('a32f0n00.xes')
-log = xes_importer.import_from_path_xes('C:\\receipt.xes')
-net, marking = inductMinDirFollows.apply(log)
-petri_exporter.export_petri_to_pnml(net, marking, "receipt.pnml")
-#net, marking = petri_importer.import_petri_from_pnml("receipt.pnml")
-for place in marking:
-	print("initial marking "+place.name)
-final_marking = petri.net.Marking()
-for p in net.places:
-    if not p.out_arcs:
-        final_marking[p] = 1
-for place in final_marking:
-	print("final marking "+place.name)
-gviz = pn_viz.graphviz_visualization(net)
-gviz.view()
-print([x["concept:name"] for x in log[0]])
-fitTraces = []
-[traceIsFit, traceFitnessValue, activatedTransitions, placeFitness] = token_replay.apply_log(log, net, marking,
-																							 final_marking,
-																							 enable_placeFitness=True)
-i = 0
-while i < len(log):
-	try:
-		print("\n",i,[x["concept:name"] for x in log[i]])
-		cfResult = align.versions.state_equation_classic.apply_trace(log[i], net, marking, final_marking)['alignment']
-		if cfResult is None:
-			print("alignment is none!")
-		else:
-			isFit = True
-			for couple in cfResult:
-				print(couple)
-				if not (couple[0] == couple[1] or couple[0] == ">>" and couple[1] == None):
-					isFit = False
-			print("isFit = "+str(isFit))
+if __name__ == "__main__":
+	#log = xes_importer.import_from_path_xes('a32f0n00.xes')
+	log = xes_importer.import_from_path_xes('C:\\receipt.xes')
 
-			if isFit:
-				fitTraces.append(log[i])
+	"""for trace in log:
+		for event in trace:
+			event["newClassifier"] = event["concept:name"] + "+complete"""
 
-			"""if (not isFit) and traceIsFit[i]:
-				print("\n", i, [x["concept:name"] for x in log[i]])
-				print(len(cfResult), len(activatedTransitions[i]))
-				print(cfResult)
-				print(activatedTransitions[i])
-				input()"""
+	net, marking = inductMinDirFollows.apply(log)
+	petri_exporter.export_petri_to_pnml(net, marking, "receipt.pnml")
+	#net, marking = petri_importer.import_petri_from_pnml("receipt.pnml")
+	for place in marking:
+		print("initial marking "+place.name)
+	final_marking = petri.net.Marking()
+	for p in net.places:
+		if not p.out_arcs:
+			final_marking[p] = 1
+	for place in final_marking:
+		print("final marking "+place.name)
+	gviz = pn_viz.graphviz_visualization(net)
+	gviz.view()
 
-			"""if isFit and traceIsFit[i]:
-				print(len(cfResult), len(activatedTransitions[i]))"""
+	#results = align.versions.state_equation_classic.apply_log(log, net, marking, final_marking)
+	#print(results)
 
-			"""if isFit and traceIsFit[i]:
-				if len(cfResult) > len(activatedTransitions[i]):
-					print("\n", i, [x["concept:name"] for x in log[i]])
-					print(len(cfResult), len(activatedTransitions[i]))
-					print(cfResult)
-					print(activatedTransitions[i])"""
-	except:
-		print("EXCEPTION ",i)
-		traceback.print_exc()
-	i = i + 1
-print(fitTraces)
-print(len(fitTraces))
+	if True:
+		fitTraces = []
+		[traceIsFit, traceFitnessValue, activatedTransitions, placeFitness] = token_replay.apply_log(log, net, marking,
+																									 final_marking,
+																									 enable_placeFitness=True)
+		i = 0
+		while i < len(log):
+			try:
+				print("\n",i,[x["concept:name"] for x in log[i]])
+				cfResult = align.versions.state_equation_classic.apply_trace(log[i], net, marking, final_marking)['alignment']
+				if cfResult is None:
+					print("alignment is none!")
+				else:
+					isFit = True
+					for couple in cfResult:
+						print(couple)
+						if not (couple[0] == couple[1] or couple[0] == ">>" and couple[1] == None):
+							isFit = False
+					print("isFit = "+str(isFit))
+
+					if isFit:
+						fitTraces.append(log[i])
+			except:
+				print("EXCEPTION ",i)
+				traceback.print_exc()
+			i = i + 1
+		print(fitTraces)
+		print(len(fitTraces))
