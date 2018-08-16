9cb5298e7fead39202154f722ac34f3b651578a9
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2018-08-16 11:31:56 +0100

    Added main indication in example file.

--- alpha_example.py
+++ alpha_example.py
@@ -3,27 +3,28 @@ from pm4py.log.importer import xes as xes_importer
 from pm4py.models import petri
 from pm4py.algo.alignments.versions import state_equation_classic
 
-log = xes_importer.import_from_path_xes('C:/Users/bas/Documents/tue/svn/private/logs/BPI_Challenge_2012.xes')
-net, marking = alpha_classic.apply(log)
-#viz = petri.visualize.graphviz_visualization(net)
-#viz.view()
+if __name__ == '__main__':
+    log = xes_importer.import_from_path_xes('C:/Users/bas/Documents/tue/svn/private/logs/BPI_Challenge_2012.xes')
+    net, marking = alpha_classic.apply(log)
+    #viz = petri.visualize.graphviz_visualization(net)
+    #viz.view()
 
-log = xes_importer.import_from_path_xes('C:/Users/bas/Documents/tue/svn/private/logs/a12_logs/a12f0n05.xes')
-
-
-final_marking = petri.net.Marking()
-for p in net.places:
-    if not p.out_arcs:
-        final_marking[p] = 1
+    log = xes_importer.import_from_path_xes('C:/Users/bas/Documents/tue/svn/private/logs/a12_logs/a12f0n05.xes')
 
 
-state_equation_classic.apply_log(log, net, marking, final_marking)
+    final_marking = petri.net.Marking()
+    for p in net.places:
+        if not p.out_arcs:
+            final_marking[p] = 1
+
+    
+    state_equation_classic.apply_log(log, net, marking, final_marking)
 
 
-#gviz = pn_viz.graphviz_visualization(net)
-#gviz.view()
+    #gviz = pn_viz.graphviz_visualization(net)
+    #gviz.view()
 
-#(sn, snm) = synchronous_product.construct(net, marking, log[0], '>>')
-#gviz = pn_viz.graphviz_visualization(sn)
-#gviz.view()
+    #(sn, snm) = synchronous_product.construct(net, marking, log[0], '>>')
+    #gviz = pn_viz.graphviz_visualization(sn)
+    #gviz.view()
 
