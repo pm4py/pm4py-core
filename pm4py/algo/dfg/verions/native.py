4866bceda9b1556ba3298cb098a6fdda6930e72e
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2018-08-21 08:50:00 +0200

    fix factory for dfg calculation

--- pm4py/algo/dfg/verions/native.py
+++ pm4py/algo/dfg/verions/native.py
@@ -1,6 +1,19 @@
 from collections import Counter
+from pm4py.log import util
 
 
-def compute_dfg(trace_log, dfg_key='concept:name'):
-    dfgs = map((lambda t: [(t[i - 1][dfg_key], t[i][dfg_key]) for i in range(1, len(t))]), trace_log)
+def apply(trace_log, activity_key=util.xes.DEFAULT_NAME_KEY):
+    '''
+    Counts the number of directly follows occurrences, i.e. of the form <...a,b...>, in an event log.
+
+    Parameters
+    ----------
+    :param trace_log:
+    :param activity_key:
+
+    Returns
+    -------
+    :return:
+    '''
+    dfgs = map((lambda t: [(t[i - 1][activity_key], t[i][activity_key]) for i in range(1, len(t))]), trace_log)
     return Counter([dfg for list in dfgs for dfg in list])
