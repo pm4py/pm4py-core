f8cc54c34086780b25fb3c9284f756989213c027
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2018-08-23 18:38:50 +0200

    some renaming in the transition system class.

--- pm4py/models/transition_system/transition_system.py
+++ pm4py/models/transition_system/transition_system.py
@@ -1,13 +1,11 @@
 class TransitionSystem(object):
 
     class State(object):
-        def __init__(self, name, incoming=None, outgoing=None):
+        def __init__(self, name, incoming=None, outgoing=None, data=None):
             self.__name = name
             self.__incoming = set() if incoming is None else incoming
             self.__outgoing = set() if outgoing is None else outgoing
-
-        def __set_name(self, name):
-            self.__name = name
+            self.__data = dict() if data is None else data
 
         def __get_name(self):
             return self.__name
@@ -18,38 +16,44 @@ class TransitionSystem(object):
         def __get_incoming(self):
             return self.__incoming
 
+        def __get_data(self):
+            return self.__data
+
         def __repr__(self):
             return str(self.name)
 
-        name = property(__get_name, __set_name)
+        name = property(__get_name)
         incoming = property(__get_incoming)
         outgoing = property(__get_outgoing)
+        data = property(__get_data)
 
     class Transition(object):
 
-        def __init__(self, name, from_state=None, to_state=None):
+        def __init__(self, name, from_state=None, to_state=None, data=None):
             self.__name = name
             self.__from_state = set() if from_state is None else from_state
             self.__to_state = set() if to_state is None else to_state
-
-        def __set_name(self, name):
-            self.__name = name
+            self.__data = dict() if data is None else data
 
         def __get_name(self):
             return self.__name
 
-        def __get_out_arcs(self):
+        def __get_to_state(self):
             return self.__to_state
 
-        def __get_in_arcs(self):
+        def __get_from_state(self):
             return self.__from_state
+
+        def __get_data(self):
+            return self.__data
 
         def __repr__(self):
             return str(self.name)
 
-        name = property(__get_name, __set_name)
-        in_arcs = property(__get_in_arcs)
-        out_arcs = property(__get_out_arcs)
+        name = property(__get_name)
+        from_state = property(__get_from_state)
+        to_state = property(__get_to_state)
+        data = property(__get_data)
 
     def __init__(self, name=None, states=None, transitions=None):
         self.__name = "" if name is None else name
@@ -69,6 +73,6 @@ class TransitionSystem(object):
         return self.__transitions
 
     name = property(__get_name, __set_name)
-    places = property(__get_states)
+    states = property(__get_states)
     transitions = property(__get_transitions)
 
