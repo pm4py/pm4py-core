71e5266523cb9629eb95cca1c6961feff0b55442
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2018-08-24 16:28:31 +0200

    some minor changes in alignments

--- pm4py/algo/alignments/utils.py
+++ pm4py/algo/alignments/utils.py
@@ -6,6 +6,17 @@ STD_SYNC_COST = 0
 
 
 def construct_standard_cost_function(synchronous_product_net, skip):
+    '''
+    Returns the standard cost function, which is:
+    * event moves: cost 1000
+    * model moves: cost 1000
+    * tau moves: cost 1
+    * sync moves: cost 0
+
+    :param synchronous_product_net:
+    :param skip:
+    :return:
+    '''
     costs = {}
     for t in synchronous_product_net.transitions:
         if (skip == t.label[0] or skip == t.label[1]) and (t.label[0] is not None and t.label[1] is not None):
--- pm4py/models/petri/utils.py
+++ pm4py/models/petri/utils.py
@@ -5,26 +5,45 @@ def add_arc_from_to(fr, to, net, weight=1):
     '''
     Adds an arc from a specific element to another element in some net. Assumes from and to are in the net!
 
-    :param fr: transition/place from
-    :param to:  transition/place to
-    :param net: net to use
-    :return: pass
+    Parameters
+    ----------
+    fr: transition/place from
+    to:  transition/place to
+    net: net to use
+
+    Returns
+    -------
+    None
     '''
-    a = petri.petrinet.PetriNet.Arc(fr, to, weight)
+    a = petri.net.PetriNet.Arc(fr, to, weight)
     net.arcs.add(a)
     fr.out_arcs.add(a)
     to.in_arcs.add(a)
 
 
 def construct_trace_net(trace, trace_name_key=xes_util.DEFAULT_NAME_KEY, activity_key=xes_util.DEFAULT_NAME_KEY):
-    net = petri.petrinet.PetriNet('trace net of %s' % trace.attributes[trace_name_key] if trace_name_key in trace.attributes else ' ')
-    place_map = {0: petri.petrinet.PetriNet.Place('p_0')}
+    '''
+    Creates a trace net, i.e. a trace in Petri net form.
+
+    Parameters
+    ----------
+    trace: :class:`list` input trace, assumed to be a list of events
+    trace_name_key: :class:`str` key of the attribute that defines the name of the trace.
+    activity_key: :class:`str` key of the attribute of the events that defines the activity name
+
+    Returns
+    -------
+    tuple: :class:`tuple` of the net, initial marking and the final marking
+
+    '''
+    net = petri.net.PetriNet('trace net of %s' % trace.attributes[trace_name_key] if trace_name_key in trace.attributes else ' ')
+    place_map = {0: petri.net.PetriNet.Place('p_0')}
     net.places.add(place_map[0])
     for i in range(0, len(trace)):
-        t = petri.petrinet.PetriNet.Transition('t' + str(i), trace[i][activity_key])
+        t = petri.net.PetriNet.Transition('t' + str(i), trace[i][activity_key])
         net.transitions.add(t)
-        place_map[i+1] = petri.petrinet.PetriNet.Place('p_' + str(i + 1))
+        place_map[i+1] = petri.net.PetriNet.Place('p_' + str(i+1))
         net.places.add(place_map[i+1])
         petri.utils.add_arc_from_to(place_map[i], t, net)
         petri.utils.add_arc_from_to(t, place_map[i+1], net)
-    return net, petri.petrinet.Marking({place_map[0]: 1}), petri.petrinet.Marking({place_map[len(trace)]: 1})
+    return net, petri.net.Marking({place_map[0]: 1}), petri.net.Marking({place_map[len(trace)]: 1})
