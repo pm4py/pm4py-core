d153b3c8f46f3c75c6adb3c7720abe641c1c0096
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-08-27 10:01:32 +0200

    Added paths filter

--- pm4py/log/util/auto_filter.py
+++ pm4py/log/util/auto_filter.py
@@ -1,5 +1,6 @@
 from pm4py.log.util import variants as variants_module
-from pm4py.log.util import start_activities, end_activities, activities
+from pm4py.log.util import start_activities, end_activities, activities, paths
+import gc
 
 def apply_auto_filter(trace_log, decreasingFactor=0.5, activity_key="concept:name"):
     """
@@ -21,14 +22,28 @@ def apply_auto_filter(trace_log, decreasingFactor=0.5, activity_key="concept:nam
     """
     
     # the following filters are applied:
+    # - activity filter (keep only activities with a reasonable number of occurrences)
+    # - paths filter (keep only paths with a reasonable number of occurrences)
     # - variant filter (keep only variants with a reasonable number of occurrences)
     # - start activities filter (keep only variants that starts with a plausible start activity)
     # - end activities filter (keep only variants that starts with a plausible end activity)
     variants = variants_module.get_variants_from_log(trace_log, activity_key=activity_key)
     filtered_log1 = activities.apply_auto_filter(trace_log, variants=variants, decreasingFactor=decreasingFactor, activity_key=activity_key)
+    trace_log = None
+    gc.collect()
     variants = variants_module.get_variants_from_log(filtered_log1, activity_key=activity_key)
-    filtered_log2 = variants_module.apply_auto_filter(filtered_log1, variants=variants, decreasingFactor=decreasingFactor, activity_key=activity_key)
-    filtered_log3 = start_activities.apply_auto_filter(filtered_log2, variants=variants, decreasingFactor=decreasingFactor, activity_key=activity_key)
-    filtered_log4 = end_activities.apply_auto_filter(filtered_log3, variants=variants, decreasingFactor=decreasingFactor, activity_key=activity_key)
-    
-    return filtered_log4
+    filtered_log2 = paths.apply_auto_filter(filtered_log1, variants=variants, decreasingFactor=decreasingFactor, activity_key=activity_key)
+    filtered_log1 = None
+    gc.collect()
+    variants = variants_module.get_variants_from_log(filtered_log2, activity_key=activity_key)
+    filtered_log3 = variants_module.apply_auto_filter(filtered_log2, variants=variants, decreasingFactor=decreasingFactor, activity_key=activity_key)
+    filtered_log2 = None
+    gc.collect()
+    filtered_log4 = start_activities.apply_auto_filter(filtered_log3, variants=variants, decreasingFactor=decreasingFactor, activity_key=activity_key)
+    filtered_log3 = None
+    gc.collect()
+    filtered_log5 = end_activities.apply_auto_filter(filtered_log4, variants=variants, decreasingFactor=decreasingFactor, activity_key=activity_key)
+    filtered_log4 = None
+    gc.collect()
+
+    return filtered_log5
