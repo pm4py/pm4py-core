9cbcc37a9defab8b5cf2827e750a974430bf3f14
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-08-30 16:07:28 +0200

    Made CSV importer more customizable: specification of quote char and number of rows

--- pm4py/log/importer/csv.py
+++ pm4py/log/importer/csv.py
@@ -2,7 +2,29 @@ from pm4py import log
 import pandas as pd
 import tempfile, os
 
-def import_from_csv_string(csv_string, sep=','):
+def import_dataframe_from_csv_string(csv_string, sep=',', quotechar=None, nrows=None):
+    """
+    Import dataframe from CSV string
+
+    Parameters
+    -----------
+    csv_string
+        CSV string
+
+    Returns
+    -----------
+    df
+        Pandas dataframe
+    """
+    fp = tempfile.NamedTemporaryFile(suffix='.csv')
+    fp.close()
+    with open(fp.name, 'w') as f:
+        f.write(csv_string)
+    df = import_dataframe_from_path(fp.name, sep=sep, quotechar=quotechar, nrows=nrows)
+    os.remove(fp.name)
+    return df
+
+def import_from_csv_string(csv_string, sep=',', quotechar=None, nrows=None):
     """
     Import CSV log from CSV string
 
@@ -20,11 +42,61 @@ def import_from_csv_string(csv_string, sep=','):
     fp.close()
     with open(fp.name, 'w') as f:
         f.write(csv_string)
-    log = import_from_path(fp.name, sep=',')
+    log = import_from_path(fp.name, sep=sep, quotechar=quotechar, nrows=nrows)
     os.remove(fp.name)
     return log
 
-def import_from_path(path, sep=','):
+def import_dataframe_from_path(path, sep=',', quotechar=None, nrows=None):
+    """
+    Imports a dataframe from the given path
+
+    Parameters
+    ----------
+    path:
+        Input CSV file path
+    sep:
+        column separator
+
+     Returns
+    -------
+    pd
+        Pandas dataframe
+    """
+    if quotechar:
+        if nrows:
+            df = pd.read_csv(path, sep=sep, quotechar=quotechar, nrows=nrows)
+        else:
+            df = pd.read_csv(path, sep=sep, quotechar=quotechar)
+    else:
+        if nrows:
+            df = pd.read_csv(path, sep=sep, nrows=nrows)
+        else:
+            df = pd.read_csv(path, sep=sep)
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except ValueError:
+                    pass
+    return df
+
+def convert_dataframe_to_event_log(df):
+    """
+    Converts a dataframe to an event log
+
+    Parameters
+    ----------
+    df
+        Pandas dataframe
+
+     Returns
+    -------
+    log : :class:`pm4py.log.instance.EventLog`
+        An event log
+    """
+    return log.instance.EventLog(df.to_dict('records'), attributes={'origin': 'csv'})
+
+def import_from_path(path, sep=',', quotechar=None, nrows=None):
     """
     Imports a CSV file from the given path
 
@@ -40,11 +112,5 @@ def import_from_path(path, sep=','):
     log : :class:`pm4py.log.instance.EventLog`
         An event log
     """
-    df = pd.read_csv(path, sep=sep)
-    for col in df.columns:
-        if df[col].dtype == 'object':
-            try:
-                df[col] = pd.to_datetime(df[col])
-            except ValueError:
-                    pass
-    return log.instance.EventLog(df.to_dict('records'), attributes={'origin': 'csv'})
+    df = import_dataframe_from_path(path, sep=sep, quotechar=quotechar, nrows=nrows)
+    return convert_dataframe_to_event_log(df)
