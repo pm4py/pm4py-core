e109fcb5ecd5214e43ffd9c28fea2eb0e10879fe
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-01 12:21:44 +0200

    Adding DFG with performance metric

--- pm4py/algo/dfg/versions/native.py
+++ pm4py/algo/dfg/versions/native.py
@@ -8,12 +8,19 @@ def apply(trace_log, parameters=None, activity_key=util.xes.DEFAULT_NAME_KEY, ti
 
     Parameters
     ----------
-    :param trace_log:
-    :param activity_key:
+    trace_log
+        Trace log
+    parameters
+        Possible parameters passed to the algorithms
+    activity_key
+        Field in the log that represents the activity (must be specified if different from concept:name)
+    timestamp_key
+        Field in the log that represents the timestamp
 
     Returns
     -------
-    :return:
+    dfg
+        DFG graph
     '''
     dfgs = map((lambda t: [(t[i - 1][activity_key], t[i][activity_key]) for i in range(1, len(t))]), trace_log)
     return Counter([dfg for list in dfgs for dfg in list])
---
+++ pm4py/algo/dfg/versions/performance.py
@@ -0,0 +1,37 @@
+from collections import Counter
+from pm4py.log import util
+from statistics import mean
+
+def apply(trace_log, parameters=None, activity_key=util.xes.DEFAULT_NAME_KEY, timestamp_key="time:timestamp"):
+    '''
+    Measure performance between couples of activities in the DFG graph
+
+    Parameters
+    ----------
+    trace_log
+        Trace log
+    parameters
+        Possible parameters passed to the algorithms
+    activity_key
+        Field in the log that represents the activity (must be specified if different from concept:name)
+    timestamp_key
+        Field in the log that represents the timestamp
+
+    Returns
+    -------
+    dfg
+        DFG graph
+    '''
+
+    dfgs0 = map((lambda t: [((t[i - 1][activity_key], t[i][activity_key]), (t[i][timestamp_key]-t[i-1][timestamp_key]).total_seconds())
+                             for i in range(1, len(t))]), trace_log)
+    ret0 = {}
+    for el in dfgs0:
+        for couple in el:
+            if not couple[0] in ret0:
+                ret0[couple[0]] = []
+            ret0[couple[0]].append(couple[1])
+    ret = Counter()
+    for key in ret0:
+        ret[key] = mean(ret0[key])
+    return ret
