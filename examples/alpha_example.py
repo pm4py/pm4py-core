67cec46c30aefbcba5056089685d06205700466e
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-01 09:29:15 +0200

    Removed broken example script

--- examples/alpha_example.py
+++
@@ -1,5 +0,0 @@
-import pm4py.log.importer as importer
-
-
-if __name__ == '__main__':
-    log = importer.xes.import_from_file_xes('C:/Users/bas/Documents/tue/svn/private/logs/BPI_Challenge_2012.xes')
