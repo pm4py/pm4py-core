e75b0ec5eb68be07d59b7bffafc9111c3bae3408
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-03 12:27:53 +0200

    Fixed path in example script

--- examples/big_dataframe_management.py
+++ examples/big_dataframe_management.py
@@ -8,7 +8,7 @@ from pm4py.log import transform
 import time
 
 time1 = time.time()
-inputLog = "C:\\roadtraffic.csv"
+inputLog = "..\\tests\\inputData\\running-example.csv"
 dataframe = csv_importer.import_dataframe_from_path_wo_timeconversion(inputLog, sep=',')
 time2 = time.time()
 print("time2 - time1: "+str(time2-time1))
@@ -18,7 +18,7 @@ print("time3 - time2: "+str(time3-time2))
 dataframe = df_filtering.filter_df_on_ncases(dataframe, case_id_glue="case:concept:name", max_no_cases=1000)
 time4 = time.time()
 print("time4 - time3: "+str(time4-time3))
-#dataframe = df_filtering.filter_df_on_case_length(dataframe, case_id_glue="case:concept:name", min_trace_length=3, max_trace_length=50)
+dataframe = df_filtering.filter_df_on_case_length(dataframe, case_id_glue="case:concept:name", min_trace_length=3, max_trace_length=50)
 print(dataframe)
 time5 = time.time()
 print("time5 - time4: "+str(time5-time4))
