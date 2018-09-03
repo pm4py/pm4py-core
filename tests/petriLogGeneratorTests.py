c6d694a6aa92c2c2c77cfce296ecd7aac3be0045
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-03 10:34:54 +0200

    Added tests for ET Conformance functioning, Petri net generator and log generator

---
+++ tests/petriLogGeneratorTests.py
@@ -0,0 +1,16 @@
+import unittest
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+from tests.constants import INPUT_DATA_DIR
+from pm4py.algo.petrigenerator.versions import simple_generator as petri_generator
+from pm4py.algo.playout import factory as playout_factory
+
+class PetriLogGeneratorTests(unittest.TestCase):
+    def test_petri_log_generation(self):
+        net, marking, final_marking = petri_generator.generate_petri()
+        log = playout_factory.apply(net, marking)
+
+if __name__ == "__main__":
+    unittest.main()
