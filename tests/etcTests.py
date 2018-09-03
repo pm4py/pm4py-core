34acbf9b22f492e09320289c3f6fe7c4c8019d3b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-03 10:34:36 +0200

    Added tests for ET Conformance functioning, Petri net generator and log generator

---
+++ tests/etcTests.py
@@ -0,0 +1,18 @@
+import unittest
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+from tests.constants import INPUT_DATA_DIR
+from pm4py.log.importer import xes as xes_importer
+from pm4py.algo.etconformance import factory as etc_factory
+from pm4py.algo.inductive.versions import dfg_only
+
+class ETCTest(unittest.TestCase):
+    def test_etc1(self):
+        log = xes_importer.import_from_file_xes(os.path.join(INPUT_DATA_DIR, "running-example.xes"))
+        net, marking, final_marking = dfg_only.apply(log, None)
+        precision = etc_factory.apply(log, net, marking, final_marking)
+
+if __name__ == "__main__":
+    unittest.main()
