ebf1df2eba61874b4e652f0e9776e72ef6021790
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-04 16:29:12 +0200

    Fixed DFG management for IMDF

--- pm4py/algo/inductive/factory.py
+++ pm4py/algo/inductive/factory.py
@@ -4,5 +4,10 @@ from pm4py.algo.inductive import versions
 INDUCTIVE_ONLY_DFG = 'only_dfg'
 VERSIONS = {INDUCTIVE_ONLY_DFG: versions.dfg_only.apply}
 
+VERSIONS_DFG = {INDUCTIVE_ONLY_DFG: versions.dfg_only.apply_dfg}
+
 def apply(log, parameters=None, activity_key=xes_util.DEFAULT_NAME_KEY, variant=INDUCTIVE_ONLY_DFG):
     return VERSIONS[variant](log, parameters, activity_key)
+
+def apply_dfg(dfg, parameters=None, activity_key=xes_util.DEFAULT_NAME_KEY, variant=INDUCTIVE_ONLY_DFG):
+    return VERSIONS_DFG[variant](dfg, parameters, activity_key)
