225bc4e7eace88e8bc75ed50d6dec13fa3f9a5ce
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-05 09:30:41 +0200

    Refactored token-based generalization and replay fitness

--- pm4py/evaluation/generalization/versions/token_based.py
+++ pm4py/evaluation/generalization/versions/token_based.py
@@ -7,6 +7,50 @@ PARAM_ACTIVITY_KEY = 'activity_key'
 
 PARAMETERS = [PARAM_ACTIVITY_KEY]
 
+def get_generalization(petri_net, activatedTransitions):
+    """
+    Gets the generalization from the Petri net and the list of activated transitions
+    during the replay
+
+    The approach has been suggested by the paper
+    Buijs, Joos CAM, Boudewijn F. van Dongen, and Wil MP van der Aalst. "Quality dimensions in process discovery:
+    The importance of fitness, precision, generalization and simplicity."
+    International Journal of Cooperative Information Systems 23.01 (2014): 1440001.
+
+    A token replay is applied and, for each transition, we can measure the number of occurrences
+    in the replay. The following formula is applied for generalization
+
+           \sum_{t \in transitions} (math.sqrt(1.0/(n_occ_replay(t)))
+    1 -    ----------------------------------------------------------
+                             # transitions
+
+    Parameters
+    -----------
+    activatedTransitions
+        Activated transition during (token-based) replay
+
+    Returns
+    -----------
+    generalization
+        Generalization measure
+    """
+
+    transOccMap = Counter()
+    for trace in activatedTransitions:
+        for trans in trace:
+            transOccMap[trans] += 1
+    inv_sq_occ_sum = 0.0
+    for trans in transOccMap:
+        this_term = 1.0 / sqrt(transOccMap[trans])
+        inv_sq_occ_sum = inv_sq_occ_sum + this_term
+    for trans in petri_net.transitions:
+        if not trans in transOccMap:
+            inv_sq_occ_sum = inv_sq_occ_sum + 1
+    generalization = 1.0
+    if len(petri_net.transitions) > 0:
+        generalization = 1.0 - inv_sq_occ_sum / float(len(petri_net.transitions))
+    return generalization
+
 def apply(log, petri_net, initial_marking, final_marking, parameters=None):
     """
     Calculates generalization on the provided log and Petri net.
@@ -39,25 +83,12 @@ def apply(log, petri_net, initial_marking, final_marking, parameters=None):
     Returns
     -----------
     generalization
-        Generalization measured
+        Generalization measure
     """
     if parameters is None:
         parameters = {}
     activity_key = parameters[PARAM_ACTIVITY_KEY] if PARAM_ACTIVITY_KEY in parameters else log_lib.util.xes.DEFAULT_NAME_KEY
     [traceIsFit, traceFitnessValue, activatedTransitions, placeFitness, reachedMarkings, enabledTransitionsInMarkings] =\
         token_replay.apply(log, petri_net, initial_marking, final_marking, activity_key=activity_key)
-    transOccMap = Counter()
-    for trace in activatedTransitions:
-        for trans in trace:
-            transOccMap[trans] += 1
-    inv_sq_occ_sum = 0.0
-    for trans in transOccMap:
-        this_term = 1.0 / sqrt(transOccMap[trans])
-        inv_sq_occ_sum = inv_sq_occ_sum + this_term
-    for trans in petri_net.transitions:
-        if not trans in transOccMap:
-            inv_sq_occ_sum = inv_sq_occ_sum + 1
-    generalization = 1.0
-    if len(petri_net.transitions) > 0:
-        generalization = 1.0 - inv_sq_occ_sum / float(len(petri_net.transitions))
-    return generalization
+
+    return get_generalization(petri_net, activatedTransitions)
--- pm4py/evaluation/replay_fitness/versions/token_replay.py
+++ pm4py/evaluation/replay_fitness/versions/token_replay.py
@@ -5,6 +5,33 @@ PARAM_ACTIVITY_KEY = 'activity_key'
 
 PARAMETERS = [PARAM_ACTIVITY_KEY]
 
+def get_fitness(traceIsFit, traceFitnessValue):
+    """
+    Gets a dictionary expressing fitness in a synthetic way from the list of boolean values
+    saying if a trace in the log is fit, and the float values of fitness associated to each trace
+
+    Parameters
+    ------------
+    traceIsFit
+        Boolean value that tells if a trace is fit according to the model
+    traceFitnessValue
+        List of float values of fitness associated to each trace
+
+    Returns
+    -----------
+    dictionary
+        Containing two keys (percFitTraces and averageFitness)
+    """
+    noTraces = len(traceIsFit)
+    fitTraces = len([x for x in traceIsFit if x])
+    sumOfFitness = sum(traceFitnessValue)
+    percFitTraces = 0.0
+    averageFitness = 0.0
+    if noTraces > 0:
+        percFitTraces = float(100.0 * fitTraces)/float(noTraces)
+        averageFitness = float(sumOfFitness)/float(noTraces)
+    return {"percFitTraces": percFitTraces, "averageFitness": averageFitness}
+
 def apply(log, petri_net, initial_marking, final_marking, parameters=None):
     """
     Apply token replay fitness evaluation
@@ -33,13 +60,6 @@ def apply(log, petri_net, initial_marking, final_marking, parameters=None):
     activity_key = parameters[PARAM_ACTIVITY_KEY] if PARAM_ACTIVITY_KEY in parameters else log_lib.util.xes.DEFAULT_NAME_KEY
     [traceIsFit, traceFitnessValue, activatedTransitions, placeFitness, reachedMarkings, enabledTransitionsInMarkings] =\
         token_replay.apply(log, petri_net, initial_marking, final_marking, activity_key=activity_key)
-    noTraces = len(traceIsFit)
-    fitTraces = len([x for x in traceIsFit if x])
-    sumOfFitness = sum(traceFitnessValue)
-    percFitTraces = 0.0
-    averageFitness = 0.0
-    if noTraces > 0:
-        percFitTraces = float(100.0 * fitTraces)/float(noTraces)
-        averageFitness = float(sumOfFitness)/float(noTraces)
-    return {"percFitTraces": percFitTraces, "averageFitness": averageFitness}
+
+    return get_fitness(traceIsFit, traceFitnessValue)
 
