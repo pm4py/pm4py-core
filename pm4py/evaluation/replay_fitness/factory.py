84db7a3d384cd59118565aac0d89ed448d1ed364
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-05 08:09:50 +0200

    Added token-based replay fitness evaluation

--- pm4py/evaluation/replay_fitness/factory.py
+++ pm4py/evaluation/replay_fitness/factory.py
@@ -1,9 +1,10 @@
-from pm4py.evaluation.replay_fitness.versions import alignment_based
+from pm4py.evaluation.replay_fitness.versions import alignment_based, token_replay
 
 ALIGNMENT_BASED = "alignments"
-VERSIONS = {ALIGNMENT_BASED: alignment_based.apply}
+TOKEN_BASED = "token_replay"
+VERSIONS = {ALIGNMENT_BASED: alignment_based.apply, TOKEN_BASED: token_replay.apply}
 
 PARAM_ACTIVITY_KEY = 'activity_key'
 
-def apply(log, petri_net, initial_marking, final_marking, parameters=None, variant="alignments"):
-    return VERSIONS[ALIGNMENT_BASED](log, petri_net, initial_marking, final_marking, parameters=parameters)
+def apply(log, petri_net, initial_marking, final_marking, parameters=None, variant="token_replay"):
+    return VERSIONS[variant](log, petri_net, initial_marking, final_marking, parameters=parameters)
