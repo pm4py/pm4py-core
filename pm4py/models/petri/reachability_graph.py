f8c323275ff319f3c6b61d8ae8cf3e5e4e19a83b
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2018-09-06 20:25:52 +0100

    Implemented reachability graph construction. Needs testing.

---
+++ pm4py/models/petri/reachability_graph.py
@@ -0,0 +1,38 @@
+from pm4py.models import petri
+from pm4py.models.transition_system import transition_system as ts
+from pm4py.models.transition_system import utils
+
+
+def construct_reachability_graph(net, initial_marking):
+    '''
+        Creates a reachability graph of a certain Petri net.
+
+        Parameters
+        ----------
+        net: Petri net
+        initial_marking: initial marking of the Ptri net.
+
+        Returns
+        -------
+        re_gr: Transition system that represents the reachability graph of the input Petri net.
+
+        '''
+    active = [initial_marking]
+    visited = []
+    re_gr = ts.TransitionSystem()
+    re_gr.states.add(ts.TransitionSystem.State(repr(initial_marking)))
+    while active:
+        curr_mark = active.pop(0)
+        curr_state = next((state for state in re_gr.states if state.name == repr(curr_mark)), None)
+        en_tr = petri.semantics.enabled_transitions(net, curr_mark)
+        for t in en_tr:
+            next_mark = petri.semantics.execute(t, net, curr_mark)
+            next_state = next((state for state in re_gr.states if state.name == repr(next_mark)), None)
+            if next_state is None:
+                next_state = ts.TransitionSystem.State(repr(next_mark))
+                re_gr.states.add(next_state)
+            utils.add_arc_from_to(repr(t), curr_state, next_state, re_gr)
+            if hash(next_mark) not in visited and next_mark not in active and hash(curr_mark) != hash(next_mark):
+                active.append(next_mark)
+        visited.append(hash(curr_mark))
+    return re_gr
