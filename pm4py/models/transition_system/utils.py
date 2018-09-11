930a51d70f835c03f5c551dfa7be9b5a481e53f9
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2018-09-11 15:20:52 +0100

    Added function that removes transitions by name in transition systems.

--- pm4py/models/transition_system/utils.py
+++ pm4py/models/transition_system/utils.py
@@ -8,7 +8,7 @@ def add_arc_from_to(name, fr, to, ts, data=None):
 
     Parameters
     ----------
-    name:
+    name: name of the transition
     fr: state from
     to:  state to
     ts: transition system to use
@@ -21,3 +21,39 @@ def add_arc_from_to(name, fr, to, ts, data=None):
     ts.transitions.add(tran)
     fr.outgoing.add(tran)
     to.incoming.add(tran)
+
+
+def remove_arc_from_to(name, fr, to, ts):
+    '''
+    Removes a transition with a specific name from a state to another state in some transition system.
+    Assumes from and to are in the transition system!
+
+    Parameters
+    ----------
+    name: name of the transition
+    fr: state from
+    to:  state to
+    ts: transition system to use
+
+    Returns
+    -------
+    None
+    '''
+    ts.transitions[:] = [t for t in ts.transitions if t.name != name]
+    fr.outgoing[:] = [t for t in fr.outgoing if t.name != name]
+    to.incoming[:] = [t for t in fr.incoming if t.name != name]
+
+
+# def transitive_reduction(ts):
+#     for vertex0 in vertices:
+#         done = set()
+#         for child in vertex0.children:
+#             df(edges, vertex0, child, done)
+#
+#     df = function(edges, vertex0, child0, done)
+#     if child0 in done:
+#         return
+#     for child in child0.children:
+#         edge.discard((vertex0, child))
+#         df(edges, vertex0, child, done)
+#     done.add(child0)
