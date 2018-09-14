f12c067a17cbc06a4b7dae447bc9775c06dcea8e
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-14 14:51:15 +0200

    Refactored filtering structure into separate package

---
+++ pm4py/filtering/tracelog/__init__.py
@@ -0,0 +1,6 @@
+from pm4py.filtering.tracelog.variants import *
+from pm4py.filtering.tracelog.attributes import *
+from pm4py.filtering.tracelog.end_activities import *
+from pm4py.filtering.tracelog.paths import *
+from pm4py.filtering.tracelog.start_activities import *
+from pm4py.filtering.tracelog.auto_filter import *
