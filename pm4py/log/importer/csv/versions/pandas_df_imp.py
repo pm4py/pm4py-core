b77104084fcc03f72fbee2e9c8382448880ca157
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-15 16:03:23 +0200

    Started refactoring of log importing and exporting into factories

--- pm4py/log/importer/csv/versions/pandas_df_imp.py
+++ pm4py/log/importer/csv/versions/pandas_df_imp.py
@@ -1,39 +1,6 @@
 from pm4py import log
-import pandas as pd
 import tempfile, os
-from pm4py.log.importer.utils import df_filtering
-
-def import_dataframe_from_csv_string(csv_string, sep=',', quotechar=None, nrows=None, sort=False, sort_field="time:timestamp"):
-    """
-    Import dataframe from CSV string
-
-    Parameters
-    -----------
-    csv_string
-        CSV string
-    sep
-        CSV columns delimiter
-    quotechar
-        (if specified) Character that starts/end big strings in CSV
-    nrows
-        (if specified) Maximum number of rows to read from the CSV
-    sort
-        Boolean value that tells if the CSV should be ordered
-    sort_field
-        If sort option is enabled, then the CSV is automatically sorted by the specified column
-
-    Returns
-    -----------
-    df
-        Pandas dataframe
-    """
-    fp = tempfile.NamedTemporaryFile(suffix='.csv')
-    fp.close()
-    with open(fp.name, 'w') as f:
-        f.write(csv_string)
-    df = import_dataframe_from_path(fp.name, sep=sep, quotechar=quotechar, nrows=nrows, sort=sort, sort_field=sort_field)
-    os.remove(fp.name)
-    return df
+from pm4py.log.adapters.pandas.csv_import_adapter import import_dataframe_from_path
 
 def import_from_csv_string(csv_string, sep=',', quotechar=None, nrows=None, sort=False, sort_field="time:timestamp", insert_event_indexes=False):
     """
@@ -65,91 +32,6 @@ def import_from_csv_string(csv_string, sep=',', quotechar=None, nrows=None, sort
     log = import_from_path(fp.name, sep=sep, quotechar=quotechar, nrows=nrows, sort=sort, sort_field=sort_field, insert_event_indexes=insert_event_indexes)
     os.remove(fp.name)
     return log
-
-def import_dataframe_from_path_wo_timeconversion(path, sep=',', quotechar=None, nrows=None):
-    """
-    Imports a dataframe from the given path (without doing the timestamp columns conversion)
-
-    Parameters
-    ----------
-    path:
-        Input CSV file path
-    sep:
-        column separator
-    quotechar
-        (if specified) Character that starts/end big strings in CSV
-    nrows
-        (if specified) Maximum number of rows to read from the CSV
-
-     Returns
-    -------
-    pd
-        Pandas dataframe
-    """
-    if quotechar:
-        if nrows:
-            df = pd.read_csv(path, sep=sep, quotechar=quotechar, nrows=nrows)
-        else:
-            df = pd.read_csv(path, sep=sep, quotechar=quotechar)
-    else:
-        if nrows:
-            df = pd.read_csv(path, sep=sep, nrows=nrows)
-        else:
-            df = pd.read_csv(path, sep=sep)
-
-    return df
-
-def convert_timestamp_columns_in_df(df):
-    """
-    Convert all dataframe columns in a dataframe
-
-    Parameters
-    -----------
-    df
-        Dataframe
-
-    Returns
-    ------------
-    df
-        Dataframe with timestamp columns converted
-    """
-    for col in df.columns:
-        if df[col].dtype == 'object':
-            try:
-                df[col] = pd.to_datetime(df[col])
-            except ValueError:
-                    pass
-    return df
-
-def import_dataframe_from_path(path, sep=',', quotechar=None, nrows=None, sort=False, sort_field="time:timestamp"):
-    """
-    Imports a dataframe from the given path
-
-    Parameters
-    ----------
-    path:
-        Input CSV file path
-    sep:
-        column separator
-    quotechar
-        (if specified) Character that starts/end big strings in CSV
-    nrows
-        (if specified) Maximum number of rows to read from the CSV
-    sort
-        Boolean value that tells if the CSV should be ordered
-    sort_field
-        If sort option is enabled, then the CSV is automatically sorted by the specified column
-
-     Returns
-    -------
-    pd
-        Pandas dataframe
-    """
-    df = import_dataframe_from_path_wo_timeconversion(path, sep=sep, quotechar=quotechar, nrows=nrows)
-    df = convert_timestamp_columns_in_df(df)
-    if sort and sort_field:
-        df = df.sort_values(sort_field)
-    return df
 
 def convert_dataframe_to_event_log(df):
     """
--- tests/dataframePrefiltering.py
+++ tests/dataframePrefiltering.py
@@ -4,6 +4,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 from tests.constants import INPUT_DATA_DIR
+from pm4py.log.adapters.pandas import csv_import_adapter as csv_import_adapter
 from pm4py.log.importer.csv.versions import pandas_df_imp
 from pm4py.log.importer.utils import df_filtering
 from pm4py.log import transform
@@ -12,11 +13,11 @@ import time
 class DataframePrefilteringTest(unittest.TestCase):
     def test_prefiltering_dataframe(self):
         inputLog = os.path.join(INPUT_DATA_DIR, "running-example.csv")
-        dataframe = pandas_df_imp.import_dataframe_from_path_wo_timeconversion(inputLog, sep=',')
+        dataframe = csv_import_adapter.import_dataframe_from_path_wo_timeconversion(inputLog, sep=',')
         dataframe = df_filtering.filter_df_on_activities(dataframe, activity_key="concept:name")
         dataframe = df_filtering.filter_df_on_ncases(dataframe, case_id_glue="case:concept:name")
         dataframe = df_filtering.filter_df_on_case_length(dataframe, case_id_glue="case:concept:name")
-        dataframe = pandas_df_imp.convert_timestamp_columns_in_df(dataframe)
+        dataframe = csv_import_adapter.convert_timestamp_columns_in_df(dataframe)
         dataframe = dataframe.sort_values('time:timestamp')
         eventLog = pandas_df_imp.convert_dataframe_to_event_log(dataframe)
         traceLog = transform.transform_event_log_to_trace_log(eventLog)
