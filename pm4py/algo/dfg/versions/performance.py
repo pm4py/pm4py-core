22e1c088116f95567fc846a124eb6448bf4f93f7
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-17 17:25:16 +0200

    Given the possibility to specify performance aggregation metric in performance DFG graph

--- pm4py/algo/dfg/versions/performance.py
+++ pm4py/algo/dfg/versions/performance.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from pm4py.log import util
-from statistics import mean
+from statistics import mean, median
 
 def apply(trace_log, parameters=None, activity_key=util.xes.DEFAULT_NAME_KEY, timestamp_key="time:timestamp"):
     '''
@@ -11,7 +11,8 @@ def apply(trace_log, parameters=None, activity_key=util.xes.DEFAULT_NAME_KEY, ti
     trace_log
         Trace log
     parameters
-        Possible parameters passed to the algorithms
+        Possible parameters passed to the algorithms:
+            aggregationMeasure -> performance aggregation measure (min, max, mean, median)
     activity_key
         Field in the log that represents the activity (must be specified if different from concept:name)
     timestamp_key
@@ -23,6 +24,14 @@ def apply(trace_log, parameters=None, activity_key=util.xes.DEFAULT_NAME_KEY, ti
         DFG graph
     '''
 
+    if parameters is None:
+        parameters = {}
+
+    aggregationMeasure = "mean"
+
+    if "aggregationMeasure" in parameters:
+        aggregationMeasure = parameters["aggregationMeasure"]
+
     dfgs0 = map((lambda t: [((t[i - 1][activity_key], t[i][activity_key]), (t[i][timestamp_key]-t[i-1][timestamp_key]).total_seconds())
                              for i in range(1, len(t))]), trace_log)
     ret0 = {}
@@ -33,5 +42,13 @@ def apply(trace_log, parameters=None, activity_key=util.xes.DEFAULT_NAME_KEY, ti
             ret0[couple[0]].append(couple[1])
     ret = Counter()
     for key in ret0:
-        ret[key] = mean(ret0[key])
+        if aggregationMeasure == "median":
+            ret[key] = median(ret0[key])
+        elif aggregationMeasure == "min":
+            ret[key] = min(ret0[key])
+        elif aggregationMeasure == "max":
+            ret[key] = max(ret0[key])
+        else:
+            ret[key] = mean(ret0[key])
+
     return ret
