e937d75ba949829385641750acf5e4147e8031a9
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-17 12:09:54 +0200

    Removed older methods about XES/CSV export

--- tests/csvImportExportTest.py
+++ tests/csvImportExportTest.py
@@ -4,17 +4,17 @@ import os, sys, inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
-from pm4py.log.importer import xes_importer as xes_importer
-from pm4py.log.exporter import xes_exporter as xes_exporter
-from pm4py.log.importer import csv_importer as csv_importer
-from pm4py.log.exporter import csv_exporter as csv_exporter
+from pm4py.log.importer.xes import factory as xes_importer
+from pm4py.log.exporter.xes import factory as xes_exporter
+from pm4py.log.importer.csv import factory as csv_importer
+from pm4py.log.exporter.csv import factory as csv_exporter
 import pm4py.log.transform as log_transform
 from tests.constants import INPUT_DATA_DIR, OUTPUT_DATA_DIR
 
 
 class CsvImportExportTest(unittest.TestCase):
     def test_importExportCSVtoXES(self):
-        eventLog = csv_importer.import_from_path(os.path.join(INPUT_DATA_DIR, "running-example.csv"))
+        eventLog = csv_importer.import_log(os.path.join(INPUT_DATA_DIR, "running-example.csv"))
         eventLog.sort()
         eventLog.sample()
         eventLog.insert_event_index_as_event_attribute()
@@ -23,13 +23,13 @@ class CsvImportExportTest(unittest.TestCase):
         traceLog.sample()
         traceLog.insert_trace_index_as_event_attribute()
         xes_exporter.export_log(traceLog, os.path.join(OUTPUT_DATA_DIR, "running-example-exported.xes"))
-        traceLogImportedAfterExport = xes_importer.import_from_file_xes(
+        traceLogImportedAfterExport = xes_importer.import_log(
             os.path.join(OUTPUT_DATA_DIR, "running-example-exported.xes"))
         self.assertEqual(len(traceLog), len(traceLogImportedAfterExport))
         os.remove(os.path.join(OUTPUT_DATA_DIR, "running-example-exported.xes"))
 
     def test_importExportCSVtoCSV(self):
-        eventLog = csv_importer.import_from_path(os.path.join(INPUT_DATA_DIR, "running-example.csv"))
+        eventLog = csv_importer.import_log(os.path.join(INPUT_DATA_DIR, "running-example.csv"))
         eventLog.sort()
         eventLog.sample()
         eventLog.insert_event_index_as_event_attribute()
@@ -39,7 +39,7 @@ class CsvImportExportTest(unittest.TestCase):
         traceLog.insert_trace_index_as_event_attribute()
         eventLogTransformed = log_transform.transform_trace_log_to_event_log(traceLog)
         csv_exporter.export_log(eventLogTransformed, os.path.join(OUTPUT_DATA_DIR, "running-example-exported.csv"))
-        eventLogImportedAfterExport = csv_importer.import_from_path(
+        eventLogImportedAfterExport = csv_importer.import_log(
             os.path.join(OUTPUT_DATA_DIR, "running-example-exported.csv"))
         traceLogImportedAfterExport = log_transform.transform_event_log_to_trace_log(eventLogImportedAfterExport)
         self.assertEqual(len(traceLog), len(traceLogImportedAfterExport))
--- tests/documentationTests/testDocCsv1.py
+++ tests/documentationTests/testDocCsv1.py
@@ -12,9 +12,9 @@ class CSV1DocumentationTest(unittest.TestCase):
     def test_csv1documentation(self):
         import os
 
-        from pm4py.log.importer import csv_importer as csv_importer
+        from pm4py.log.importer.csv import factory as csv_importer
 
-        event_log = csv_importer.import_from_path("inputData\\running-example.csv", sep=",")
+        event_log = csv_importer.import_log("inputData\\running-example.csv")
 
         event_log_length = len(event_log)
         # print(event_log_length)
@@ -29,16 +29,16 @@ class CSV1DocumentationTest(unittest.TestCase):
         from pm4py.log.importer.csv.versions import pandas_df_imp
         from pm4py.log import transform
 
-        dataframe = pandas_df_imp.import_dataframe_from_path("inputData\\running-example.csv", sep=",")
+        dataframe = pandas_df_imp.import_dataframe_from_path("inputData\\running-example.csv")
         event_log = pandas_df_imp.convert_dataframe_to_event_log(dataframe)
         trace_log = transform.transform_event_log_to_trace_log(event_log, case_glue="case:concept:name")
 
-        from pm4py.log.exporter import csv_exporter as csv_exporter
+        from pm4py.log.exporter.csv import factory as csv_exporter
 
         csv_exporter.export_log(event_log, "outputFile1.csv")
         os.remove("outputFile1.csv")
 
-        from pm4py.log.exporter import csv_exporter as csv_exporter
+        from pm4py.log.exporter.csv import factory as csv_exporter
 
         csv_exporter.export_log(trace_log, "outputFile2.csv")
         os.remove("outputFile2.csv")
