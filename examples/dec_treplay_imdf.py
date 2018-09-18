fcbd086ac6048c2b93000e62ebd130b35f9d4cae
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-18 09:59:52 +0200

    Added support for aggregation measure specification in performance decoration through token-based replay

---
+++ examples/dec_treplay_imdf.py
@@ -0,0 +1,18 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+from pm4py.log.importer.xes import factory as xes_importer
+from pm4py.algo.inductive import factory as inductive_miner
+from pm4py.visualization.petrinet import factory as pn_vis_factory
+
+# import the log
+logPath = os.path.join("..", "tests", "inputData", "receipt.xes")
+log = xes_importer.import_log(logPath)
+# apply Inductive Miner
+net, initial_marking, final_marking = inductive_miner.apply(log)
+# get visualization
+variant = "performance"
+parameters_viz = {"aggregationMeasure":"mean"}
+gviz = pn_vis_factory.apply(net, initial_marking, final_marking, log=log, variant=variant, parameters=parameters_viz)
+gviz.view()
