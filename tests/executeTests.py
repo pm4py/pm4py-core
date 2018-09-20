e4160f9c32390230ad27154e0bf4fcfd99b40ed3
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-20 11:47:03 +0200

    Additional test for trying import everything

--- tests/executeTests.py
+++ tests/executeTests.py
@@ -3,6 +3,7 @@ import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
+from tests.importEverything import Pm4pyImportPackageTest
 from tests.xesImportExportTest import XesImportExportTest
 from tests.csvImportExportTest import CsvImportExportTest
 from tests.alphaMinerTest import AlphaMinerTest
