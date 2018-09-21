00e8b9be15a6dc95c523824b96cd5bcd8694ccb4
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-21 14:10:57 +0200

    Intermediate commit

--- tests/dataframePrefiltering.py
+++ tests/dataframePrefiltering.py
@@ -6,8 +6,8 @@ sys.path.insert(0,parentdir)
 from tests.constants import INPUT_DATA_DIR
 from pm4py.entities.log.adapters.pandas import csv_import_adapter as csv_import_adapter
 from pm4py.entities.log.importer.csv.versions import pandas_df_imp
-from pm4py.algo.filtering.pandas import df_filtering
 from pm4py.algo.filtering.pandas.attributes import attributes_filter
+from pm4py.algo.filtering.pandas.cases import case_filter
 from pm4py.entities.log import transform
 
 
@@ -16,8 +16,8 @@ class DataframePrefilteringTest(unittest.TestCase):
         inputLog = os.path.join(INPUT_DATA_DIR, "running-example.csv")
         dataframe = csv_import_adapter.import_dataframe_from_path_wo_timeconversion(inputLog, sep=',')
         dataframe = attributes_filter.filter_df_keeping_specno_activities(dataframe, activity_key="concept:name")
-        dataframe = df_filtering.filter_df_on_ncases(dataframe, case_id_glue="case:concept:name")
-        #dataframe = df_filtering.filter_df_on_case_length(dataframe, case_id_glue="case:concept:name")
+        dataframe = case_filter.filter_df_on_ncases(dataframe, case_id_glue="case:concept:name")
+        #dataframe = case_filter.filter_df_on_case_length(dataframe, case_id_glue="case:concept:name")
         dataframe = csv_import_adapter.convert_timestamp_columns_in_df(dataframe)
         dataframe = dataframe.sort_values('time:timestamp')
         eventLog = pandas_df_imp.convert_dataframe_to_event_log(dataframe)
