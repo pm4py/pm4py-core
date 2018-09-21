f0128caa59f329d2fb75e3127b96f2021ce9a92c
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-21 16:18:39 +0200

    Added save methods when needed

--- pm4py/util/simple_view.py
+++ pm4py/util/simple_view.py
@@ -14,6 +14,7 @@ from pm4py.visualization.petrinet import factory as pn_vis_factory
 from pm4py.algo.discovery.transition_system import factory as ts_factory
 from pm4py.visualization.transition_system import factory as ts_vis_factory
 from pm4py.algo.discovery.transition_system.parameters import *
+from pm4py.visualization.common.save import *
 
 def apply(original_log, parameters=None):
     """
--- pm4py/visualization/transition_system/factory.py
+++ pm4py/visualization/transition_system/factory.py
@@ -1,4 +1,5 @@
 from pm4py.visualization.transition_system.versions import view_based
+from pm4py.visualization.common.save import *
 
 VIEW_BASED = "view_based"
 WO_DECORATION = "wo_decoration"
