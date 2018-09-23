000798ab981cd430e4173a585c8ac1da9a1a981c
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-23 08:34:38 +0200

    Refactoring of the cases package, for method arguments and position

--- pm4py/algo/filtering/pandas/variants/variants_filter.py
+++ pm4py/algo/filtering/pandas/variants/variants_filter.py
@@ -2,38 +2,6 @@ from pm4py.entities.log.util import xes
 from pm4py.util import constants
 from pm4py.algo.filtering.common import filtering_constants
 
-def get_variants(df, parameters=None):
-    """
-    Get variants from a Pandas dataframe
-
-    Parameters
-    -----------
-    df
-        Dataframe
-    parameters
-        Parameters of the algorithm, including:
-            case_id_glue -> Column that contains the Case ID
-            activity_key -> Column that contains the activity
-            max_variants_to_return -> Maximum number of variants to return
-
-    Returns
-    -----------
-    variants_list
-        List of variants inside the Pandas dataframe
-    """
-    if parameters is None:
-        parameters = {}
-
-    case_id_glue = parameters[constants.PARAMETER_CONSTANT_CASEID_KEY] if constants.PARAMETER_CONSTANT_CASEID_KEY in parameters else filtering_constants.CASE_CONCEPT_NAME
-    activity_key = parameters[constants.PARAMETER_CONSTANT_ACTIVITY_KEY] if constants.PARAMETER_CONSTANT_ACTIVITY_KEY in parameters else xes.DEFAULT_NAME_KEY
-    max_variants_to_return = parameters["max_variants_to_return"] if "max_variants_to_return" in parameters else None
-    variantsDf = df.groupby(case_id_glue)[activity_key].agg({'variant': lambda col: ','.join(col)}).reset_index()
-    variantsList = variantsDf.groupby("variant").agg("count").reset_index().to_dict('records')
-    variantsList = sorted(variantsList, key=lambda x: x[case_id_glue], reverse=True)
-    if max_variants_to_return:
-        variantsList = variantsList[:min(len(variantsList), max_variants_to_return)]
-    return variantsList
-
 def apply(df, admitted_variants, parameters=None):
     """
     Apply a filter on variants
