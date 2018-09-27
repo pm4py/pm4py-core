ea3002dad89e6888b13300e653b8931b7c596d71
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-27 07:44:31 +0200

    Modified methods in order to use the safe Petri net delete transition method

--- pm4py/algo/repair/petri_reduction/versions/tokenbased.py
+++ pm4py/algo/repair/petri_reduction/versions/tokenbased.py
@@ -1,3 +1,5 @@
+from pm4py.entities import petri
+
 def apply(net, parameters=None):
     """
     Apply petri_reduction on the Petrinet removing hidden transitions
@@ -30,10 +32,6 @@ def apply(net, parameters=None):
     for trans in transitions:
         if trans.label is None:
             if not trans in enabledTransInAtLeastOneTrace:
-                for arc in trans.in_arcs:
-                    net.arcs.remove(arc)
-                for arc in trans.out_arcs:
-                    net.arcs.remove(arc)
-                net.transitions.remove(trans)
+                net = petri.utils.remove_transition(net, trans)
 
     return net
