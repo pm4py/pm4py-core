b8b7bb297d4f46ebbe81d96bd0f16d6db75f07bf
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-09-27 07:43:52 +0200

    Removed unused method. Added method to safely delete a transition from the Petri net

--- pm4py/entities/petri/utils.py
+++ pm4py/entities/petri/utils.py
@@ -1,6 +1,36 @@
 from pm4py.entities import petri
 from pm4py.entities.log.util import xes as xes_util
 
+def remove_transition(net, trans):
+    """
+    Remove a transition from a Petri net
+
+    Parameters
+    ----------
+    net
+        Petri net
+    trans
+        Transition to remove
+
+    Returns
+    ----------
+    net
+        Petri net
+    """
+    if trans in net.transitions:
+        print("removing ",trans.name)
+        in_arcs = trans.in_arcs
+        for arc in in_arcs:
+            place = arc.source
+            place.out_arcs.remove(arc)
+            net.arcs.remove(arc)
+        out_arcs = trans.out_arcs
+        for arc in out_arcs:
+            place = arc.target
+            place.in_arcs.remove(arc)
+            net.arcs.remove(arc)
+        net.transitions.remove(trans)
+    return net
 
 def add_arc_from_to(fr, to, net, weight=1):
     '''
