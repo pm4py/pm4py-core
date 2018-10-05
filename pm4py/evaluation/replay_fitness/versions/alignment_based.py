81816dd8d8c2a2b0301478aef08fc3d67446e575
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-10-05 15:05:19 +0200

    Refactoring

--- pm4py/evaluation/replay_fitness/versions/alignment_based.py
+++ pm4py/evaluation/replay_fitness/versions/alignment_based.py
@@ -3,11 +3,7 @@ import multiprocessing as mp
 import pm4py
 from pm4py.entities import log as log_lib
 from pm4py.algo.conformance import alignments
-from pm4py.entities.log.util import xes as xes_util
-
-PARAM_ACTIVITY_KEY = xes_util.DEFAULT_NAME_KEY
-
-PARAMETERS = [PARAM_ACTIVITY_KEY]
+from pm4py import util as pmutil
 
 def evaluate(aligned_traces, parameters=None):
     """
@@ -48,7 +44,7 @@ def apply(log, petri_net, initial_marking, final_marking, parameters=None):
     if parameters is None:
         parameters = {}
     activity_key = parameters[
-        PARAM_ACTIVITY_KEY] if PARAM_ACTIVITY_KEY in parameters else log_lib.util.xes.DEFAULT_NAME_KEY
+        pmutil.constants.PARAMETER_CONSTANT_ACTIVITY_KEY] if pmutil.constants.PARAMETER_CONSTANT_ACTIVITY_KEY in parameters else log_lib.util.xes.DEFAULT_NAME_KEY
     best_worst = pm4py.algo.conformance.alignments.versions.state_equation_a_star.apply(log_lib.log.Trace(), petri_net, initial_marking,
                                                                                         final_marking)
     best_worst_costs = best_worst['cost'] // alignments.utils.STD_MODEL_LOG_MOVE_COST
@@ -77,9 +73,12 @@ def apply_trace(trace, petri_net, initial_marking, final_marking, best_worst, ac
     dictionary: `dict` with keys **alignment**, **cost**, **visited_states**, **queued_states** and **traversed_arcs**
     '''
     alignment = pm4py.algo.conformance.alignments.versions.state_equation_a_star.apply(trace, petri_net, initial_marking, final_marking, {
-        pm4py.algo.conformance.alignments.versions.state_equation_a_star.PARAM_ACTIVITY_KEY: activity_key})
+        pmutil.constants.PARAMETER_CONSTANT_ACTIVITY_KEY: activity_key})
     fixed_costs = alignment['cost'] // alignments.utils.STD_MODEL_LOG_MOVE_COST
-    fitness = 1 - (fixed_costs / best_worst)
+    if best_worst > 0:
+        fitness = 1 - (fixed_costs / best_worst)
+    else:
+        fitness = 1
     return {'trace': trace, 'alignment': alignment['alignment'], 'cost': fixed_costs, 'fitness': fitness,
             'visited_states': alignment['visited_states'], 'queued_states': alignment['queued_states'],
             'traversed_arcs': alignment['traversed_arcs']}
