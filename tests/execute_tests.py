2435d4306f6a9fa06fc55dacb4ae6be6ead7e431
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-10-05 14:34:17 +0200

    Refactored name of tests

---
+++ tests/execute_tests.py
@@ -0,0 +1,30 @@
+import unittest
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+from tests.imp_everything import Pm4pyImportPackageTest
+from tests.xes_impexp_test import XesImportExportTest
+from tests.csv_impexp_test import CsvImportExportTest
+from tests.alpha_test import AlphaMinerTest
+from tests.imdf_test import InductiveMinerDFTest
+from tests.alignment_test import AlignmentTest
+from tests.petri_imp_exp_test import PetriImportExportTest
+from tests.documentationTests.testDocAlpha1 import AlphaMinerDocumentationTest
+from tests.documentationTests.testDocInductive1 import InductiveMinerDocumentationTest
+from tests.documentationTests.testDocDfGraph1 import DfGraphDocumentationTest
+from tests.documentationTests.testDocXes1 import XES1DocumentationTest
+from tests.documentationTests.testDocCsv1 import CSV1DocumentationTest
+from tests.documentationTests.testDocClassifiers1 import Classifiers1DocumentationTest
+from tests.documentationTests.testDocMeasures import DocMeasuresDocumentationTest
+from tests.dataframe_prefilter import DataframePrefilteringTest
+from tests.filtering_test import LogFilteringTest
+from tests.etc_tests import ETCTest
+from tests.petri_log_gen_tests import PetriLogGeneratorTests
+from tests.trans_syst_tests import TransitionSystemTest
+from tests.evaluation_tests import ProcessModelEvaluationTests
+from tests.visual_test import VisualizationTest1
+from tests.case_man_test import CaseManagementTest
+
+if __name__ == "__main__":
+	unittest.main()
