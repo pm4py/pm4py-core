803b6773adced6b3503a4dda0e701ac318c9a491
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-10-09 14:30:47 +0200

    Added utility functions

--- pm4py/algo/discovery/inductive/data_structures/process_tree.py
+++ pm4py/algo/discovery/inductive/data_structures/process_tree.py
@@ -71,6 +71,101 @@ class ProcessTree(object):
         if proceed_to_add:
             self.children.append(trans)
 
+    def get_first_terminal_child_transitions(self):
+        """
+        Gets the list of transitions belonging to the first terminal child node of the current tree
+
+        Returns
+        ---------
+        transitions_list
+            List of transitions belonging to the first terminal child node
+        """
+        if self.children:
+            if type(self.children[0]) is process_tree.ProcessTree:
+                return self.children[0].get_first_terminal_child_transitions()
+            else:
+                return self.children
+
+    def get_last_terminal_child_transitions(self):
+        """
+        Gets the list of transitions belonging to the last terminal child node of the current tree
+
+        Returns
+        ---------
+        transitions_list
+            List of transitions belonging to the first terminal child node
+        """
+        if self.children:
+            if type(self.children[-1]) is process_tree.ProcessTree:
+                return self.children[-1].get_last_terminal_child_transitions()
+            else:
+                return self.children
+
+    def check_initial_loop(self):
+        """
+        Check if the tree, on-the-left, starts with a loop
+
+        Returns
+        ----------
+        boolean
+            True if it starts with an initial loop
+        """
+        if self.children:
+            if type(self.children[0]) is process_tree.ProcessTree:
+                if self.children[0].operator == tree_constants.LOOP_OPERATOR:
+                    return True
+                else:
+                    return self.children[0].check_terminal_loop()
+        return False
+
+    def check_terminal_loop(self):
+        """
+        Check if the tree, on-the-right, ends with a loop
+
+        Returns
+        -----------
+        boolean
+            True if it ends with a terminal loop
+        """
+        if self.children:
+            if type(self.children[-1]) is process_tree.ProcessTree:
+                if self.children[-1].operator == tree_constants.LOOP_OPERATOR:
+                    return True
+                else:
+                    return self.children[-1].check_terminal_loop()
+        return False
+
+    def check_tau_mandatory_at_initial_marking(self):
+        """
+        When a conversion to a Petri net is operated, check if is mandatory to add a hidden transition
+        at initial marking
+
+        Returns
+        ----------
+        boolean
+            Boolean that is true if it is mandatory to add a hidden transition connecting the initial marking
+            to the rest of the process
+        """
+        condition1 = self.check_initial_loop()
+        condition2 = len(self.get_first_terminal_child_transitions()) > 1
+
+        return condition1 or condition2
+
+    def check_tau_mandatory_at_final_marking(self):
+        """
+        When a conversion to a Petri net is operated, check if is mandatory to add a hidden transition
+        at final marking
+
+        Returns
+        ----------
+        boolean
+            Boolean that is true if it is mandatory to add a hidden transition connecting
+            the rest of the process to the final marking
+        """
+        condition1 = self.check_terminal_loop()
+        condition2 = len(self.get_last_terminal_child_transitions()) > 1
+
+        return condition1 or condition2
 
 class PT_Transition(object):
     def __init__(self, name, label):
