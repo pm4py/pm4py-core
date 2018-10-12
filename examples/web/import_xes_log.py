cd28fe835a3d02cdafd96a7ffee8e7086cb138b8
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2018-10-12 17:27:01 +0200

    example code for importing event logs (based on the documentation on the website)

--- examples/web/import_xes_log.py
+++ examples/web/import_xes_log.py
@@ -8,3 +8,17 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfi
 if __name__ == '__main__':
     log_path = os.path.join("..", "..", "tests", "input_data", "running-example.xes")
     log = xes_import_factory.apply(log_path)
+
+    for case_index, case in enumerate(log):
+        print("\n case index: %d  case id: %s" % (case_index, case.attributes["concept:name"]))
+        for event_index, event in enumerate(case):
+            print("event index: %d  event activity: %s" % (event_index, event["concept:name"]))
+
+    parameters = {"timestamp_sort": True}
+
+    log = xes_import_factory.apply(log_path, variant="nonstandard", parameters=parameters)
+
+    for case_index, case in enumerate(log):
+        print("\n case index: %d  case id: %s" % (case_index, case.attributes["concept:name"]))
+        for event_index, event in enumerate(case):
+            print("event index: %d  event activity: %s" % (event_index, event["concept:name"]))
