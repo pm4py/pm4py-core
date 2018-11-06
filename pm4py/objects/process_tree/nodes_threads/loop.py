eebe8ae6f67d020d9fe43cb99869186c575fa2cd
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-11-06 17:14:54 +0100

    Intermediate commit

--- pm4py/objects/process_tree/nodes_threads/loop.py
+++ pm4py/objects/process_tree/nodes_threads/loop.py
@@ -4,9 +4,20 @@ from pm4py.objects.process_tree import process_tree
 
 class Loop(NodeThread):
     def __init__(self, node_object):
+        """
+        Initialize the node thread providing the node object
+
+        Parameters
+        -----------
+        node_object
+            Object related to the node of the process tree
+        """
         NodeThread.__init__(self, node_object)
 
     def run(self):
+        """
+        Runs the current thread (in order to generate the trace)
+        """
         for node in self.node_object.get_children():
             if type(node) is process_tree.PTTransition:
                 if node.label is not None:
