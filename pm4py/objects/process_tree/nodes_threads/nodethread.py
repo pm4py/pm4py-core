2b5c895efe616c2341a4721d618c43345e0a9d75
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-11-06 17:14:54 +0100

    Intermediate commit

--- pm4py/objects/process_tree/nodes_threads/nodethread.py
+++ pm4py/objects/process_tree/nodes_threads/nodethread.py
@@ -15,7 +15,18 @@ class NodeThread(Thread):
         Thread.__init__(self)
 
     def set_concurrent_trace_obj(self, conc_trace_obj):
+        """
+        Sets the concurrent trace object in order to append events to it
+
+        Parameters
+        -----------
+        conc_trace_obj
+            Concurrent trace object
+        """
         self.conc_trace_obj = conc_trace_obj
 
     def run(self):
+        """
+        Runs the current thread (in order to generate the trace)
+        """
         return None
