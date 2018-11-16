fa6177c33488d401d62d0db8015260277253b101
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-11-16 11:52:59 +0100

    Added exponential random variable

---
+++ pm4py/objects/random_variables/exponential/random_variable.py
@@ -0,0 +1,133 @@
+from scipy.stats import expon
+import numpy as np
+import sys
+
+
+class Exponential(object):
+    """
+    Describes a normal variable
+    """
+    def __init__(self, loc=1, scale=1):
+        """
+        Constructor
+
+        Parameters
+        -----------
+        loc
+            Loc of the distribution (see docs.scipy.org/doc/scipy/reference/generated/scipy.stats.expon.html)
+        scale
+            Scale of the distribution
+        """
+        self.loc = loc
+        self.scale = scale
+
+    def read_from_string(self, distribution_parameters):
+        """
+        Initialize distribution parameters from string
+
+        Parameters
+        -----------
+        distribution_parameters
+            Current distribution parameters as exported on the Petri net
+        """
+        self.loc = distribution_parameters.split(";")[0]
+        self.scale = distribution_parameters.split(";")[0]
+
+    def get_transition_type(self):
+        """
+        Get the type of transition associated to the current distribution
+
+        Returns
+        -----------
+        transition_type
+            String representing the type of the transition
+        """
+        return "TIMED"
+
+    def get_distribution_type(self):
+        """
+        Get current distribution type
+
+        Returns
+        -----------
+        distribution_type
+            String representing the distribution type
+        """
+        return "EXPONENTIAL"
+
+    def get_distribution_parameters(self):
+        """
+        Get a string representing distribution parameters
+
+        Returns
+        -----------
+        distribution_parameters
+            String representing distribution parameters
+        """
+        return str(self.loc) + ";" + str(self.scale)
+
+    def calculate_loglikelihood(self, values):
+        """
+        Calculate log likelihood
+
+        Parameters
+        ------------
+        values
+            Empirical values to work on
+
+        Returns
+        ------------
+        likelihood
+            Log likelihood that the values follows the distribution
+        """
+        if len(values) > 1:
+            sum = 0
+            for value in values:
+                sum = sum + np.log(expon.pdf(value, self.loc, self.scale))
+            return sum
+        return -sys.float_info.max
+
+    def calculate_parameters(self, values):
+        """
+        Calculate parameters of the current distribution
+
+        Parameters
+        -----------
+        values
+            Empirical values to work on
+        """
+        if len(values) > 1:
+            self.loc, self.scale = expon.fit(values)
+
+    def get_value(self):
+        """
+        Get a random value following the distribution
+
+        Returns
+        -----------
+        value
+            Value obtained following the distribution
+        """
+        return expon.rvs(self.loc, self.scale)
+
+    def __str__(self):
+        """
+        Returns a representation of the current object
+
+        Returns
+        ----------
+        repr
+            Representation of the current object
+        """
+        return self.get_distribution_type() + " " + self.get_distribution_parameters()
+
+    def __repr__(self):
+        """
+        Returns a representation of the current object
+
+        Returns
+        ----------
+        repr
+            Representation of the current object
+        """
+        return self.get_distribution_type() + " " + self.get_distribution_parameters()
--- tests/random_variables_test.py
+++ tests/random_variables_test.py
@@ -3,6 +3,7 @@ import unittest
 from pm4py.objects.random_variables.normal.random_variable import Normal
 from pm4py.objects.random_variables.random_variable import RandomVariable
 from pm4py.objects.random_variables.uniform.random_variable import Uniform
+from pm4py.objects.random_variables.exponential.random_variable import Exponential
 from pm4py.objects.random_variables.constant0.random_variable import Constant0
 
 
@@ -15,7 +16,7 @@ class RandomVariableTest(unittest.TestCase):
         scale = 32
         tol = 0.15
         U = Uniform(loc=loc, scale=scale)
-        values = [U.get_value() for i in range(100)]
+        values = [U.get_value() for i in range(400)]
         R = RandomVariable()
         R.calculate_parameters(values)
         if not R.get_distribution_type() == "UNIFORM":
@@ -35,7 +36,7 @@ class RandomVariableTest(unittest.TestCase):
         sigma = 4
         tol = 0.15
         N = Normal(mu=mu, sigma=sigma)
-        values = [N.get_value() for i in range(100)]
+        values = [N.get_value() for i in range(400)]
         R = RandomVariable()
         R.calculate_parameters(values)
         if not R.get_distribution_type() == "NORMAL":
@@ -45,6 +46,26 @@ class RandomVariableTest(unittest.TestCase):
         diff_value_mu = abs(mu - mu_R) / (max(abs(mu), abs(mu_R)))
         diff_value_sigma = abs(sigma - sigma_R) / (max(abs(sigma), abs(sigma_R)))
         if diff_value_mu > tol or diff_value_sigma > tol:
+            raise Exception("parameters found outside tolerance")
+
+    def test_exponential_variable(self):
+        # to avoid static method warnings in tests,
+        # that by construction of the unittest package have to be expressed in such way
+        self.dummy_variable = "dummy_value"
+        loc = 53
+        scale = 4
+        tol = 0.2
+        E = Exponential(loc=loc, scale=scale)
+        values = [E.get_value() for i in range(400)]
+        R = RandomVariable()
+        R.calculate_parameters(values)
+        if not R.get_distribution_type() == "EXPONENTIAL":
+            raise Exception("Expected an exponential!")
+        loc_R = R.random_variable.loc
+        scale_R = R.random_variable.scale
+        diff_value_loc = abs(loc - loc_R) / (max(abs(loc), abs(loc_R)))
+        diff_value_scale = abs(scale - scale_R) / (max(abs(scale), abs(scale_R)))
+        if diff_value_loc > tol or diff_value_scale > tol:
             raise Exception("parameters found outside tolerance")
 
     def test_constant0_variable(self):
