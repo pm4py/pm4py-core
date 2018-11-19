e31c44e47a6b8c832c83d5c253e40cd2b6734f4b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-11-19 17:00:19 +0100

    Added support for steady-state analysis from CTMC

--- pm4py/visualization/transition_system/util/visualize_graphviz.py
+++ pm4py/visualization/transition_system/util/visualize_graphviz.py
@@ -8,8 +8,8 @@ def visualize(ts, parameters=None):
         parameters = {}
 
     image_format = parameters["format"] if "format" in parameters else "png"
-    show_labels = parameters["show_labels"] if "show_labels" in parameters else False
-    show_names = parameters["show_names"] if "show_names" in parameters else False
+    show_labels = parameters["show_labels"] if "show_labels" in parameters else True
+    show_names = parameters["show_names"] if "show_names" in parameters else True
 
     filename = tempfile.NamedTemporaryFile(suffix='.gv')
     viz = Digraph(ts.name, filename=filename.name, engine='dot')
