bb9c7600546b06394a227cb6ae9edc4cfada3e74
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-11-19 15:06:34 +0100

    Completely fixed concept of tangible reachability

--- pm4py/objects/stochastic_petri/tangible_reachability.py
+++ pm4py/objects/stochastic_petri/tangible_reachability.py
@@ -14,18 +14,27 @@ def get_tangible_reachability_from_reachability(reach_graph, stochastic_info):
     tangible_reach_graph
         Tangible reachability graph
     """
-    timed_or_hidden_transitions = []
+    timed_transitions = []
     for trans in stochastic_info.keys():
         random_variable = stochastic_info[trans]
         transition_type = random_variable.get_transition_type()
-        if transition_type == "TIMED" or trans.label is None:
-            timed_or_hidden_transitions.append(trans.name)
-    transitions_reach = list(reach_graph.transitions)
-    for t in transitions_reach:
-        if t.name not in timed_or_hidden_transitions:
-            reach_graph.transitions.remove(t)
-            t.from_state.outgoing.remove(t)
-            t.to_state.incoming.remove(t)
+        if transition_type == "TIMED":
+            timed_transitions.append(trans.name)
+    states_reach = list(reach_graph.states)
+    for s in states_reach:
+        state_outgoing_trans = list(s.outgoing)
+        state_ingoing_trans = list(s.incoming)
+        timed_trans_outgoing = [x for x in state_outgoing_trans if x.name in timed_transitions]
+        if not len(state_outgoing_trans) == len(timed_trans_outgoing):
+            for t in state_outgoing_trans:
+                reach_graph.transitions.remove(t)
+                t.from_state.outgoing.remove(t)
+                t.to_state.incoming.remove(t)
+            for t in state_ingoing_trans:
+                reach_graph.transitions.remove(t)
+                t.from_state.outgoing.remove(t)
+                t.to_state.incoming.remove(t)
+            reach_graph.states.remove(s)
     states_reach = list(reach_graph.states)
     for s in states_reach:
         if len(s.incoming) == 0 and len(s.outgoing) == 0:
