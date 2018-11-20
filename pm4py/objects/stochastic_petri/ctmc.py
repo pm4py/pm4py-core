05ab813b07d12ba76b53f20b1207c430ae9c3740
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-11-20 08:04:00 +0100

    Added support for CTMC steady state analysis

--- pm4py/objects/stochastic_petri/ctmc.py
+++ pm4py/objects/stochastic_petri/ctmc.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.linalg import expm
 from collections import Counter
+from numpy.linalg import svd
 
 
 def get_Q_matrix_from_tangible_exponential(tangible_reach_graph, stochastic_info):
@@ -105,3 +106,74 @@ def transient_analysis_from_tangible_q_matrix_and_states_vector(tangible_reach_g
         transient_result[states[i]] = res[0, i]
 
     return transient_result
+
+def nullspace(A, atol=1e-13, rtol=0):
+    """Compute an approximate basis for the nullspace of A.
+
+    The algorithm used by this function is based on the singular value
+    decomposition of `A`.
+
+    Parameters
+    ----------
+    A : ndarray
+        A should be at most 2-D.  A 1-D array with length k will be treated
+        as a 2-D with shape (1, k)
+    atol : float
+        The absolute tolerance for a zero singular value.  Singular values
+        smaller than `atol` are considered to be zero.
+    rtol : float
+        The relative tolerance.  Singular values less than rtol*smax are
+        considered to be zero, where smax is the largest singular value.
+
+    If both `atol` and `rtol` are positive, the combined tolerance is the
+    maximum of the two; that is::
+        tol = max(atol, rtol * smax)
+    Singular values smaller than `tol` are considered to be zero.
+
+    Return value
+    ------------
+    ns : ndarray
+        If `A` is an array with shape (m, k), then `ns` will be an array
+        with shape (k, n), where n is the estimated dimension of the
+        nullspace of `A`.  The columns of `ns` are a basis for the
+        nullspace; each element in numpy.dot(A, ns) will be approximately
+        zero.
+    """
+
+    A = np.atleast_2d(A)
+    u, s, vh = svd(A)
+    tol = max(atol, rtol * s[0])
+    nnz = (s >= tol).sum()
+    ns = vh[nnz:].conj().T
+    return ns
+
+def steadystate_analysis_from_tangible_q_matrix(tangible_reach_graph, Q_matrix, tol=1e-14):
+    """
+    Do steadystate analysis from tangible reachability graph and Q matrix
+
+    Parameters
+    ------------
+    tangible_reach_graph
+        Tangible reachability graph
+    Q_matrix
+        Q matrix
+    tol
+        Tolerance in order to admit states in the steady state
+
+    Returns
+    ------------
+    steadystate
+        Dictionary of states along with their probability in the long term
+    """
+    Q_matrix_trans = np.matrix.transpose(Q_matrix)
+    kernel = np.matrix.transpose(nullspace(Q_matrix_trans))[0]
+    # normalize to 1 the vector of probabilities
+    if (np.sum(kernel) < 0):
+        kernel = -kernel
+    kernel = kernel / np.sum(kernel)
+    states = sorted(list(tangible_reach_graph.states), key=lambda x: x.name)
+    steadystate = {}
+    for i in range(len(states)):
+        if kernel[i] > tol:
+            steadystate[states[i]] = kernel[i]
+    return steadystate
