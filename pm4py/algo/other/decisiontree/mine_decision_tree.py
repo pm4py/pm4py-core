02352c8073a43106f974756534d521d42ec1a8f4
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-11-22 12:01:40 +0100

    Implementation of decision tree mining and visualization

--- pm4py/algo/other/decisiontree/mine_decision_tree.py
+++ pm4py/algo/other/decisiontree/mine_decision_tree.py
@@ -1,6 +1,22 @@
 from sklearn import tree
 
+
 def mine(data, target):
+    """
+    Mine a decision tree
+
+    Parameters
+    ------------
+    data
+        Data from which the decision tree shall be learned
+    target
+        Target classes
+
+    Returns
+    ------------
+    clf
+        Decision tree
+    """
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(data, target)
     return clf
