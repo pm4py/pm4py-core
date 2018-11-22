d1a7481c71b14fbc5b4bf7315d8c6f41d1261cbc
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-11-22 15:32:46 +0100

    Added a cluster algorithm based on log representation, PCA and DBScan clustering algorithm

--- pm4py/algo/other/__init__.py
+++ pm4py/algo/other/__init__.py
@@ -1,1 +1,1 @@
-from pm4py.algo.other import decisiontree
+from pm4py.algo.other import decisiontree, clustering
---
+++ pm4py/algo/other/clustering/factory.py
@@ -0,0 +1,25 @@
+from pm4py.algo.other.clustering.versions import decisiontree
+
+DECISIONTREE = "decisiontree"
+
+VERSIONS = {DECISIONTREE: decisiontree.apply}
+
+def apply(log, parameters=None, variant=DECISIONTREE):
+    """
+    Factory method to apply a trace log clustering algorithm
+
+    Parameters
+    -------------
+    log
+        Trace log object
+    parameters
+        Parameters of the algorithm
+    variant
+        Chosen clustering algorithm
+
+    Returns
+    -----------
+    log_list
+        A list containing, for each cluster, a different log
+    """
+    return VERSIONS[variant](log, parameters=parameters)
