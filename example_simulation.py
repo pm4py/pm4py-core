0813dc5b9b02d39cee88b623f6f52edfd6a76908
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-12-18 17:27:26 +0100

    Added linear programming performance bounds

--- example_simulation.py
+++ example_simulation.py
@@ -6,16 +6,19 @@ from pm4py.objects.stochastic_petri import map as stochastic_map
 from pm4py.statistics.traces.tracelog.case_arrival import get_case_arrival_avg
 from pm4py.visualization.petrinet import factory as pn_vis_factory
 from pm4py.objects.stochastic_petri.lp_perf_bounds import LpPerfBounds
+from pm4py.util.constants import PARAMETER_CONSTANT_ACTIVITY_KEY, PARAMETER_CONSTANT_ATTRIBUTE_KEY
 
 
 log_name = os.path.join("tests", "input_data", "running-example.xes")
 log = xes_importer.apply(log_name)
 # obtain a simple, sound workflow net, containing only visibile unique transitions,
 # applying the Alpha Miner to some of the top variants (methods by Ale)
-net, initial_marking, final_marking = simple_extraction_factory.apply(log, classic_output=True)
+activity_key = "concept:name"
+parameters = {PARAMETER_CONSTANT_ACTIVITY_KEY: activity_key, PARAMETER_CONSTANT_ATTRIBUTE_KEY: activity_key}
+net, initial_marking, final_marking = simple_extraction_factory.apply(log, classic_output=True, parameters=parameters)
 # visualize the output Petri net
 gviz = pn_vis_factory.apply(net, initial_marking, final_marking)
-pn_vis_factory.view(gviz)
+#pn_vis_factory.view(gviz)
 # gets the average time between cases starts
 avg_time_starts = get_case_arrival_avg(log)
 print("avg_time_starts=", avg_time_starts)
@@ -26,4 +29,12 @@ print("smap=", smap)
 perf_bound_obj = LpPerfBounds(net, initial_marking, final_marking, smap, avg_time_starts)
 net1, imarking1, fmarking1 = perf_bound_obj.get_net()
 gviz = pn_vis_factory.apply(net1, imarking1, fmarking1)
-pn_vis_factory.view(gviz)
+#pn_vis_factory.view(gviz)
+
+for var in list(perf_bound_obj.var_corr.keys()):
+    var_corr = perf_bound_obj.var_corr[var]
+
+    minimum = perf_bound_obj.solve_problem(var, maximize=False)
+    maximum = perf_bound_obj.solve_problem(var, maximize=True)
+
+    print(var, minimum.x[var_corr], maximum.x[var_corr])
