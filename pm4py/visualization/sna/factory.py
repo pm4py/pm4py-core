81a2a130f738ebc588cf50374524a08b03f2a0ce
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-10 15:00:50 +0100

    Added SNA visualization through NetworkX

---
+++ pm4py/visualization/sna/factory.py
@@ -0,0 +1,63 @@
+from pm4py.visualization.sna.versions import networkx, pyviz
+
+NETWORKX = "networkx"
+PYVIZ = "pyviz"
+
+VERSIONS_APPLY = {NETWORKX: networkx.apply, PYVIZ: pyviz.apply}
+VERSIONS_VIEW = {NETWORKX: networkx.view, PYVIZ: pyviz.view}
+VERSIONS_SAVE = {NETWORKX: networkx.save, PYVIZ: pyviz.save}
+
+
+def apply(mco, rsc_rsc_matrix, parameters=None, variant=NETWORKX):
+    """
+    Perform SNA visualization starting from the Matrix Container object
+    and the Resource-Resource matrix
+
+    Parameters
+    -------------
+    mco
+        Matrix container object
+    rsc_rsc_matrix
+        Resource-resource matrix
+    parameters
+        Possible parameters of the algorithm, including:
+            weight_threshold: the weight threshold to use in displaying the graph
+            directed: indicates if the graph has to be drawn directed
+            format: format of the output image (png, svg ...)
+
+    Returns
+    -------------
+    temp_file_name
+        Name of a temporary file where the visualization is placed
+    """
+    return VERSIONS_APPLY[variant](mco, rsc_rsc_matrix, parameters=parameters)
+
+
+def view(temp_file_name, parameters=None, variant=NETWORKX):
+    """
+    View the SNA visualization on the screen
+
+    Parameters
+    -------------
+    temp_file_name
+        Temporary file name
+    parameters
+        Possible parameters of the algorithm
+    """
+    return VERSIONS_VIEW[variant](temp_file_name, parameters=parameters)
+
+
+def save(temp_file_name, dest_file, parameters=None, variant=NETWORKX):
+    """
+    Save the SNA visualization from a temporary file to a well-defined destination file
+
+    Parameters
+    -------------
+    temp_file_name
+        Temporary file name
+    dest_file
+        Destination file
+    parameters
+        Possible parameters of the algorithm
+    """
+    return VERSIONS_SAVE[variant](temp_file_name, dest_file, parameters=parameters)
