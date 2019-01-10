df178ddde7f0ec1ba181166be383421f065c4556
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-10 13:27:07 +0100

    Intermediate commit

---
+++ pm4py/algo/sna/metrics/real_handover/versions/classic.py
@@ -0,0 +1,15 @@
+def apply(mco, parameters=None):
+    """
+    Calculate the Real Handover of Work metric
+
+    Parameters
+    ------------
+    mco
+        Matrix container object
+    parameters
+        Parameters of the algorithm
+    """
+    if parameters is None:
+        parameters = {}
+
+    return None
