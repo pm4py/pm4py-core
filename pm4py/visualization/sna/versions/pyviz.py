481044bdcb8866370999d7c38dde78c9fa64d4aa
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-10 15:00:50 +0100

    Added SNA visualization through NetworkX

---
+++ pm4py/visualization/sna/versions/pyviz.py
@@ -0,0 +1,10 @@
+def apply(mco, rsc_rsc_matrix, parameters=None):
+    return None
+
+
+def view(temp_file_name, parameters=None):
+    return None
+
+
+def save(temp_file_name, dest_file, parameters=None):
+    return None
