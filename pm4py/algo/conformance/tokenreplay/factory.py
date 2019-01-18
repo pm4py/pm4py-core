c1ddab3e1dc19ab1bf1c672acabf80bd04b85785
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2019-01-18 13:59:57 +0100

    allow the token based replay factory to work directly on event logs

--- pm4py/algo/conformance/tokenreplay/factory.py
+++ pm4py/algo/conformance/tokenreplay/factory.py
@@ -1,4 +1,7 @@
 from pm4py.algo.conformance.tokenreplay.versions import token_replay
+import pm4py
+from pm4py.objects.log.util import general as log_util
+from pm4py.objects.log import transform as log_transform
 
 TOKEN_REPLAY = "token_replay"
 VERSIONS = {TOKEN_REPLAY: token_replay.apply}
@@ -25,4 +28,17 @@ def apply(log, net, initial_marking, final_marking, parameters=None, variant="to
     variant
         Variant of the algorithm to use
     """
+    if isinstance(log, pm4py.objects.log.log.EventLog) and (not isinstance(log, pm4py.objects.log.log.TraceLog)):
+        parameters = parameters if parameters is not None else dict()
+        if log_util.PARAMETER_KEY_CASE_GLUE in parameters:
+            glue = parameters[log_util.PARAMETER_KEY_CASE_GLUE]
+        else:
+            glue = log_util.CASE_ATTRIBUTE_GLUE
+        if log_util.PARAMETER_KEY_CASE_ATTRIBUTE_PRFIX in parameters:
+            case_pref = parameters[log_util.PARAMETER_KEY_CASE_ATTRIBUTE_PRFIX]
+        else:
+            case_pref = log_util.CASE_ATTRIBUTE_PREFIX
+        log = log_transform.transform_event_log_to_trace_log(log, case_glue=glue,
+                                                                   includes_case_attributes=False,
+                                                                   case_attribute_prefix=case_pref)
     return VERSIONS[variant](log, net, initial_marking, final_marking, parameters=parameters)
