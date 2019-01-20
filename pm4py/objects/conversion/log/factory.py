a39aaf2697311fc39bc5e6941388db12f8c7c600
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-20 12:09:36 +0100

    Adding support for deepcopy log objects when converting

--- pm4py/objects/conversion/log/factory.py
+++ pm4py/objects/conversion/log/factory.py
@@ -1,8 +1,11 @@
 from pm4py.objects.conversion.log.versions import to_event_log, to_trace_log
+from pm4py.objects.conversion.log import constants
 
 
-TO_TRACE_LOG = 'to_trace_log'
-TO_EVENT_LOG = 'to_event_log'
+TO_TRACE_LOG = constants.TO_TRACE_LOG
+TO_EVENT_LOG = constants.TO_EVENT_LOG
+
+DEEPCOPY = constants.DEEPCOPY
 
 VERSIONS = {TO_TRACE_LOG: to_trace_log.apply, TO_EVENT_LOG: to_event_log.apply}
 
---
+++ pm4py/objects/conversion/log/versions/__init__.py
@@ -0,0 +1,1 @@
+from pm4py.objects.conversion.log.versions import to_event_log, to_trace_log
