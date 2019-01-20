33e72c2c8c927201e34cbfee7ec7ccad146e483b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-20 08:06:00 +0100

    Added deployment-testing docker for Python 3.5

--- tests/test_dockers/linux/execute_plain.sh
+++ tests/test_dockers/linux/execute_plain.sh
@@ -1,3 +1,8 @@
+cd python35
+docker build --no-cache -t testdockerlinuxpython35 .
+docker run testdockerlinuxpython35 bash -c "cd pm4py-source-master/tests && python execute_tests.py"
+docker run testdockerlinuxpython35 bash -c "python -c \"import pm4py ; print(pm4py.__version__)\""
+cd ..
 cd python37
 docker build --no-cache -t testdockerlinuxpython37 .
 docker run testdockerlinuxpython37 bash -c "cd pm4py-source-master/tests && python execute_tests.py"
