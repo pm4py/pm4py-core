1ec1a0b7e63ba779d010a35341d6005e3c2feb2d
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-21 13:59:41 +0100

    Added log transformation to a reduced version of the log containing only the events before a specified activity

--- pm4py/algo/other/decisiontree/__init__.py
+++ pm4py/algo/other/decisiontree/__init__.py
@@ -1,1 +1,2 @@
-from pm4py.algo.other.decisiontree import get_class_representation, get_log_representation, mine_decision_tree, split_data
+from pm4py.algo.other.decisiontree import get_class_representation, get_log_representation, log_transforming, \
+    mine_decision_tree, split_data
