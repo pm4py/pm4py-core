273622b9261a441fbdd6240ea9041aeefca05154
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-22 11:48:26 +0100

    Fixed example

--- examples/tree_dec_mining_wo_replay.py
+++ examples/tree_dec_mining_wo_replay.py
@@ -1,9 +1,10 @@
 from pm4py.algo.other.decisiontree.applications import decision_mining_given_activities
 from pm4py.objects.log.importer.xes import factory as xes_importer
+import os
 
 
 def execute_script():
-    log = xes_importer.apply("C:\\running-example.xes")
+    log = xes_importer.apply(os.path.join("..", "tests", "input_data", "running-example.xes"))
     rules = decision_mining_given_activities.get_decision_mining_rules_given_activities(log, ["pay compensation",
                                                                                               "reject request"])
     print("Decision rules = ")
