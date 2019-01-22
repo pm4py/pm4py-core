71637b533e1be4ebd911b30d7924088e7af0965a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-22 11:25:33 +0100

    Added decision mining of the path leading to given activities

--- pm4py/algo/other/decisiontree/mine_decision_tree.py
+++ pm4py/algo/other/decisiontree/mine_decision_tree.py
@@ -1,7 +1,9 @@
 from sklearn import tree
 
+DEFAULT_MAX_REC_DEPTH = 7
 
-def mine(data, target):
+
+def mine(data, target, max_depth=DEFAULT_MAX_REC_DEPTH):
     """
     Mine a decision tree
 
@@ -11,12 +13,15 @@ def mine(data, target):
         Data from which the decision tree shall be learned
     target
         Target classes
+    max_depth
+        Maximum depth of the recursion tree
 
     Returns
     ------------
     clf
         Decision tree
     """
-    clf = tree.DecisionTreeClassifier()
+    clf = tree.DecisionTreeClassifier(max_depth=max_depth)
     clf = clf.fit(data, target)
+
     return clf
