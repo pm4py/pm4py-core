51a1b832b84b857ad75ce48007c521612fb72a0a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-31 11:05:34 +0100

    Added same support to dataframe and log conversions in conformance checking techniques

--- pm4py/algo/conformance/tokenreplay/factory.py
+++ pm4py/algo/conformance/tokenreplay/factory.py
@@ -1,5 +1,8 @@
+from pm4py import util as pmutil
 from pm4py.algo.conformance.tokenreplay.versions import token_replay
 from pm4py.objects.conversion.log import factory as log_converter
+from pm4py.objects.log.util import general as log_util
+from pm4py.objects.log.util import xes as xes_util
 
 TOKEN_REPLAY = "token_replay"
 VERSIONS = {TOKEN_REPLAY: token_replay.apply}
@@ -26,5 +29,13 @@ def apply(log, net, initial_marking, final_marking, parameters=None, variant=TOK
     variant
         Variant of the algorithm to use
     """
+    if parameters is None:
+        parameters = {}
+    if pmutil.constants.PARAMETER_CONSTANT_ACTIVITY_KEY not in parameters:
+        parameters[pmutil.constants.PARAMETER_CONSTANT_ACTIVITY_KEY] = xes_util.DEFAULT_NAME_KEY
+    if pmutil.constants.PARAMETER_CONSTANT_TIMESTAMP_KEY not in parameters:
+        parameters[pmutil.constants.PARAMETER_CONSTANT_TIMESTAMP_KEY] = xes_util.DEFAULT_TIMESTAMP_KEY
+    if pmutil.constants.PARAMETER_CONSTANT_CASEID_KEY not in parameters:
+        parameters[pmutil.constants.PARAMETER_CONSTANT_CASEID_KEY] = log_util.CASE_ATTRIBUTE_GLUE
     return VERSIONS[variant](log_converter.apply(log, parameters, log_converter.TO_TRACE_LOG), net, initial_marking,
                              final_marking, parameters=parameters)
