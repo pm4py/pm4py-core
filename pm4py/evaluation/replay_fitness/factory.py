190e084b30a554ef2394c3d12ca8dc35da4d1897
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-31 11:13:36 +0100

    Added same support to dataframe and log conversions in evaluation techniques

--- pm4py/evaluation/replay_fitness/factory.py
+++ pm4py/evaluation/replay_fitness/factory.py
@@ -1,4 +1,8 @@
+from pm4py import util as pmutil
 from pm4py.evaluation.replay_fitness.versions import alignment_based, token_replay
+from pm4py.objects.conversion.log import factory as log_conversion
+from pm4py.objects.log.util import general as log_util
+from pm4py.objects.log.util import xes as xes_util
 
 ALIGNMENT_BASED = "alignments"
 TOKEN_BASED = "token_replay"
@@ -33,7 +37,17 @@ def apply(log, petri_net, initial_marking, final_marking, parameters=None, varia
     fitness_eval
         Fitness evaluation
     """
-    return VERSIONS[variant](log, petri_net, initial_marking, final_marking, parameters=parameters)
+    if parameters is None:
+        parameters = {}
+    if pmutil.constants.PARAMETER_CONSTANT_ACTIVITY_KEY not in parameters:
+        parameters[pmutil.constants.PARAMETER_CONSTANT_ACTIVITY_KEY] = xes_util.DEFAULT_NAME_KEY
+    if pmutil.constants.PARAMETER_CONSTANT_TIMESTAMP_KEY not in parameters:
+        parameters[pmutil.constants.PARAMETER_CONSTANT_TIMESTAMP_KEY] = xes_util.DEFAULT_TIMESTAMP_KEY
+    if pmutil.constants.PARAMETER_CONSTANT_CASEID_KEY not in parameters:
+        parameters[pmutil.constants.PARAMETER_CONSTANT_CASEID_KEY] = log_util.CASE_ATTRIBUTE_GLUE
+
+    return VERSIONS[variant](log_conversion.apply(log, parameters, log_conversion.TO_TRACE_LOG), petri_net,
+                             initial_marking, final_marking, parameters=parameters)
 
 
 def evaluate(results, parameters="None", variant="token_replay"):
