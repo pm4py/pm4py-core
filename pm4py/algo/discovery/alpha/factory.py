a52183ae8e7b47a7c87ea68a793eef70540e85f9
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-31 10:43:01 +0100

    When a dataframe object is provided to a method that makes use of the conversion, then a trace log or an event log is obtained

--- pm4py/algo/discovery/alpha/factory.py
+++ pm4py/algo/discovery/alpha/factory.py
@@ -1,8 +1,11 @@
+import pandas
+
 from pm4py import util as pmutil
 from pm4py.algo.discovery.alpha import versions
-from pm4py.objects.log.util import xes as xes_util
-
+from pm4py.algo.discovery.dfg.adapters.pandas import df_statistics
 from pm4py.objects.conversion.log import factory as log_conversion
+from pm4py.objects.log.util import xes as xes_util
+from pm4py.objects.log.util import general as log_util
 
 ALPHA_VERSION_CLASSIC = 'classic'
 VERSIONS = {ALPHA_VERSION_CLASSIC: versions.classic.apply}
@@ -38,6 +41,15 @@ def apply(log, parameters=None, variant=ALPHA_VERSION_CLASSIC):
         parameters = {}
     if pmutil.constants.PARAMETER_CONSTANT_ACTIVITY_KEY not in parameters:
         parameters[pmutil.constants.PARAMETER_CONSTANT_ACTIVITY_KEY] = xes_util.DEFAULT_NAME_KEY
+    if pmutil.constants.PARAMETER_CONSTANT_TIMESTAMP_KEY not in parameters:
+        parameters[pmutil.constants.PARAMETER_CONSTANT_TIMESTAMP_KEY] = xes_util.DEFAULT_TIMESTAMP_KEY
+    if pmutil.constants.PARAMETER_CONSTANT_CASEID_KEY not in parameters:
+        parameters[pmutil.constants.PARAMETER_CONSTANT_CASEID_KEY] = log_util.CASE_ATTRIBUTE_GLUE
+    if isinstance(log, pandas.core.frame.DataFrame):
+        dfg = df_statistics.get_dfg_graph(log, case_id_glue=parameters[pmutil.constants.PARAMETER_CONSTANT_CASEID_KEY],
+                                          activity_key=parameters[pmutil.constants.PARAMETER_CONSTANT_ACTIVITY_KEY],
+                                          timestamp_key=parameters[pmutil.constants.PARAMETER_CONSTANT_TIMESTAMP_KEY])
+        return VERSIONS_DFG[variant](dfg, parameters=parameters)
     return VERSIONS[variant](log_conversion.apply(log, parameters, log_conversion.TO_TRACE_LOG), parameters)
 
 
