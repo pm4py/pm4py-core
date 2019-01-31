e748fa70e36663d493b48e2c5d8b9d44e1c3eb29
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-01-31 10:21:59 +0100

    Added from_dataframe conversion utility

--- pm4py/objects/conversion/log/factory.py
+++ pm4py/objects/conversion/log/factory.py
@@ -1,16 +1,16 @@
-from pm4py.objects.conversion.log.versions import to_event_log, to_trace_log, to_dataframe
 from pm4py.objects.conversion.log import constants
-
+from pm4py.objects.conversion.log.versions import to_event_log, to_trace_log, to_dataframe, from_dataframe
 
 TO_TRACE_LOG = constants.TO_TRACE_LOG
 TO_EVENT_LOG = constants.TO_EVENT_LOG
 TO_DATAFRAME = constants.TO_DATAFRAME
+FROM_DATAFRAME = constants.TO_DATAFRAME
 
 DEEPCOPY = constants.DEEPCOPY
 
-VERSIONS = {TO_TRACE_LOG: to_trace_log.apply, TO_EVENT_LOG: to_event_log.apply, TO_DATAFRAME: to_dataframe.apply}
+VERSIONS = {TO_TRACE_LOG: to_trace_log.apply, TO_EVENT_LOG: to_event_log.apply, TO_DATAFRAME: to_dataframe.apply,
+            FROM_DATAFRAME: from_dataframe.apply}
 
 
 def apply(log, parameters=None, variant=TO_TRACE_LOG):
     return VERSIONS[variant](log, parameters=parameters)
-
