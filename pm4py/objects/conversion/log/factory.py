601d8b9f447b6f026de9b9b5382ba1916b20ab85
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2019-02-01 15:10:05 +0100

    target of the conversion defines the name of the version file

--- pm4py/objects/conversion/log/factory.py
+++ pm4py/objects/conversion/log/factory.py
@@ -1,17 +1,15 @@
 from pm4py.objects.conversion.log import constants
-from pm4py.objects.conversion.log.versions import to_event_stream, to_event_log, to_dataframe, from_dataframe
+from pm4py.objects.conversion.log.versions import to_event_stream, to_event_log, to_dataframe
 
 TO_TRACE_LOG = constants.TO_TRACE_LOG
 TO_EVENT_LOG = constants.TO_EVENT_LOG
 TO_EVENT_STREAM = constants.TO_EVENT_STREAM
 TO_DATAFRAME = constants.TO_DATAFRAME
-FROM_DATAFRAME = constants.TO_DATAFRAME
 
 DEEPCOPY = constants.DEEPCOPY
 
 VERSIONS = {TO_TRACE_LOG: to_event_log.apply, TO_EVENT_LOG: to_event_log.apply, TO_EVENT_STREAM: to_event_stream.apply,
-            TO_DATAFRAME: to_dataframe.apply,
-            FROM_DATAFRAME: from_dataframe.apply}
+            TO_DATAFRAME: to_dataframe.apply}
 
 
 def apply(log, parameters=None, variant=TO_EVENT_LOG):
