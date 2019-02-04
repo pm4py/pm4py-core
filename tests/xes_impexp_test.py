25f5f9ba064f601a166ed996d8cea78eeb345b9c
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-02-04 07:49:12 +0100

    Removed old log transform (issue #48)

--- tests/xes_impexp_test.py
+++ tests/xes_impexp_test.py
@@ -2,7 +2,7 @@ from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.objects.log.exporter.xes import factory as xes_exporter
 from pm4py.objects.log.importer.csv import factory as csv_importer
 from pm4py.objects.log.exporter.csv import factory as csv_exporter
-import pm4py.objects.log.transform as log_transform
+from pm4py.objects.conversion.log import factory as log_conv_fact
 from tests.constants import INPUT_DATA_DIR, OUTPUT_DATA_DIR, PROBLEMATIC_XES_DIR, COMPRESSED_INPUT_DATA
 import logging
 import unittest
@@ -26,11 +26,11 @@ class XesImportExportTest(unittest.TestCase):
         # that by construction of the unittest package have to be expressed in such way
         self.dummy_variable = "dummy_value"
         log = xes_importer.import_log(os.path.join(INPUT_DATA_DIR, "running-example.xes"))
-        event_log = log_transform.transform_event_log_to_event_stream(log)
+        event_log = log_conv_fact.apply(log, variant=log_conv_fact.TO_EVENT_STREAM)
         csv_exporter.export(event_log, os.path.join(OUTPUT_DATA_DIR, "running-example-exported.csv"))
         event_log_newimport = csv_importer.import_event_stream(
             os.path.join(OUTPUT_DATA_DIR, "running-example-exported.csv"))
-        log_imported_after_export = log_transform.transform_event_stream_to_event_log(event_log_newimport)
+        log_imported_after_export = log_conv_fact.apply(event_log_newimport)
         self.assertEqual(len(log), len(log_imported_after_export))
         os.remove(os.path.join(OUTPUT_DATA_DIR, "running-example-exported.csv"))
 
