5c04c50cc278a28d11d255ebfe1075bee2428d7b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-02-13 13:35:57 +0100

    Intermediate commit

---
+++ pm4py/algo/other/conceptdrift/versions/decisiontree.py
@@ -0,0 +1,14 @@
+def apply(log, parameters=None):
+    """
+    Apply PCA + DBSCAN clustering, taking into account the control flow perspective but also the other perspectives,
+    in order to possibly detect concept drift
+
+    Parameters
+    -------------
+    log
+        Log
+    parameters
+        Possible parameters of the algorithm
+    """
+
+    return None
