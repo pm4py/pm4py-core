4443a0ee8b8cff2ffe6c4babc9a6e36b361e563a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-02-14 09:33:16 +0100

    Refactoring

--- examples/concept_drift.py
+++ examples/concept_drift.py
@@ -1,7 +1,7 @@
 import os
 
 from pm4py.algo.other.conceptdrift import factory as concept_drift_factory
-from pm4py.algo.other.conceptdrift.utils import get_decision_tree
+from pm4py.algo.other.conceptdrift.utils import get_representation
 from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.visualization.decisiontree import factory as dec_tree_visualization
 
@@ -19,7 +19,7 @@ def execute_script():
         # if the concept drift has been found
 
         # find the decision tree that permits to understand which differences were there
-        clf, feature_names, classes = get_decision_tree.get_decision_tree(logs_list[0], logs_list[1])
+        clf, feature_names, classes = get_representation.get_decision_tree(logs_list[0], logs_list[1])
 
         # represent the decision tree
         gviz = dec_tree_visualization.apply(clf, feature_names, classes, parameters={"format": "svg"})
