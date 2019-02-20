006f30c7fa4d98c62d9211556193b0e63c173967
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-02-20 11:38:04 +0100

    Refactoring

--- tests/dec_tree_test.py
+++ tests/dec_tree_test.py
@@ -1,15 +1,14 @@
 import os
 import unittest
 
-from pm4py.objects.log.util import get_log_representation, get_class_representation
-from pm4py.algo.other.decisiontree import mine_decision_tree
-from pm4py.objects.log.importer.xes import factory as xes_importer
-from pm4py.visualization.decisiontree import factory as dt_vis_factory
+from sklearn import tree
+
 from pm4py.algo.other.clustering import factory as clusterer
 from pm4py.algo.other.conceptdrift import factory as conc_drift_detection_factory
 from pm4py.algo.other.conceptdrift.utils import get_representation
-
-from sklearn import tree
+from pm4py.objects.log.importer.xes import factory as xes_importer
+from pm4py.objects.log.util import get_log_representation, get_class_representation
+from pm4py.visualization.decisiontree import factory as dt_vis_factory
 
 
 class DecisionTreeTest(unittest.TestCase):
