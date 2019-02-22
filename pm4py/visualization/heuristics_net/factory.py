5d35da7a26208a9a41322dbcd956b4f34be4ff7d
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-02-22 08:46:29 +0100

    Intermediate commit

---
+++ pm4py/visualization/heuristics_net/factory.py
@@ -0,0 +1,71 @@
+import os
+import shutil
+import subprocess
+import sys
+
+from pm4py.visualization.heuristics_net.versions import pydotplus
+
+PYDOTPLUS = "pydotplus"
+
+VERSIONS = {PYDOTPLUS: pydotplus.apply}
+
+
+def apply(heu_net, parameters=None, variant=PYDOTPLUS):
+    return VERSIONS[variant](heu_net, parameters=parameters)
+
+
+def view(figure):
+    """
+    View on the screen a figure that has been rendered
+
+    Parameters
+    ----------
+    figure
+        figure
+    """
+    try:
+        filename = figure.name
+        figure = filename
+    except AttributeError:
+        # continue without problems, a proper path has been provided
+        pass
+
+    is_ipynb = False
+
+    try:
+        get_ipython()
+        is_ipynb = True
+    except NameError:
+        pass
+
+    if is_ipynb:
+        from IPython.display import Image
+        return Image(open(figure, "rb").read())
+    else:
+        if sys.platform.startswith('darwin'):
+            subprocess.call(('open', figure))
+        elif os.name == 'nt':  # For Windows
+            os.startfile(figure)
+        elif os.name == 'posix':  # For Linux, Mac, etc.
+            subprocess.call(('xdg-open', figure))
+
+
+def save(figure, output_file_path):
+    """
+    Save a figure that has been rendered
+
+    Parameters
+    -----------
+    figure
+        figure
+    output_file_path
+        Path where the figure should be saved
+    """
+    try:
+        filename = figure.name
+        figure = filename
+    except AttributeError:
+        # continue without problems, a proper path has been provided
+        pass
+
+    shutil.copyfile(figure, output_file_path)
