705ac6c3bee85f3d17e91e59d76a05e333235ecb
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-02-25 09:54:27 +0100

    Added prefix matrix log utility

--- pm4py/objects/log/util/__init__.py
+++ pm4py/objects/log/util/__init__.py
@@ -1,2 +1,2 @@
 from pm4py.objects.log.util import compression, general, insert_classifier, string_to_file, log, xes, sampling, \
-    sorting, index_attribute, get_class_representation, get_log_representation, get_prefixes
+    sorting, index_attribute, get_class_representation, get_log_representation, get_prefixes, prefix_matrix
