07a30bb93ae4aeb913717c358ea8c5aaa2b6c3de
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-03-18 09:40:16 +0100

    Added possibility to force names in transition systems

--- pm4py/visualization/transition_system/util/visualize_graphviz.py
+++ pm4py/visualization/transition_system/util/visualize_graphviz.py
@@ -2,6 +2,8 @@ import tempfile
 
 from graphviz import Digraph
 
+from copy import copy
+
 
 def visualize(ts, parameters=None):
     if parameters is None:
@@ -10,6 +12,13 @@ def visualize(ts, parameters=None):
     image_format = parameters["format"] if "format" in parameters else "png"
     show_labels = parameters["show_labels"] if "show_labels" in parameters else True
     show_names = parameters["show_names"] if "show_names" in parameters else True
+    force_names = parameters["force_names"] if "force_names" in parameters else None
+
+    if force_names:
+        nts = copy(ts)
+        for index, state in enumerate(nts.states):
+            state.name = state.name + " (%.2f)" % (force_names[state])
+        ts = nts
 
     filename = tempfile.NamedTemporaryFile(suffix='.gv')
     viz = Digraph(ts.name, filename=filename.name, engine='dot')
