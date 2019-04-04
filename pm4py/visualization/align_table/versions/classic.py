db3ae9c3c8436d6f56a646ccd41b14d80589c8db
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-04-04 11:13:04 +0200

    Intermediate commit

--- pm4py/visualization/align_table/versions/classic.py
+++ pm4py/visualization/align_table/versions/classic.py
@@ -1,45 +1,42 @@
 from graphviz import Source
-
+from pm4py.algo.filtering.log.variants import variants_filter
 
 def apply(log, aligned_traces, parameters=None):
     if parameters is None:
         parameters = {}
 
+    variants_idx_dict = variants_filter.get_variants_from_log_trace_idx(log, parameters=parameters)
+
+    variants_idx_list = []
+    for variant in variants_idx_dict:
+        variants_idx_list.append((variant, variants_idx_dict[variant]))
+    variants_idx_list = sorted(variants_idx_list, key=lambda x: len(x[1]), reverse=True)
+
     image_format = parameters["format"] if "format" in parameters else "png"
 
-    table_dot = """digraph {
+    table_alignments_list = ["digraph {\n","tbl [\n","shape=plaintext\n","label=<\n"]
+    table_alignments_list.append("<table border='0' cellborder='1' color='blue' cellspacing='0'>\n")
 
-      tbl [
+    table_alignments_list.append("<tr><td>Variant</td><td>Alignment</td></tr>\n")
 
-        shape=plaintext
-        label=<
+    for index, variant in enumerate(variants_idx_list):
+        al_tr = aligned_traces[variant[1][0]]
+        table_alignments_list.append("<tr>")
+        table_alignments_list.append("<td>Variant "+str(index+1)+" ("+str(len(variant[1]))+" occurrences)<br />"+variant[0]+"</td>")
+        table_alignments_list.append("<td><table border='0'><tr>")
+        for move in al_tr['alignment']:
+            move_descr = str(move[1]).replace(">","&gt;")
+            table_alignments_list.append("<td>"+move_descr+"</td>")
+        table_alignments_list.append("</tr></table></td>")
+        table_alignments_list.append("</tr>")
 
-          <table border='0' cellborder='1' color='blue' cellspacing='0'>
-            <tr><td>foo</td><td>bar</td><td>baz</td></tr>
-            <tr><td cellpadding='4'>
-              <table color='orange' cellspacing='0'>
-                <tr><td>one  </td><td>two  </td><td>three</td></tr>
-                <tr><td>four </td><td>five </td><td>six  </td></tr>
-                <tr><td>seven</td><td>eight</td><td>nine </td></tr>
-              </table>
-            </td>
-            <td colspan='2' rowspan='2'>
-              <table color='pink' border='0' cellborder='1' cellpadding='10' cellspacing='0'>
-                <tr><td>eins</td><td>zwei</td><td rowspan='2'>drei<br/>sechs</td></tr>
-                <tr><td>vier</td><td>fünf</td>                             </tr>
-              </table>
-            </td> 
-            </tr>
+    table_alignments_list.append("</table>\n")
+    table_alignments_list.append(">];\n")
+    table_alignments_list.append("}\n")
 
-            <tr><td>abc</td></tr>
+    table_alignments = "".join(table_alignments_list)
 
-          </table>
-
-        >];
-
-    }"""
-
-    gviz = Source(table_dot)
+    gviz = Source(table_alignments)
     gviz.format = image_format
 
     return gviz
