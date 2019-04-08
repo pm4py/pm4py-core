69c5cd9f6ea2f274dd3ab4602c027fc4ca913d38
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-04-08 16:14:32 +0200

    Reservoir sampling case based

--- pm4py/objects/event_store/factory.py
+++ pm4py/objects/event_store/factory.py
@@ -1,10 +1,12 @@
-from pm4py.objects.event_store.versions import sliding_window, reservoir_sampling_event_based
+from pm4py.objects.event_store.versions import sliding_window, reservoir_sampling_event_based, reservoir_sampling_case_based
 
 SLIDING_WINDOW = "sliding_window"
 RESERVOIR_SAMPLING_EVENT_BASED = "reservoir_sampling_event_based"
+RESERVOIR_SAMPLING_CASE_BASED = "reservoir_sampling_case_based"
 
 VERSIONS = {SLIDING_WINDOW: sliding_window.SlidingWindowEventStore,
-            RESERVOIR_SAMPLING_EVENT_BASED: reservoir_sampling_event_based.ReservoirSamplingEventBasedStore}
+            RESERVOIR_SAMPLING_EVENT_BASED: reservoir_sampling_event_based.ReservoirSamplingEventBasedStore,
+            RESERVOIR_SAMPLING_CASE_BASED: reservoir_sampling_case_based.ReservoirSamplingTraceBasedStore}
 
 
 def apply(variant=SLIDING_WINDOW, parameters=None):
--- pm4py/objects/event_store/versions/reservoir_sampling_case_based.py
+++ pm4py/objects/event_store/versions/reservoir_sampling_case_based.py
@@ -1,9 +1,10 @@
+import math
 import random
 import sys
-import math
 
 from pm4py.algo.filtering.common.filtering_constants import CASE_CONCEPT_NAME
 from pm4py.objects.event_store.utils.event_store import EventStore
+from pm4py.objects.log.log import EventLog, Trace
 from pm4py.util import constants
 
 N = "N"
@@ -80,12 +81,27 @@ class ReservoirSamplingTraceBasedStore(EventStore):
         event
             Event
         """
-        """case = event[self.case_glue]
+        case = event[self.case_glue]
+        keys = list(self.case_events.keys())
         if case in self.case_events:
             if len(self.case_events[case]) > self.max_case_length:
-                self.case_events[case].append
-            self.case_events[case].append(event)"""
-        pass
+                event_to_delete = self.case_events[case][0]
+                del self.event_stream[self.event_stream.index(event_to_delete)]
+                del self.case_events[case][0]
+            self.case_events[case].append(event)
+            self.event_stream.append(event)
+        elif len(self.case_events) < self.k:
+            self.case_events[case] = [event]
+            self.event_stream.append(event)
+        else:
+            r = random.randrange(0, len(keys))
+            events_to_delete = self.case_events[keys[r]]
+            for eve in events_to_delete:
+                del self.event_stream[self.event_stream.index(eve)]
+            del self.case_events[keys[r]]
+            self.case_events[case] = []
+            self.case_events[case].append(event)
+            self.event_stream.append(event)
 
     def after_push(self, event):
         """
@@ -98,3 +114,20 @@ class ReservoirSamplingTraceBasedStore(EventStore):
             Event
         """
         pass
+
+    def get_log(self):
+        """
+        Gets the event log from the current reservoir
+
+        Returns
+        ------------
+        event_log
+            Event log
+        """
+        log = EventLog()
+        for trace0 in self.case_events:
+            trace = Trace()
+            for event0 in trace0:
+                trace.append(event0)
+            log.append(trace)
+        return log
