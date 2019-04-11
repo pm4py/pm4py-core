e49ca6f1ff7201b8cb85dbea599e91d2ff3cf9f4
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-04-11 10:55:09 +0200

    Pushing fix for #70

--- pm4py/objects/log/importer/csv/versions/pandas_df_imp.py
+++ pm4py/objects/log/importer/csv/versions/pandas_df_imp.py
@@ -54,12 +54,12 @@ def import_event_stream(path, parameters=None):
 
     df = import_dataframe_from_path(path, sep=sep, quotechar=quotechar, nrows=nrows, sort=sort, sort_field=sort_field,
                                     timest_format=timest_format, timest_columns=timest_columns)
-    event_log = log_conv_fact.apply(df, variant=log_conv_fact.TO_EVENT_STREAM)
+    stream = log_conv_fact.apply(df, variant=log_conv_fact.TO_EVENT_STREAM)
 
     if insert_event_indexes:
-        event_log.insert_event_index_as_event_attribute()
+        stream.insert_event_index_as_event_attribute()
 
-    return event_log
+    return stream
 
 
 def import_log(path, parameters=None):
