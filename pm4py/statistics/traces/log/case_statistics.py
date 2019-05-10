9c52da3991d60f256f4766eb6391add0ea252095
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-05-10 09:50:17 +0200

    Reported from later branches

--- pm4py/statistics/traces/log/case_statistics.py
+++ pm4py/statistics/traces/log/case_statistics.py
@@ -225,6 +225,9 @@ def get_first_quartile_caseduration(log, parameters=None):
     """
     if parameters is None:
         parameters = {}
+
+    parameters["sorted"] = True
+
     duration_values = get_all_casedurations(log, parameters=parameters)
     if duration_values:
         return duration_values[int((len(duration_values) * 3) / 4)]
@@ -249,6 +252,9 @@ def get_median_caseduration(log, parameters=None):
     """
     if parameters is None:
         parameters = {}
+
+    parameters["sorted"] = True
+    
     duration_values = get_all_casedurations(log, parameters=parameters)
     if duration_values:
         return duration_values[int(len(duration_values) / 2)]
