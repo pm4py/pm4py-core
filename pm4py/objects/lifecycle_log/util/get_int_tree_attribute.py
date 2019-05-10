7b7a5501b02adc635e021cd932f41b987dc7f08c
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-05-10 14:30:13 +0200

    Added missing docstring

--- pm4py/objects/lifecycle_log/util/get_int_tree_attribute.py
+++ pm4py/objects/lifecycle_log/util/get_int_tree_attribute.py
@@ -5,6 +5,23 @@ from pm4py.objects.log.util import xes
 
 
 def get_int_tree(log, parameters=None):
+    """
+    Gets a set of interval trees for a particular attribute (one interval tree per value)
+    that aims to show how many intersections (contemporary work) are there for that attribute
+    (activity/resource/station ...)
+
+    Parameters
+    ------------
+    log
+        Interval log (if not, it is automatically converted)
+    parameters
+        Possible parameters of the algorithm
+
+    Returns
+    ------------
+    dict_int_trees
+        Dictionary of interval trees, one for each attribute value
+    """
     if parameters is None:
         parameters = {}
 
