7b86ee1f3d53e10b2724f38134b6072bc7005211
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2019-06-11 16:55:20 +0200

    fix code of alpha example

--- examples/alpha_miner.py
+++ examples/alpha_miner.py
@@ -2,15 +2,16 @@ import os
 
 from pm4py.objects.log.importer.xes import factory as xes_import
 from pm4py.algo.discovery.alpha import factory as alpha_miner
+from pm4py.visualization.petrinet import factory as pn_vis_factory
 
 
 def execute_script():
     log_path = os.path.join("..", "tests", "input_data", "running-example.xes")
     log = xes_import.apply(log_path)
 
-    net, iM, fM = alpha_miner.apply(log)
-    from pm4py.visualization.petrinet import factory as pn_vis_factory
-    gviz = pn_vis_factory.apply(net, iM, fM, parameters={"format": "svg", "debug": True})
+    net, i_m, f_m = alpha_miner.apply(log)
+
+    gviz = pn_vis_factory.apply(net, i_m, f_m, parameters={"format": "svg", "debug": True})
     pn_vis_factory.view(gviz)
 
 
