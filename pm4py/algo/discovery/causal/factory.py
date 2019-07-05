f1e44ecda6aa10869f6e816b5824c1c47dbaa778
Sebastiaan J. van Zelst <s.j.v.zelst@pads.rwth-aachen.de>
2019-07-05 16:15:16 +0200

    add documentation to the 'causal graph factory'

--- pm4py/algo/discovery/causal/factory.py
+++ pm4py/algo/discovery/causal/factory.py
@@ -7,4 +7,19 @@ versions = {CAUSAL_ALPHA: alpha.apply, CAUSAL_HEURISTIC: heuristic.apply}
 
 
 def apply(dfg, variant=CAUSAL_ALPHA):
+    """
+    Computes the causal relation on the basis of a given directly follows graph.
+
+    Parameters
+    -----------
+    dfg
+        Directly follows graph
+    variant
+        Variant of the algorithm to use (classic)
+
+    Returns
+    -----------
+    causal relations
+        dict
+    """
     return versions[variant](dfg)
