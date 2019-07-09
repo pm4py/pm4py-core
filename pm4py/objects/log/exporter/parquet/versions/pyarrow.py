05c5ddca653a2797e680a7c4c0806b81eba92ad3
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-09 13:04:04 +0200

    Added partitioning strategies

--- pm4py/objects/log/exporter/parquet/versions/pyarrow.py
+++ pm4py/objects/log/exporter/parquet/versions/pyarrow.py
@@ -1,5 +1,10 @@
 import pyarrow as pa
 import pyarrow.parquet as pq
+import os
+import shutil
+from pm4py.util import constants
+from pm4py.objects.log.util import xes
+from pm4py.algo.filtering.common.filtering_constants import CASE_CONCEPT_NAME
 
 
 def apply(df, path, parameters=None):
@@ -18,9 +23,28 @@ def apply(df, path, parameters=None):
     if parameters is None:
         parameters = {}
 
+    case_id_key = parameters[
+        constants.PARAMETER_CONSTANT_CASEID_KEY] if constants.PARAMETER_CONSTANT_CASEID_KEY in parameters else CASE_CONCEPT_NAME
+
     compression = parameters["compression"] if "compression" in parameters else "snappy"
+    partition_cols = parameters["partition_cols"] if "partition_cols" in parameters else None
+    auto_partitioning = parameters["auto_partitioning"] if "auto_partitioning" in parameters else False
+    num_partitions = parameters["num_partitions"] if "num_partitions" in parameters else 128
+
+    if partition_cols is None and auto_partitioning:
+        df["@@partitioning"] = df[case_id_key].rank(method='dense', ascending=False).astype(int) % num_partitions
+        partition_cols = ["@@partitioning"]
 
     df.columns = [x.replace(":", "AAA") for x in df.columns]
-    df = pa.Table.from_pandas(df)
-    pq.write_table(df, path, compression=compression)
+    if partition_cols is not None:
+        partition_cols = [x.replace(":", "AAA") for x in partition_cols]
 
+    df = pa.Table.from_pandas(df)
+
+    if partition_cols is not None:
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        os.mkdir(path)
+        pq.write_to_dataset(df, path, compression=compression, partition_cols=partition_cols)
+    else:
+        pq.write_table(df, path, compression=compression)
--- pm4py/objects/log/importer/parquet/factory.py
+++ pm4py/objects/log/importer/parquet/factory.py
@@ -1,4 +1,9 @@
 from pm4py.objects.log.importer.parquet.versions import pyarrow
+import os
+import pandas as pd
+from pm4py.util import constants
+from pm4py.objects.log.util import xes
+from pm4py.algo.filtering.common.filtering_constants import CASE_CONCEPT_NAME
 
 PYARROW = "pyarrow"
 
@@ -26,7 +31,25 @@ def apply(path, parameters=None, variant=PYARROW):
     df
         Pandas dataframe
     """
-    return VERSIONS[variant](path, parameters=parameters)
+    if parameters is None:
+        parameters = {}
+
+    timestamp_key = parameters[
+        constants.PARAMETER_CONSTANT_TIMESTAMP_KEY] if constants.PARAMETER_CONSTANT_TIMESTAMP_KEY in parameters else xes.DEFAULT_TIMESTAMP_KEY
+    case_id_key = parameters[
+        constants.PARAMETER_CONSTANT_CASEID_KEY] if constants.PARAMETER_CONSTANT_CASEID_KEY in parameters else CASE_CONCEPT_NAME
+
+    if os.path.isdir(path):
+        all_parquets = get_list_parquet(path, parameters=parameters)
+        dataframes = []
+        for file in all_parquets:
+            dataframes.append(VERSIONS[variant](file, parameters=parameters))
+        df = pd.concat(dataframes)
+        df = df.sort_values([case_id_key, timestamp_key])
+    else:
+        df = VERSIONS[variant](path, parameters=parameters)
+
+    return df
 
 
 def import_log(path, parameters=None, variant=PYARROW):
@@ -48,7 +71,7 @@ def import_log(path, parameters=None, variant=PYARROW):
     df
         Pandas dataframe
     """
-    return VERSIONS[variant](path, parameters=parameters)
+    return apply(path, variant=variant, parameters=parameters)
 
 
 def import_df(path, parameters=None, variant=PYARROW):
@@ -70,4 +93,39 @@ def import_df(path, parameters=None, variant=PYARROW):
     df
         Pandas dataframe
     """
-    return VERSIONS[variant](path, parameters=parameters)
+    return apply(path, variant=variant, parameters=parameters)
+
+
+def get_list_parquet(path, parameters=None):
+    """
+    Gets the list of Parquets contained into a dataset
+
+    Parameters
+    -------------
+    path
+        Path where the dataset is
+    parameters
+        Possible parameters of the algorithm
+
+    Returns
+    -------------
+    paths
+        List of paths
+    """
+    if parameters is None:
+        parameters = {}
+
+    paths = []
+    subelements = os.listdir(path)
+    for subel in subelements:
+        if os.path.isdir(os.path.join(path, subel)):
+            sub2 = os.listdir(os.path.join(path, subel))
+            for subel2 in sub2:
+                if subel2.endswith(".parquet"):
+                    if os.path.isfile(os.path.join(path, subel, subel2)):
+                        paths.append(os.path.join(path, subel, subel2))
+        else:
+            if subel.endswith(".parquet"):
+                if os.path.isfile(os.path.join(path, subel)):
+                    paths.append(os.path.join(path, subel))
+    return paths
