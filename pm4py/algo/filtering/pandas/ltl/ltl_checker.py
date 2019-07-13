13cffa78b70f925e35b3c0a00019652c05649b0e
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-13 13:38:05 +0200

    A eventually B for Pandas dataframes

--- pm4py/algo/filtering/pandas/ltl/ltl_checker.py
+++ pm4py/algo/filtering/pandas/ltl/ltl_checker.py
@@ -2,8 +2,10 @@ from pm4py.algo.filtering.common.filtering_constants import CASE_CONCEPT_NAME
 from pm4py.objects.log.util.xes import DEFAULT_NAME_KEY
 from pm4py.util.constants import PARAMETER_CONSTANT_ATTRIBUTE_KEY, PARAMETER_CONSTANT_CASEID_KEY
 
+POSITIVE = "positive"
 
-def A_eventually_B(log, A, B, parameters=None):
+
+def A_eventually_B(df, A, B, parameters=None):
     if parameters is None:
         parameters = {}
 
@@ -11,3 +13,23 @@ def A_eventually_B(log, A, B, parameters=None):
         PARAMETER_CONSTANT_CASEID_KEY] if PARAMETER_CONSTANT_CASEID_KEY in parameters else CASE_CONCEPT_NAME
     attribute_key = parameters[
         PARAMETER_CONSTANT_ATTRIBUTE_KEY] if PARAMETER_CONSTANT_ATTRIBUTE_KEY in parameters else DEFAULT_NAME_KEY
+    positive = parameters[POSITIVE] if POSITIVE in parameters else True
+
+    df = df[[case_id_glue, attribute_key]]
+    df["@@index"] = df.index
+    df_A = df[df[attribute_key] == A]
+    df_B = df[df[attribute_key] == B]
+    df_B["@@conceptname"] = df_B[case_id_glue]
+    df_B = df_B.groupby(case_id_glue).last().set_index("@@conceptname")
+
+    df_join = df_A.join(df_B, on=case_id_glue, rsuffix="_2").dropna()
+    df_join["@@diffindex"] = df_join["@@index_2"] - df_join["@@index"]
+    df_join = df_join[df_join["@@diffindex"] > 0]
+
+    i1 = df.set_index(case_id_glue).index
+    i2 = df_join.set_index(case_id_glue).index
+
+    if positive:
+        return df[i1.isin(i2)]
+    else:
+        return df[~i1.isin(i2)]
