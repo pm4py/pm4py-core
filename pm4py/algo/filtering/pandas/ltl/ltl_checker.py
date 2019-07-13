687d89ede4b94d2f56b60f8c703515e17fa7579a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-13 13:53:17 +0200

    Added four eyes principle for Pandas dataframes

--- pm4py/algo/filtering/pandas/ltl/ltl_checker.py
+++ pm4py/algo/filtering/pandas/ltl/ltl_checker.py
@@ -1,11 +1,31 @@
 from pm4py.algo.filtering.common.filtering_constants import CASE_CONCEPT_NAME
-from pm4py.objects.log.util.xes import DEFAULT_NAME_KEY
-from pm4py.util.constants import PARAMETER_CONSTANT_ATTRIBUTE_KEY, PARAMETER_CONSTANT_CASEID_KEY
+from pm4py.objects.log.util.xes import DEFAULT_NAME_KEY, DEFAULT_RESOURCE_KEY
+from pm4py.util.constants import PARAMETER_CONSTANT_ATTRIBUTE_KEY, PARAMETER_CONSTANT_CASEID_KEY, \
+    PARAMETER_CONSTANT_RESOURCE_KEY
 
 POSITIVE = "positive"
 
 
 def A_eventually_B(df, A, B, parameters=None):
+    """
+    Applies the A eventually B rule
+
+    Parameters
+    ------------
+    df
+        Dataframe
+    A
+        A Attribute value
+    B
+        B Attribute value
+    parameters
+        Parameters of the algorithm, including the attribute key
+
+    Returns
+    ------------
+    filtered_df
+        Filtered dataframe
+    """
     if parameters is None:
         parameters = {}
 
@@ -33,3 +53,55 @@ def A_eventually_B(df, A, B, parameters=None):
         return df[i1.isin(i2)]
     else:
         return df[~i1.isin(i2)]
+
+
+def four_eyes_principle(df, A, B, parameters=None):
+    """
+    Verifies the Four Eyes Principle given A and B
+
+    Parameters
+    -------------
+    df
+        Dataframe
+    A
+        A attribute value
+    B
+        B attribute value
+    parameters
+        Parameters of the algorithm, including the attribute key
+
+    Returns
+    --------------
+    filtered_df
+        Filtered dataframe
+    """
+    if parameters is None:
+        parameters = {}
+
+    case_id_glue = parameters[
+        PARAMETER_CONSTANT_CASEID_KEY] if PARAMETER_CONSTANT_CASEID_KEY in parameters else CASE_CONCEPT_NAME
+    attribute_key = parameters[
+        PARAMETER_CONSTANT_ATTRIBUTE_KEY] if PARAMETER_CONSTANT_ATTRIBUTE_KEY in parameters else DEFAULT_NAME_KEY
+    resource_key = parameters[
+        PARAMETER_CONSTANT_RESOURCE_KEY] if PARAMETER_CONSTANT_RESOURCE_KEY in parameters else DEFAULT_RESOURCE_KEY
+    positive = parameters[POSITIVE] if POSITIVE in parameters else True
+
+    df = df[[case_id_glue, attribute_key, resource_key]]
+
+    df_A = df[df[attribute_key] == A]
+    df_B = df[df[attribute_key] == B]
+    df_B["@@conceptname"] = df_B[case_id_glue]
+    df_B = df_B.groupby(case_id_glue).last().set_index("@@conceptname")
+
+    df_join = df_A.join(df_B, on=case_id_glue, rsuffix="_2").dropna()
+    df_join_pos = df_join[df_join[resource_key] == df_join[resource_key + "_2"]]
+    df_join_neg = df_join[df_join[resource_key] != df_join[resource_key + "_2"]]
+
+    i1 = df.set_index(case_id_glue).index
+    i2 = df_join_pos.set_index(case_id_glue).index
+    i3 = df_join_neg.set_index(case_id_glue).index
+
+    if positive:
+        return df[i1.isin(i3) & ~i1.isin(i2)]
+    else:
+        return df[i1.isin(i2)]
