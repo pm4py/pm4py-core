78d16789cba03d94e57b04d54a2bca93b4a03d3c
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-13 13:15:32 +0200

    log A eventually B

--- pm4py/algo/filtering/log/ltl/ltl_checker.py
+++ pm4py/algo/filtering/log/ltl/ltl_checker.py
@@ -3,6 +3,8 @@ from pm4py.objects.log.log import EventLog
 from pm4py.objects.log.util.xes import DEFAULT_NAME_KEY
 from pm4py.util.constants import PARAMETER_CONSTANT_ATTRIBUTE_KEY
 
+POSITIVE = "positive"
+
 
 def A_eventually_B(log, A, B, parameters=None):
     if parameters is None:
@@ -13,3 +15,18 @@ def A_eventually_B(log, A, B, parameters=None):
 
     attribute_key = parameters[
         PARAMETER_CONSTANT_ATTRIBUTE_KEY] if PARAMETER_CONSTANT_ATTRIBUTE_KEY in parameters else DEFAULT_NAME_KEY
+    positive = parameters[POSITIVE] if POSITIVE in parameters else True
+
+    new_log = EventLog()
+
+    for trace in log:
+        occ_A = [i for i in range(len(trace)) if attribute_key in trace[i] and trace[i][attribute_key] == A]
+        occ_B = [i for i in range(len(trace)) if attribute_key in trace[i] and trace[i][attribute_key] == B]
+
+        if len(A) > 0 and len(occ_B) > 0 and occ_A[-1] < occ_B[-1]:
+            if positive:
+                new_log.append(trace)
+        elif not positive:
+            new_log.append(trace)
+
+    return new_log
