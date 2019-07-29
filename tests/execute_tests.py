dd94def5b88e0df706cda0ef950d0ba1fa39a288
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-29 12:01:16 +0200

    Added tests for passed time

--- tests/execute_tests.py
+++ tests/execute_tests.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     parent_dir = os.path.dirname(current_dir)
     sys.path.insert(0, parent_dir)
+    from tests.passed_time import PassedTimeTest
     from tests.ltl_checking_logs import LtlCheckingLogTest
     from tests.ltl_checking_pandas import LtlCheckingPandasTest
     from tests.imp_everything import Pm4pyImportPackageTest
@@ -36,6 +37,7 @@ if __name__ == "__main__":
     from tests.heuminer_test import HeuMinerTest
     from tests.matrix_rep_test import MatrixRepTest
 
+    test_pttime = PassedTimeTest()
     test_ltl_logs = LtlCheckingLogTest()
     test_ltl_pandas = LtlCheckingPandasTest()
     test_rv = RandomVariableTest()
