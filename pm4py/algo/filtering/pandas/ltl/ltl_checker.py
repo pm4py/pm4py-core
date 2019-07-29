8b30aa8bed5a0ca4f0f1cee7d626da52ea1fdc38
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-29 10:54:10 +0200

    Added attr_value_different_persons rule

--- pm4py/algo/filtering/pandas/ltl/ltl_checker.py
+++ pm4py/algo/filtering/pandas/ltl/ltl_checker.py
@@ -230,3 +230,53 @@ def four_eyes_principle(df, A, B, parameters=None):
         return df[i1.isin(i3) & ~i1.isin(i2)]
     else:
         return df[i1.isin(i2)]
+
+
+def attr_value_different_persons(df, A, parameters=None):
+    """
+    Checks whether an attribute value is assumed on events done by different resources
+
+    Parameters
+    ------------
+    df
+        Dataframe
+    A
+        A attribute value
+    parameters
+        Parameters of the algorithm, including the attribute key and the positive parameter:
+            - if True, then filters all the cases containing occurrences of A done by different resources
+            - if False, then filters all the cases not containing occurrences of A done by different resources
+
+    Returns
+    -------------
+    filtered_df
+        Filtered dataframe
+    """
+    if parameters is None:
+        parameters = {}
+
+    case_id_glue = parameters[
+        PARAMETER_CONSTANT_CASEID_KEY] if PARAMETER_CONSTANT_CASEID_KEY in parameters else CASE_CONCEPT_NAME
+    attribute_key = parameters[
+        PARAMETER_CONSTANT_ATTRIBUTE_KEY] if PARAMETER_CONSTANT_ATTRIBUTE_KEY in parameters else DEFAULT_NAME_KEY
+    resource_key = parameters[
+        PARAMETER_CONSTANT_RESOURCE_KEY] if PARAMETER_CONSTANT_RESOURCE_KEY in parameters else DEFAULT_RESOURCE_KEY
+    positive = parameters[POSITIVE] if POSITIVE in parameters else True
+
+    df = df[[case_id_glue, attribute_key, resource_key]]
+
+    df_A = df[df[attribute_key] == A]
+    df_B = df[df[attribute_key] == A]
+    df_B["@@conceptname"] = df_B[case_id_glue]
+    df_B = df_B.groupby(case_id_glue).last().set_index("@@conceptname")
+
+    df_join = df_A.join(df_B, on=case_id_glue, rsuffix="_2").dropna()
+    df_join_neg = df_join[df_join[resource_key] != df_join[resource_key + "_2"]]
+
+    i1 = df.set_index(case_id_glue).index
+    i2 = df_join_neg.set_index(case_id_glue).index
+
+    if positive:
+        return df[i1.isin(i2)]
+    else:
+        return df[~i1.isin(i2)]
