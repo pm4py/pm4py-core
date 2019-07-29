04e4b806b61e4171f86b68aa92fe0102ac3a6fa7
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-29 11:45:17 +0200

    Intermediate commit

--- pm4py/statistics/passed_time/pandas/factory.py
+++ pm4py/statistics/passed_time/pandas/factory.py
@@ -1,9 +1,10 @@
-from pm4py.statistics.passed_time.pandas.versions import pre, post
+from pm4py.statistics.passed_time.pandas.versions import pre, post, prepost
 
 PRE = 'pre'
 POST = 'post'
+PREPOST = 'prepost'
 
-VERSIONS = {PRE: pre.apply, POST: post.apply}
+VERSIONS = {PRE: pre.apply, POST: post.apply, PREPOST: prepost.apply}
 
 
 def apply(df, activity, variant=PRE, parameters=None):
