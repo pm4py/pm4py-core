eead65c1b77b7dcbf3c098ac373bec0002333392
Alessandro Berti <javert@northwood.northwood.net>
2019-09-17 15:16:48 +0200

    Check the simplicity approach implemented there. It is not corresponding with the literature. An implementation corresponding to the literature has been proposed.

--- pm4py/evaluation/simplicity/versions/arc_degree.py
+++ pm4py/evaluation/simplicity/versions/arc_degree.py
@@ -1,3 +1,5 @@
+from statistics import mean
+
 def apply(petri_net, parameters=None):
     """
     Gets simplicity from a Petri net
@@ -20,9 +22,32 @@ def apply(petri_net, parameters=None):
     """
     if parameters is None:
         parameters = {}
-    str(parameters)
-    arc_degree = 0.0
-    if len(petri_net.transitions) > 0:
-        arc_degree = float(len(petri_net.places)) / float(len(petri_net.transitions))
-    simplicity = 1.0 / (1.0 + arc_degree)
+    #str(parameters)
+    #arc_degree = 0.0
+    #if len(petri_net.transitions) > 0:
+    #    arc_degree = float(len(petri_net.places)) / float(len(petri_net.transitions))
+    #simplicity = 1.0 / (1.0 + arc_degree)
+
+    # TODO: verify the real provenence of the approach before!
+
+    all_arc_degrees = []
+    for place in petri_net.places:
+        all_arc_degrees.append(len(place.in_arcs) + len(place.out_arcs))
+    for trans in petri_net.transitions:
+        all_arc_degrees.append(len(trans.in_arcs) + len(trans.out_arcs))
+
+    # original model: we have plenty of choices there.
+    # one choice is about taking a model containing the most frequent variant,
+    # along with a short circuit between the final and the initial marking.
+    # in that case, the average arc degree of the "original model" is 2
+
+    # or take as original model the one containing only the transitions, in that case
+    # the average arc degree of the original model is 0.
+
+    # let's stick with 2
+
+    mean_degree = mean(all_arc_degrees) if all_arc_degrees else 0.0
+
+    simplicity = 1.0 / (1.0 + (mean_degree - 2))
+
     return simplicity
