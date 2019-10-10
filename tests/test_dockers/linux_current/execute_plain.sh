0019240ee6881a8c8255f3e74b64e53eb1a2bf47
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-10-10 08:01:47 +0200

    Removing Python35 compatibility :(

--- tests/test_dockers/linux_current/execute_plain.sh
+++ tests/test_dockers/linux_current/execute_plain.sh
@@ -1,8 +1,3 @@
-cd python35
-docker build --no-cache -t testdockerlinuxpython35develop .
-docker run testdockerlinuxpython35develop bash -c "cd pm4py-source-develop/tests && python execute_tests.py"
-docker run testdockerlinuxpython35develop bash -c "python -c \"import pm4py ; print(pm4py.__version__)\""
-cd ..
 cd python37
 docker build --no-cache -t testdockerlinuxpython37develop .
 docker run testdockerlinuxpython37develop bash -c "cd pm4py-source-develop/tests && python execute_tests.py"
