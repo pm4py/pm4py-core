b49198803ba676319839891a8e2c890dd61e2c2e
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-10-10 11:20:30 +0200

    Trial

--- exampleaa.py
+++ exampleaa.py
@@ -1,3 +1,6 @@
+import psutil
+import ray
+
 import heapq
 import sys
 from copy import copy
@@ -29,8 +32,15 @@ from pm4py.algo.conformance.alignments.versions.state_equation_a_star import DEF
 from pm4py.algo.conformance.alignments.versions.state_equation_a_star import PARAMETERS
 from pm4py.algo.conformance.alignments.versions.state_equation_a_star import apply_from_variants_list_petri_string
 
-import ray
+from pm4py.algo.conformance import alignments as ali
+from pm4py.algo.conformance.alignments import factory as align_factory
+import math
 
+num_cpus = psutil.cpu_count(logical=False)
+
+ray.init(num_cpus=num_cpus)
+
+@ray.remote
 def apply_from_variants_list_ray(var_list, petri_net_string, parameters=None):
     """
     Apply the alignments from the specification of a list of variants in the log
@@ -52,3 +62,84 @@ def apply_from_variants_list_ray(var_list, petri_net_string, parameters=None):
 
     res = apply_from_variants_list_petri_string(var_list, petri_net_string, parameters=parameters)
     return res
+
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def apply_log_ray(log, petri_net, initial_marking, final_marking, parameters=None, version=align_factory.VERSION_STATE_EQUATION_A_STAR):
+    if parameters is None:
+        parameters = dict()
+    activity_key = parameters[
+        PARAMETER_CONSTANT_ACTIVITY_KEY] if PARAMETER_CONSTANT_ACTIVITY_KEY in parameters else DEFAULT_NAME_KEY
+    model_cost_function = parameters[
+        PARAM_MODEL_COST_FUNCTION] if PARAM_MODEL_COST_FUNCTION in parameters else None
+    sync_cost_function = parameters[
+        PARAM_SYNC_COST_FUNCTION] if PARAM_SYNC_COST_FUNCTION in parameters else None
+    if model_cost_function is None or sync_cost_function is None:
+        # reset variables value
+        model_cost_function = dict()
+        sync_cost_function = dict()
+        for t in petri_net.transitions:
+            if t.label is not None:
+                model_cost_function[t] = ali.utils.STD_MODEL_LOG_MOVE_COST
+                sync_cost_function[t] = 0
+            else:
+                model_cost_function[t] = 1
+
+    parameters[pm4py.util.constants.PARAMETER_CONSTANT_ACTIVITY_KEY] = activity_key
+    parameters[
+        PARAM_MODEL_COST_FUNCTION] = model_cost_function
+    parameters[
+        PARAM_SYNC_COST_FUNCTION] = sync_cost_function
+    best_worst_cost = align_factory.VERSIONS_COST[version](petri_net, initial_marking, final_marking, parameters=parameters)
+
+    variants_idxs = parameters[align_factory.VARIANTS_IDX] if align_factory.VARIANTS_IDX in parameters else None
+    if variants_idxs is None:
+        variants_idxs = align_factory.variants_module.get_variants_from_log_trace_idx(log, parameters=parameters)
+    variants_list = [[x, len(y)] for x, y in variants_idxs.items()]
+
+    petri_net_string = align_factory.petri_exporter.export_petri_as_string(petri_net, initial_marking, final_marking)
+
+    n = math.ceil(len(variants_list)/num_cpus)
+
+    variants_list_split = list(chunks(variants_list, n))
+
+    results = ray.get([apply_from_variants_list_ray.remote(x, petri_net_string) for x in variants_list_split])
+    print(results)
+    input()
+
+    al_idx = {}
+    for index, el in enumerate(variants_list_split):
+        for index2, var_item in enumerate(el):
+            variant = var_item[0]
+            for trace_idx in variants_idxs[variant]:
+                al_idx[trace_idx] = results[index][variant]
+
+    alignments = []
+    for i in range(len(log)):
+        alignments.append(al_idx[i])
+
+    # assign fitness to traces
+    for index, align in enumerate(alignments):
+        unfitness_upper_part = align['cost'] // ali.utils.STD_MODEL_LOG_MOVE_COST
+        if unfitness_upper_part == 0:
+            align['fitness'] = 1
+        elif (len(log[index]) + best_worst_cost) > 0:
+            align['fitness'] = 1 - (
+                    (align['cost'] // ali.utils.STD_MODEL_LOG_MOVE_COST) / (len(log[index]) + best_worst_cost))
+        else:
+            align['fitness'] = 0
+
+    return alignments
+
+if __name__ == "__main__":
+    import pm4pycvxopt
+    from pm4py.objects.log.importer.xes import factory as xes_importer
+    from pm4py.algo.discovery.inductive import factory as inductive_miner
+    from pm4py.algo.conformance.alignments import factory as alignments_factory
+
+    log = xes_importer.apply("tests/input_data/running-example.xes")
+    net, im, fm = inductive_miner.apply(log)
+    apply_log_ray(log, net, im, fm)
