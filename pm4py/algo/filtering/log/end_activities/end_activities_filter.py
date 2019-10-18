8306ef47712895d5c59781942129b88f4636bcde
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-10-18 08:30:49 +0200

    Bug fix

--- pm4py/algo/filtering/log/end_activities/end_activities_filter.py
+++ pm4py/algo/filtering/log/end_activities/end_activities_filter.py
@@ -29,7 +29,7 @@ def apply(log, admitted_end_activities, parameters=None):
     attribute_key = parameters[
         PARAMETER_CONSTANT_ACTIVITY_KEY] if PARAMETER_CONSTANT_ACTIVITY_KEY in parameters else DEFAULT_NAME_KEY
 
-    filtered_log = [trace for trace in log if trace and trace[-1][attribute_key] in admitted_end_activities]
+    filtered_log = EventLog([trace for trace in log if trace and trace[-1][attribute_key] in admitted_end_activities])
     return filtered_log
 
 
