71c11279895852c8d521de66baf0d3ccf278288a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-10-18 13:03:54 +0200

    First version of the simulation

--- pm4py/algo/simulation/montecarlo/factory.py
+++ pm4py/algo/simulation/montecarlo/factory.py
@@ -1,8 +1,8 @@
-from pm4py.algo.simulation.montecarlo.versions import petri_arrival_time
+from pm4py.algo.simulation.montecarlo.versions import petri_arrival_time_max_1t
 
-PETRI_ARRIVAL_TIME = 'petri_arrival_time'
+PETRI_ARRIVAL_TIME_MAX_1T = 'petri_arrival_time_max_1t'
 
-VERSIONS = {PETRI_ARRIVAL_TIME: petri_arrival_time.apply}
+VERSIONS = {PETRI_ARRIVAL_TIME_MAX_1T: petri_arrival_time_max_1t.apply}
 
-def apply(log, net, im, fm, variant=PETRI_ARRIVAL_TIME, parameters=None):
+def apply(log, net, im, fm, variant=PETRI_ARRIVAL_TIME_MAX_1T, parameters=None):
     return VERSIONS[variant](log, net, im, fm, parameters=parameters)
