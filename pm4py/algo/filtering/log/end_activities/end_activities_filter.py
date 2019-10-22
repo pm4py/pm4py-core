474c21931b9e203cc0908ae929a103c7f88c238a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-10-22 07:57:17 +0200

    Hotfixing problem with filtering

--- pm4py/algo/filtering/log/end_activities/end_activities_filter.py
+++ pm4py/algo/filtering/log/end_activities/end_activities_filter.py
@@ -30,7 +30,7 @@ def apply(log, admitted_end_activities, parameters=None):
         PARAMETER_CONSTANT_ACTIVITY_KEY] if PARAMETER_CONSTANT_ACTIVITY_KEY in parameters else DEFAULT_NAME_KEY
 
     filtered_log = [trace for trace in log if trace and trace[-1][attribute_key] in admitted_end_activities]
-    return filtered_log
+    return EventLog(filtered_log)
 
 
 def get_end_activities(log, parameters=None):
