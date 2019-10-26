814b31c01b0372ea5ca3f70a62c22f4269e5b41a
Alessandro Berti <javert@northwood.northwood.net>
2019-10-26 09:30:20 +0200

    Adding deps

--- tests/test_dockers/linux_stablewopip/execute_plain.sh
+++ tests/test_dockers/linux_stablewopip/execute_plain.sh
@@ -4,9 +4,9 @@
 #docker run testdockerlinuxpython38master bash -c "python -c \"import pm4py ; print(pm4py.__version__)\""
 #cd ..
 cd python35
-docker build --no-cache -t testdockerlinuxpython35develop .
-docker run testdockerlinuxpython35develop bash -c "cd pm4py-source-master/tests && python execute_tests.py"
-docker run testdockerlinuxpython35develop bash -c "python -c \"import pm4py ; print(pm4py.__version__)\""
+docker build --no-cache -t testdockerlinuxpython35master .
+docker run testdockerlinuxpython35master bash -c "cd pm4py-source-master/tests && python execute_tests.py"
+docker run testdockerlinuxpython35master bash -c "python -c \"import pm4py ; print(pm4py.__version__)\""
 cd ..
 cd python37
 docker build --no-cache -t testdockerlinuxpython37master .
