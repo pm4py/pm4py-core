5da3c6706b7615a1b692eca8128a9a90e9394b04
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-11-08 07:01:15 +0100

    Removed dijkstra_heu_a_star version (useless in comparison to the other two)

--- pm4py/algo/conformance/alignments/versions/__init__.py
+++ pm4py/algo/conformance/alignments/versions/__init__.py
@@ -1,1 +1,1 @@
-from pm4py.algo.conformance.alignments.versions import state_equation_a_star, dijkstra_heu_a_star, dijkstra_no_heuristics
+from pm4py.algo.conformance.alignments.versions import state_equation_a_star, dijkstra_no_heuristics
