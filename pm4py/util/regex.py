753dc600ffc0da361bfbe81241cf5ceff8ca4928
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-11-15 07:06:02 +0100

    Refactoring

--- pm4py/util/regex.py
+++ pm4py/util/regex.py
@@ -1,3 +1,40 @@
+import re
+
+def check_reg_matching(reg, stru):
+    """
+    Check if a regular expression matches a given string
+
+    Parameters
+    -------------
+    reg
+        Regular expression
+    stru
+        String
+
+    Returns
+    -------------
+    boolean
+        Matches or not?
+    """
+    if type(reg) is str:
+        reg = re.compile(reg)
+
+    match = re.match(reg, stru)
+
+    if match is not None:
+        stru = str(match)
+        if "match='" in stru:
+            stru = str(match).split("match='")[1].split("'")[0]
+        elif "match=\"" in stru:
+            stru = str(match).split("match=\"")[1].split("\"")[0]
+        else:
+            raise Exception("match not contained in the match")
+        if len(stru) == 0:
+            return False
+        return True
+    else:
+        return False
+
 def get_new_char(label, shared_obj):
     """
     Get a new single character describing the activity, for the regex
