1996a2901b3c9a323bd2ac885d18701e16fd500d
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-11-19 09:47:43 +0100

    Serialization/deserialization of Pandas dataframes

--- pm4py/objects/log/deserialization/versions/pyarrow_event_log.py
+++ pm4py/objects/log/deserialization/versions/pyarrow_event_log.py
@@ -8,3 +8,9 @@ def apply(bytes, parameters=None):
 def import_from_file(file_path, parameters=None):
     if parameters is None:
         parameters = {}
+
+    F = open(file_path, "rb")
+    bytes = F.read()
+    F.close()
+
+    return apply(bytes, parameters=parameters)
