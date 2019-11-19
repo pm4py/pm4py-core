256f6edc3ff9c357d07142bdf6570030cc42a6af
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-11-19 10:15:54 +0100

    Serialization/deserialization using Pyarrow of event logs

--- pm4py/objects/log/deserialization/versions/pyarrow_event_log.py
+++ pm4py/objects/log/deserialization/versions/pyarrow_event_log.py
@@ -1,8 +1,19 @@
 import pyarrow
+from pm4py.objects.log.log import EventLog, Trace, Event
 
 def apply(bytes, parameters=None):
     if parameters is None:
         parameters = {}
+
+    buffer = pyarrow.py_buffer(bytes)
+    list_objs = pyarrow.deserialize(buffer)
+    log = EventLog(attributes=list_objs[0], extensions=list_objs[1], omni_present=list_objs[2], classifiers=list_objs[3])
+    for i in range(len(list_objs[4])):
+        trace = Trace(attributes=list_objs[4][i])
+        for j in range(len(list_objs[5][i])):
+            trace.append(Event(list_objs[5][i][j]))
+        log.append(trace)
+    return log
 
 
 def import_from_file(file_path, parameters=None):
--- pm4py/objects/log/serialization/versions/pyarrow_event_log.py
+++ pm4py/objects/log/serialization/versions/pyarrow_event_log.py
@@ -5,6 +5,16 @@ def apply(log, parameters=None):
     if parameters is None:
         parameters = {}
 
+    list_objs = []
+    list_objs.append(log.attributes)
+    list_objs.append(log.extensions)
+    list_objs.append(log.omni_present)
+    list_objs.append(log.classifiers)
+    list_objs.append([trace.attributes for trace in log])
+    list_objs.append([[dict(y) for y in trace._list] for trace in log])
+
+    return pyarrow.serialize(list_objs).to_buffer().to_pybytes()
+
 
 def export_to_file(log, file_path, parameters=None):
     if parameters is None:
