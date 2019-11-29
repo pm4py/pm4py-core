fea69fe06610627c1e4b4e4f58100fcbfe54bd33
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2019-11-29 16:26:41 +0100

    updating version information

--- pm4py/__init__.py
+++ pm4py/__init__.py
@@ -4,9 +4,9 @@ time.clock = time.process_time
 
 from pm4py import algo, evaluation, objects, util, visualization, statistics, streaming
 
-__version__ = '1.2.6'
+__version__ = '1.2.7'
 __doc__ = "Process Mining for Python"
 __author__ = 'Fraunhofer Institute for Applied Technology'
-__author_email__ = 'pm4py@pads.rwth-aachen.de'
+__author_email__ = 'pm4py@fit.fraunhofer.de'
 __maintainer__ = 'Fraunhofer Institute for Applied Technology'
-__maintainer_email__ = "pm4py@pads.rwth-aachen.de"
+__maintainer_email__ = "pm4py@fit.fraunhofer.de"
