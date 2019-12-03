108521c570d1e85418754077ecdf2b57ec42d70a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-12-03 14:27:10 +0100

    Fixed Align-ETConformance approach

--- pm4py/evaluation/precision/versions/align_etconformance.py
+++ pm4py/evaluation/precision/versions/align_etconformance.py
@@ -43,9 +43,10 @@ def apply(log, net, marking, final_marking, parameters=None):
     sum_ee = 0
     sum_at = 0
 
-    if not (petri.check_soundness.check_wfnet(net) and petri.check_soundness.check_relaxed_soundness_net_in_fin_marking(net,
-                                                                                                            marking,
-                                                                                                            final_marking)):
+    if not (petri.check_soundness.check_wfnet(net) and petri.check_soundness.check_relaxed_soundness_net_in_fin_marking(
+            net,
+            marking,
+            final_marking)):
         raise Exception("trying to apply Align-ETConformance on a Petri net that is not a relaxed sound workflow net!!")
 
     prefixes, prefix_count = precision_utils.get_log_prefixes(log, activity_key=activity_key)
@@ -58,11 +59,14 @@ def apply(log, net, marking, final_marking, parameters=None):
     for i in range(len(all_markings)):
         atm = all_markings[i]
 
-        log_transitions = set(prefixes[prefixes_keys[i]])
-        activated_transitions_labels = set(x.label for x in utils.get_visible_transitions_eventually_enabled_by_marking(net, atm) if x.label is not None)
-        sum_at += len(activated_transitions_labels) * prefix_count[prefixes_keys[i]]
-        escaping_edges = activated_transitions_labels.difference(log_transitions)
-        sum_ee += len(escaping_edges) * prefix_count[prefixes_keys[i]]
+        if atm is not None:
+            log_transitions = set(prefixes[prefixes_keys[i]])
+            activated_transitions_labels = set(
+                x.label for x in utils.get_visible_transitions_eventually_enabled_by_marking(net, atm) if
+                x.label is not None)
+            sum_at += len(activated_transitions_labels) * prefix_count[prefixes_keys[i]]
+            escaping_edges = activated_transitions_labels.difference(log_transitions)
+            sum_ee += len(escaping_edges) * prefix_count[prefixes_keys[i]]
 
     # fix: also the empty prefix should be counted!
     start_activities = set(start_activities_filter.get_start_activities(log, parameters=parameters))
@@ -100,19 +104,23 @@ def transform_markings_from_sync_to_original_net(markings0, net, parameters=None
     if parameters is None:
         parameters = {}
 
-    places_corr = {p.name:p for p in net.places}
+    places_corr = {p.name: p for p in net.places}
 
     markings = []
 
     for i in range(len(markings0)):
         res = markings0[i]
         atm = petri.petrinet.Marking()
-        for pl, count in res.items():
-            if pl[0] == utils.SKIP:
-                atm[places_corr[pl[1]]] = count
-        markings.append(atm)
+        if res is not None:
+            for pl, count in res.items():
+                if pl[0] == utils.SKIP:
+                    atm[places_corr[pl[1]]] = count
+            markings.append(atm)
+        else:
+            markings.append(None)
 
     return markings
+
 
 def align_fake_log_stop_marking(fake_log, net, marking, final_marking, parameters=None):
     """
@@ -139,7 +147,6 @@ def align_fake_log_stop_marking(fake_log, net, marking, final_marking, parameter
     """
     if parameters is None:
         parameters = {}
-    max_trace_length = max(len(x) for x in fake_log)
     align_result = []
     for i in range(len(fake_log)):
         trace = fake_log[i]
@@ -150,15 +157,19 @@ def align_fake_log_stop_marking(fake_log, net, marking, final_marking, parameter
                 stop_marking[pl] = count
         cost_function = utils.construct_standard_cost_function(sync_net, utils.SKIP)
 
-        res = __search(sync_net, sync_initial_marking, sync_final_marking, stop_marking, cost_function, utils.SKIP, max_trace_length)
-        res2 = {}
+        res = __search(sync_net, sync_initial_marking, sync_final_marking, stop_marking, cost_function, utils.SKIP)
 
-        for pl in res:
-            res2[(pl.name[0], pl.name[1])] = res[pl]
+        if res is not None:
+            res2 = {}
+            for pl in res:
+                res2[(pl.name[0], pl.name[1])] = res[pl]
 
-        align_result.append(res2)
+            align_result.append(res2)
+        else:
+            align_result.append(None)
 
     return align_result
+
 
 def build_sync_net(trace, petri_net, initial_marking, final_marking, parameters=None):
     """
@@ -194,7 +205,7 @@ def build_sync_net(trace, petri_net, initial_marking, final_marking, parameters=
     return sync_prod, sync_initial_marking, sync_final_marking
 
 
-def __search(sync_net, ini, fin, stop, cost_function, skip, max_trace_length):
+def __search(sync_net, ini, fin, stop, cost_function, skip):
     from pm4py.objects.petri.utils import decorate_places_preset_trans, decorate_transitions_prepostset
 
     decorate_transitions_prepostset(sync_net)
@@ -223,11 +234,10 @@ def __search(sync_net, ini, fin, stop, cost_function, skip, max_trace_length):
         h_cvx = matrix(h_cvx)
         cost_vec = matrix(cost_vec)
 
-    h, x = utils.__compute_exact_heuristic_new_version(sync_net, a_matrix, h_cvx, g_matrix, cost_vec, incidence_matrix, ini,
-                                                 fin_vec, lp_solver_factory.DEFAULT_LP_SOLVER_VARIANT, use_cvxopt=use_cvxopt)
-    # heuristics need to be adapted for prefix alignments
-    # here we make the heuristics way less powerful
-    h = h / (max_trace_length + 1.0)
+    h, x = utils.__compute_exact_heuristic_new_version(sync_net, a_matrix, h_cvx, g_matrix, cost_vec, incidence_matrix,
+                                                       ini,
+                                                       fin_vec, lp_solver_factory.DEFAULT_LP_SOLVER_VARIANT,
+                                                       use_cvxopt=use_cvxopt)
     ini_state = utils.SearchTuple(0 + h, 0, h, ini, None, None, x, True)
     open_set = [ini_state]
     heapq.heapify(open_set)
@@ -247,11 +257,9 @@ def __search(sync_net, ini, fin, stop, cost_function, skip, max_trace_length):
 
         while not curr.trust:
             h, x = utils.__compute_exact_heuristic_new_version(sync_net, a_matrix, h_cvx, g_matrix, cost_vec,
-                                                         incidence_matrix, curr.m,
-                                                         fin_vec, lp_solver_factory.DEFAULT_LP_SOLVER_VARIANT, use_cvxopt=use_cvxopt)
-            # heuristics need to be adapted for prefix alignments
-            # here we make the heuristics way less powerful
-            h = h / (max_trace_length + 1.0)
+                                                               incidence_matrix, curr.m,
+                                                               fin_vec, lp_solver_factory.DEFAULT_LP_SOLVER_VARIANT,
+                                                               use_cvxopt=use_cvxopt)
 
             # 11/10/19: shall not a state for which we compute the exact heuristics be
             # by nature a trusted solution?
@@ -284,8 +292,9 @@ def __search(sync_net, ini, fin, stop, cost_function, skip, max_trace_length):
 
         enabled_trans = [t for t in possible_enabling_transitions if t.sub_marking <= current_marking]
 
-        trans_to_visit_with_cost = [(t, cost_function[t]) for t in enabled_trans if not (
-                curr.t is not None and utils.__is_log_move(curr.t, skip) and utils.__is_model_move(t, skip))]
+        trans_to_visit_with_cost = [(t, cost_function[t]) for t in enabled_trans if
+                                    not (t is None or utils.__is_log_move(t, skip) or (
+                                                utils.__is_model_move(t, skip) and not t.label[1] is None))]
 
         for t, cost in trans_to_visit_with_cost:
             traversed += 1
@@ -297,9 +306,6 @@ def __search(sync_net, ini, fin, stop, cost_function, skip, max_trace_length):
 
             queued += 1
             h, x = utils.__derive_heuristic(incidence_matrix, cost_vec, curr.x, t, curr.h)
-            # heuristics need to be adapted for prefix alignments
-            # here we make the heuristics way less powerful
-            h = h / (max_trace_length + 1.0)
 
             trustable = utils.__trust_solution(x)
             new_f = g + h
