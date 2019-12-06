ab54e61785d731881169275ce9b022f9ecf61390
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-12-06 07:23:56 +0100

    Proposed fix for #63

--- pm4py/evaluation/precision/factory.py
+++ pm4py/evaluation/precision/factory.py
@@ -3,16 +3,15 @@ from pm4py.evaluation.precision.versions import etconformance_token, align_etcon
 from pm4py.objects.conversion.log import factory as log_conversion
 from pm4py.objects.log.util import general as log_util
 from pm4py.objects.log.util import xes as xes_util
+from pm4py.objects import petri
+
 
 ETCONFORMANCE_TOKEN = "etconformance"
 ALIGN_ETCONFORMANCE = "align_etconformance"
 
 VERSIONS = {ETCONFORMANCE_TOKEN: etconformance_token.apply, ALIGN_ETCONFORMANCE: align_etconformance.apply}
 
-DEFAULT_VARIANT = ETCONFORMANCE_TOKEN
-
-
-def apply(log, net, marking, final_marking, parameters=None, variant=DEFAULT_VARIANT):
+def apply(log, net, marking, final_marking, parameters=None, variant=None):
     """
     Factory method to apply ET Conformance
 
@@ -41,5 +40,20 @@ def apply(log, net, marking, final_marking, parameters=None, variant=DEFAULT_VAR
     if pmutil.constants.PARAMETER_CONSTANT_CASEID_KEY not in parameters:
         parameters[pmutil.constants.PARAMETER_CONSTANT_CASEID_KEY] = log_util.CASE_ATTRIBUTE_GLUE
 
-    return VERSIONS[variant](log_conversion.apply(log, parameters, log_conversion.TO_EVENT_LOG), net, marking,
+    log = log_conversion.apply(log, parameters, log_conversion.TO_EVENT_LOG)
+
+    # execute the following part of code when the variant is not specified by the user
+    if variant is None:
+        if not (petri.check_soundness.check_wfnet(
+                net) and petri.check_soundness.check_relaxed_soundness_net_in_fin_marking(
+                net,
+                marking,
+                final_marking)):
+            # in the case the net is not a relaxed sound workflow net, we must apply token-based replay
+            variant = ETCONFORMANCE_TOKEN
+        else:
+            # otherwise, use the align-etconformance approach (safer, in the case the model contains duplicates)
+            variant = ALIGN_ETCONFORMANCE
+
+    return VERSIONS[variant](log, net, marking,
                              final_marking, parameters=parameters)
