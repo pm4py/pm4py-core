ddd1fa39b15a1b2ea5695eadc901599c1e899581
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-01-29 09:50:05 +0100

    Fixing decomposition

--- pm4py/objects/petri/decomposition.py
+++ pm4py/objects/petri/decomposition.py
@@ -1,12 +1,10 @@
 import networkx as nx
 from pm4py.objects.petri.petrinet import PetriNet, Marking
 from pm4py.objects.petri.utils import add_arc_from_to
-from pm4py.objects.log.log import Trace
-from pm4py.util import constants
-from pm4py.objects.log.util import xes
+import hashlib
 
 
-def get_graph_components(places, inv_trans, trans_dup_label):
+def get_graph_components(places, inv_trans, trans_dup_label, tmap):
     G = nx.Graph()
     for x in places:
         G.add_node(x)
@@ -14,6 +12,13 @@ def get_graph_components(places, inv_trans, trans_dup_label):
         G.add_node(x)
     for x in trans_dup_label:
         G.add_node(x)
+    for x in trans_dup_label:
+        i = 0
+        while i < len(tmap[x])-1:
+            j = i + 1
+            while j < len(tmap[x]):
+                G.add_edge(tmap[x][i].name, tmap[x][j].name)
+                j = j + 1
     all_inserted_val = set(places.values()).union(inv_trans.values()).union(trans_dup_label.values())
     for v1 in all_inserted_val:
         for arc in v1.out_arcs:
@@ -35,7 +40,7 @@ def decompose(net, im, fm):
             tmap[t.label].append(t)
     trans_dup_label = {x.name: x for x in net.transitions if x.label is not None and len(tmap[x.label]) > 1}
     trans_labels = {x.name: x.label for x in net.transitions}
-    conn_comp = get_graph_components(places, inv_trans, trans_dup_label)
+    conn_comp = get_graph_components(places, inv_trans, trans_dup_label, tmap)
     list_nets = []
     for cmp in conn_comp:
         net_new = PetriNet("")
@@ -71,7 +76,10 @@ def decompose(net, im, fm):
                     im_new[lmap[el]] = im[old_place]
                 if old_place in fm:
                     fm_new[lmap[el]] = fm[old_place]
-        lvis_labels = [t.label for t in net_new.transitions if t.label is not None]
+        lvis_labels = sorted([t.label for t in net_new.transitions if t.label is not None])
+        t_tuple = tuple(sorted(list(int(hashlib.md5(t.name.encode('utf-8')).hexdigest(), 16) for t in net_new.transitions)))
         net_new.lvis_labels = lvis_labels
+        net_new.t_tuple = t_tuple
+
         list_nets.append((net_new, im_new, fm_new))
     return list_nets
