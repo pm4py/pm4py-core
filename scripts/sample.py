d79dc3482f9d2d94b85924851855b3ea2e0de5a9
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2020-02-14 18:04:09 +0100

    simplify the sample script

--- scripts/sample.py
+++ scripts/sample.py
@@ -6,16 +6,8 @@ Use the scripts folder to write custom scripts for your (research) project, when
 
 
 def pm4py_demo_script():
-    pt1 = pt_util.parse('->(\'a\',\'b\')')
-    pt2 = pt_util.parse('->(\'a\',\'b\')')
-    pt3 = pt_util.parse('X(\'b\',->(\'a\',->(\'a\', X(\'b\', tau))))')
-
-    print(pt1 == pt2)
-    print(pt2 == pt3)
-    print(pt1 == pt3)
-    print(hash(pt1))
-    print(hash(pt2))
-    print(hash(pt3))
+    pt1 = pt_util.parse('X(\'a\',\'b\')')
+    print(pt1)
 
 
 if __name__ == '__main__':
