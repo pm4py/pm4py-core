f9e48dddf3f908a1def32a9a9bd70e16bfc2d399
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-02-15 12:31:07 +0100

    Fixing tests

--- tests/inductive_other_tests.py
+++ tests/inductive_other_tests.py
@@ -1,18 +1,9 @@
-import inspect
-import os
-import sys
-import unittest
-
-current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
-
 import unittest
 from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.objects.log.importer.csv import factory as csv_importer
 from pm4py.algo.discovery.inductive import factory as inductive_miner
 from pm4py.statistics.variants.log import get as log_variants
-from pm4py.statistics.traces.pandas import case_statistics as pd_case_statistics
+from pm4py.statistics.variants.pandas import get as pd_variants
 import os
 
 
@@ -21,48 +12,57 @@ class InductiveMinerOtherTests(unittest.TestCase):
         df = csv_importer.import_dataframe_from_path(os.path.join("input_data", "running-example.csv"))
         net, im, fm = inductive_miner.apply(df, variant=inductive_miner.DFG_BASED)
 
+
     def test_dataframe_dfg_based_old(self):
         df = csv_importer.import_dataframe_from_path(os.path.join("input_data", "running-example.csv"))
         net, im, fm = inductive_miner.apply(df, variant=inductive_miner.DFG_BASED_OLD_VERSION)
+
 
     def test_log_variants_dfg_based(self):
         log = xes_importer.apply(os.path.join("input_data", "running-example.xes"))
         net, im, fm = inductive_miner.apply_variants(log_variants.get_variants(log),
                                                      variant=inductive_miner.DFG_BASED)
 
+
     def test_log_tree_variants_dfg_based(self):
         log = xes_importer.apply(os.path.join("input_data", "running-example.xes"))
         tree = inductive_miner.apply_tree_variants(log_variants.get_variants(log),
                                                    variant=inductive_miner.DFG_BASED)
 
+
     def test_df_variants_dfg_based(self):
         df = csv_importer.import_dataframe_from_path(os.path.join("input_data", "running-example.csv"))
-        net, im, fm = inductive_miner.apply_variants(pd_case_statistics.get_variant_statistics(df),
+        net, im, fm = inductive_miner.apply_variants(pd_variants.get_variants_set(df),
                                                      variant=inductive_miner.DFG_BASED)
+
 
     def test_df_tree_variants_dfg_based(self):
         df = csv_importer.import_dataframe_from_path(os.path.join("input_data", "running-example.csv"))
-        tree = inductive_miner.apply_tree_variants(pd_case_statistics.get_variant_statistics(df),
+        tree = inductive_miner.apply_tree_variants(pd_variants.get_variants_set(df),
                                                    variant=inductive_miner.DFG_BASED)
+
 
     def test_log_variants_dfg_based_old(self):
         log = xes_importer.apply(os.path.join("input_data", "running-example.xes"))
         net, im, fm = inductive_miner.apply_variants(log_variants.get_variants(log),
                                                      variant=inductive_miner.DFG_BASED_OLD_VERSION)
 
+
     def test_log_tree_variants_dfg_based_old(self):
         log = xes_importer.apply(os.path.join("input_data", "running-example.xes"))
         tree = inductive_miner.apply_tree_variants(log_variants.get_variants(log),
                                                    variant=inductive_miner.DFG_BASED_OLD_VERSION)
 
+
     def test_df_variants_dfg_based_old(self):
         df = csv_importer.import_dataframe_from_path(os.path.join("input_data", "running-example.csv"))
-        net, im, fm = inductive_miner.apply_variants(pd_case_statistics.get_variant_statistics(df),
+        net, im, fm = inductive_miner.apply_variants(pd_variants.get_variants_set(df),
                                                      variant=inductive_miner.DFG_BASED_OLD_VERSION)
+
 
     def test_df_tree_variants_dfg_based_old(self):
         df = csv_importer.import_dataframe_from_path(os.path.join("input_data", "running-example.csv"))
-        tree = inductive_miner.apply_tree_variants(pd_case_statistics.get_variant_statistics(df),
+        tree = inductive_miner.apply_tree_variants(pd_variants.get_variants_set(df),
                                                    variant=inductive_miner.DFG_BASED_OLD_VERSION)
 
 
