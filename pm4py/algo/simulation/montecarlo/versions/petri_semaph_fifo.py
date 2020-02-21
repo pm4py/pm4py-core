04a930af2043fbaf9ee981356b3fc0c071ffb86b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-02-21 09:53:26 +0100

    docs(pm4py): Improving docs related to the Monte Carlo simulation

--- pm4py/algo/simulation/montecarlo/versions/petri_semaph_fifo.py
+++ pm4py/algo/simulation/montecarlo/versions/petri_semaph_fifo.py
@@ -35,6 +35,12 @@ DEFAULT_SMALL_SCALE_FACTOR = 864000.0
 # 1 min to finish a thread
 DEFAULT_MAX_THREAD_EXECUTION_TIME = 60.0
 
+OUTPUT_PLACES_INTERVAL_TREES = "places_interval_trees"
+OUTPUT_TRANSITIONS_INTERVAL_TREES = "transitions_interval_trees"
+OUTPUT_CASES_EX_TIME = "cases_ex_time"
+OUTPUT_MEDIAN_CASES_EX_TIME = "median_cases_ex_time"
+OUTPUT_CASE_ARRIVAL_RATIO = "input_case_arrival_ratio"
+OUTPUT_TOTAL_CASES_TIME = "total_cases_time"
 
 class SimulationDiagnostics(Thread):
     def __init__(self, sim_thread):
@@ -181,7 +187,6 @@ class SimulationThread(Thread):
                 if rem_time == 0:
                     break
                 if sem_value == 0:
-                    # FIFO: we assume that the first worked 'piece' is also the first to finish
                     waiting_time = max(waiting_time,
                                        place.assigned_time.pop(
                                            0) - current_time) if place.assigned_time else waiting_time
@@ -223,10 +228,12 @@ class SimulationThread(Thread):
                 p_ex_time = place.assigned_time.pop(0)
                 if current_time - p_ex_time > 0:
                     places_interval_trees[place].add(Interval(p_ex_time, current_time))
-                    sleep((current_time - p_ex_time) / self.small_scale_factor)
                 place.assigned_time.append(current_time)
                 place.assigned_time = sorted(place.assigned_time)
                 place.semaphore.release()
+
+            # sleep before starting next iteration
+            sleep((waiting_time + execution_time) / self.small_scale_factor)
 
         if first_event is not None and last_event is not None:
             cases_ex_time.append(last_event[xes_constants.DEFAULT_TIMESTAMP_KEY].timestamp() - first_event[
@@ -254,27 +261,50 @@ class SimulationThread(Thread):
 
 def apply(log, net, im, fm, parameters=None):
     """
-    Performs a Monte Carlo simulation of a Petri net
+    Performs a Monte Carlo simulation of an accepting Petri net without duplicate transitions and where the preset is always
+    distinct from the postset (FIFO variant; the semaphores pile up if waiting is needed, and the first in is the first to win
+    the semaphore)
 
     Parameters
     -------------
     log
         Event log
     net
-        Petri net
+        Accepting Petri net without duplicate transitions and where the preset is always distinct from the postset
     im
         Initial marking
     fm
         Final marking
     parameters
-        Parameters of the algorithm
+        Parameters of the algorithm:
+            PARAM_NUM_SIMULATIONS => (default: 100)
+            PARAM_FORCE_DISTRIBUTION => Force a particular stochastic distribution (e.g. normal) when the stochastic map
+            is discovered from the log (default: None; no distribution is forced)
+            PARAM_ENABLE_DIAGNOSTICS => Enable the printing of diagnostics (default: True)
+            PARAM_DIAGN_INTERVAL => Interval of time in which diagnostics of the simulation are printed (default: 32)
+            PARAM_CASE_ARRIVAL_RATIO => Case arrival of new cases (default: None; inferred from the log)
+            PARAM_PROVIDED_SMAP => Stochastic map that is used in the simulation (default: None; inferred from the log)
+            PARAM_MAP_RESOURCES_PER_PLACE => Specification of the number of resources available per place
+            (default: None; each place gets the default number of resources)
+            PARAM_DEFAULT_NUM_RESOURCES_PER_PLACE => Default number of resources per place when not specified
+            (default: 1; each place gets 1 resource and has to wait for the resource to finish)
+            PARAM_SMALL_SCALE_FACTOR => Scale factor for the sleeping time of the actual simulation
+            (default: 864000.0, 10gg)
+            PARAM_MAX_THREAD_EXECUTION_TIME => Maximum execution time per thread (default: 60.0, 1 minute)
 
     Returns
     ------------
     simulated_log
         Simulated event log
     simulation_result
-        Result of the simulation
+        Result of the simulation:
+            OUTPUT_PLACES_INTERVAL_TREES => inteval trees that associate to each place the times in which it was occupied.
+            OUTPUT_TRANSITIONS_INTERVAL_TREES => interval trees that associate to each transition the intervals of time
+            in which it could not fire because some token was in the output.
+            OUTPUT_CASES_EX_TIME => Throughput time of the cases included in the simulated log
+            OUTPUT_MEDIAN_CASES_EX_TIME => Median of the throughput times
+            OUTPUT_CASE_ARRIVAL_RATIO => Case arrival ratio that was specified in the simulation
+            OUTPUT_TOTAL_CASES_TIME => Total time occupied by cases of the simulated log
     """
     if parameters is None:
         parameters = {}
@@ -374,7 +404,7 @@ def apply(log, net, im, fm, parameters=None):
 
     transitions_interval_trees = {t.name: y for t, y in transitions_interval_trees.items()}
 
-    return log, {"places_interval_trees": places_interval_trees,
-                 "transitions_interval_trees": transitions_interval_trees, "cases_ex_time": cases_ex_time,
-                 "median_cases_ex_time": median(cases_ex_time), "input_case_arrival_ratio": case_arrival_ratio,
-                 "total_time": max_timestamp - min_timestamp}
+    return log, {OUTPUT_PLACES_INTERVAL_TREES: places_interval_trees,
+                 OUTPUT_TRANSITIONS_INTERVAL_TREES: transitions_interval_trees, OUTPUT_CASES_EX_TIME: cases_ex_time,
+                 OUTPUT_MEDIAN_CASES_EX_TIME: median(cases_ex_time), OUTPUT_CASE_ARRIVAL_RATIO: case_arrival_ratio,
+                 OUTPUT_TOTAL_CASES_TIME: max_timestamp - min_timestamp}
