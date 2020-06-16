a373460aff7298cf4729872dcb0c3c32b08f4762
Adam Peter Banham <n7176546@qut.edu.au>
2020-06-16 13:58:55 +1000

    updated function descriptions and type hinting for function signatures

--- pm4py/algo/discovery/alpha/versions/classic_support.py
+++ pm4py/algo/discovery/alpha/versions/classic_support.py
@@ -1,24 +1,51 @@
 import pm4py.algo.discovery.alpha.versions.classic as alpha_classic
 
-from typing import TypeVar
+from typing import TypeVar,List
 from multiprocessing import Pool
 from os import cpu_count
 from math import ceil
 
 ClassicAlphaAbstraction = TypeVar('ClassicAlphaAbstraction')
+Key = TypeVar('Key')
+Value = TypeVar('Value')
 
 def find_alpha_pair(
     left_pair:tuple,
-    right_pairs:list,
-    parallel_relation:list,
-    causal_relation:list,
-    alpha_parallel_relation_starts:list,
-    alpha_causal_relation_starts:list,
-    alpha_parallel_relation_ends:list,
-    alpha_causal_relation_ends:list
-    ) -> list:
+    right_pairs:List[tuple],
+    parallel_relation:List[tuple],
+    causal_relation:List[tuple],
+    alpha_parallel_relation_starts:List[Key],
+    alpha_causal_relation_starts:List[Key],
+    alpha_parallel_relation_ends:List[Value],
+    alpha_causal_relation_ends:List[Value]
+    ) -> List[tuple]:
+    """
+    find_alpha_pair
+    ----------------
+    Creates alpha pairs for the Alpha Miner (classic) based on the give relations,
+    left side and a collection of possible right pairs
 
-    new_rights = []
+    Parameters
+    ------------
+    left_pair\n
+        \tConsidered left pair (tuple)\n
+    right_pairs\n
+        \tPossible joins for left_pair (list of tuples)\n
+    parallel_relation\n
+        \tList of tuples from ClassicAlphaAbstraction.parallel_relation\n
+    causal_relation\n
+        \tList of tuples from ClassicAlphaAbstraction.causal_relation\n
+    alpha_parallel_relation_[starts([0])/ends([1])]\n
+        \tList of key/values from ClassicAlphaAbstraction.parallel_relation\n
+    alpha_causal_relation_[starts([0])/ends([1])]\n
+        \tList of key/values from ClassicAlphaAbstraction.causal_relation\n
+
+    Returns
+    -------
+    new_rights :: List of tuples\n
+        \tA new list of possible pair combinations to be added to pairs, not filtered
+    """
+    new_pairs = []
     for right_pair in right_pairs:
         if left_pair[0].issubset(right_pair[0]) or left_pair[1].issubset(right_pair[1]):
                         if not (
@@ -34,16 +61,36 @@ def find_alpha_pair(
                             alpha_parallel_relation_starts,alpha_causal_relation_starts,
                             alpha_parallel_relation_ends,alpha_causal_relation_ends)
                             ):
-                                new_rights.append(
+                                new_pairs.append(
                                      (left_pair[0] | right_pair[0], left_pair[1] | right_pair[1])
                                 )
-    return new_rights
+    return new_pairs
 
 def run_batch_alpha_pairs(
     t1:tuple,
-    pairs:list,
+    pairs:List[tuple],
     alpha_abstraction:ClassicAlphaAbstraction,
-    ) -> list :
+    ) -> List[tuple] :
+        """
+        run_batch_alpha_pairs
+        -----------
+        Generates new set of alpha pairs by paritioning work over a process pool of workers (CPU_CORES - 1 of machine)
+        then returns the new alpha pairs list for the next iteration of focused left pair (t1).
+
+        Parameters
+        ------------
+        t1 :: tuple\n
+            \tThe current focused `left_pair` to join on\n
+        pairs :: List[tuple]\n
+            \tThe current set of alpha pairs from iterations to attempt to join t1 to.
+        alpha_abstraction :: ClassicAlphaAbstraction\n
+            \tThe data structure abstraction for this selection of traces.
+
+        Returns
+        --------
+        pairs :: List[tuple]\n
+            \t A new list of unique alpha pairs ready for the next iteration of left pair (t1).
+        """
         # make starts and ends for relations
         alpha_parallel_relation_starts = [
         start 
