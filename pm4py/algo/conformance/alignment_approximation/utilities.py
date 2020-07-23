ab49da75e15cb7ef5d6eb910e8251ca991de4a25
schuster-daniel <daniel.schuster@fit.fraunhofer.de>
2020-07-23 17:34:17 +0200

    refactoring alignment approximation

--- pm4py/algo/conformance/alignment_approximation/utilities.py
+++ pm4py/algo/conformance/alignment_approximation/utilities.py
@@ -3,7 +3,6 @@ from pm4py.algo.conformance.alignments.algorithm import Variants
 from pm4py.objects.conversion.process_tree.factory import apply as convert_pt_to_petri_net
 from pm4py.objects.log.log import Trace, Event
 from pm4py.objects.process_tree.process_tree import ProcessTree
-from pm4py.objects.process_tree.util import is_leaf
 from typing import Set, List
 from pm4py.util.xes_constants import DEFAULT_NAME_KEY
 from pm4py.objects.petri.align_utils import SKIP, STD_MODEL_LOG_MOVE_COST
@@ -28,45 +27,6 @@ def list_of_str_to_trace(activities: List[str]) -> Trace:
     return t
 
 
-def get_process_tree_height(pt: ProcessTree) -> int:
-    """
-    calculates from the given node the max height downwards
-    :param pt: process tree node
-    :return: height
-    """
-    if is_leaf(pt):
-        return 1
-    else:
-        return 1 + max([get_process_tree_height(x) for x in pt.children])
-
-
-def get_number_leaf_nodes(pt: ProcessTree) -> int:
-    if is_leaf(pt):
-        return 1
-    else:
-        res = 0
-        for c in pt.children:
-            res += get_number_leaf_nodes(c)
-        return res
-
-
-def get_number_nodes(pt: ProcessTree) -> int:
-    res = 1
-    for c in pt.children:
-        res += get_number_nodes(c)
-    return res
-
-
-def get_number_inner_nodes(pt: ProcessTree) -> int:
-    if is_leaf(pt):
-        return 0
-    else:
-        res = 1
-        for c in pt.children:
-            res += get_number_inner_nodes(c)
-        return res
-
-
 def get_activity_labels_from_trace(trace: Trace) -> Set[str]:
     labels = set()
     for a in trace:
@@ -74,7 +34,7 @@ def get_activity_labels_from_trace(trace: Trace) -> Set[str]:
     return labels
 
 
-def get_costs_from_alignment(align) -> int:
+def apply_standard_cost_function_to_alignment(align: List) -> int:
     res = 0
     for move in align:
         if move[0] == SKIP and move[1] is not None:
@@ -87,22 +47,10 @@ def get_costs_from_alignment(align) -> int:
 
 
 def empty_sequence_accepted(pt: ProcessTree) -> bool:
-    alignment = __calculate_optimal_alignment(pt, Trace(), variant=Variants.VERSION_STATE_EQUATION_A_STAR)
+    alignment = calculate_optimal_alignment(pt, Trace(), variant=Variants.VERSION_STATE_EQUATION_A_STAR)
     return alignment["cost"] < STD_MODEL_LOG_MOVE_COST
 
 
-def process_tree_to_binary_process_tree(pt: ProcessTree) -> ProcessTree:
-    if len(pt.children) > 2:
-        new_subtree = ProcessTree()
-        new_subtree.operator = pt.operator
-        new_subtree.children = pt.children[1:]
-        pt.children = pt.children[:1]
-        pt.children.append(new_subtree)
-    for c in pt.children:
-        process_tree_to_binary_process_tree(c)
-    return pt
-
-
-def __calculate_optimal_alignment(pt: ProcessTree, trace: Trace, variant=Variants.VERSION_STATE_EQUATION_A_STAR):
+def calculate_optimal_alignment(pt: ProcessTree, trace: Trace, variant=Variants.VERSION_STATE_EQUATION_A_STAR):
     net, im, fm = convert_pt_to_petri_net(pt)
     return get_alignment(trace, net, im, fm, variant=variant)
