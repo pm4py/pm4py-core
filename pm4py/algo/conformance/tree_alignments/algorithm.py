d5864b61bbc7753835a0bc06c2b0f40058cd78f8
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-08-07 13:54:52 +0200

    refactor(pm4py): refactoring
    Refactoring

--- pm4py/algo/conformance/tree_alignments/algorithm.py
+++ pm4py/algo/conformance/tree_alignments/algorithm.py
@@ -1,13 +1,16 @@
-from pm4py.algo.conformance.tree_alignments.variants.approximated import algorithm as approximated
+from pm4py.algo.conformance.tree_alignments.variants.approximated import matrix_lp as approximated_matrix_lp
+from pm4py.algo.conformance.tree_alignments.variants.approximated import original as approximated_original
+
 from pm4py.util import exec_utils
 from enum import Enum
 
 
 class Variants(Enum):
-    APPROXIMATED = approximated
+    APPROXIMATED_ORIGINAL = approximated_original
+    APPROXIMATED_MATRIX_LP = approximated_matrix_lp
 
 
-DEFAULT_VARIANT = Variants.APPROXIMATED
+DEFAULT_VARIANT = Variants.APPROXIMATED_MATRIX_LP
 
 
 def apply(obj, pt, variant=DEFAULT_VARIANT, parameters=None):
