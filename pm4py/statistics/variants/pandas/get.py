b1f423ca99f17699a713d96cde0c9382d2d59c6f
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-08-09 14:09:07 +0200

    feat(pm4py): get_variants_count
    get_variants_count method

--- pm4py/statistics/variants/pandas/get.py
+++ pm4py/statistics/variants/pandas/get.py
@@ -1,5 +1,31 @@
 from pm4py.statistics.traces.pandas import case_statistics
-from pm4py.statistics.parameters import Parameters
+from collections import Counter
+
+
+def get_variants_count(df, parameters=None):
+    """
+    Gets the dictionary of variants from the current dataframe
+
+    Parameters
+    --------------
+    df
+        Dataframe
+    parameters
+        Possible parameters of the algorithm, including:
+            Parameters.ACTIVITY_KEY -> Column that contains the activity
+
+    Returns
+    --------------
+    variants_set
+        Dictionary of variants in the log
+    """
+    if parameters is None:
+        parameters = {}
+    var_stats = case_statistics.get_variant_statistics(df, parameters=parameters)
+    if var_stats:
+        count_key = list(x for x in var_stats[0].keys() if not x == "variant")[0]
+        return {x["variant"]: x[count_key] for x in var_stats}
+    return {}
 
 
 def get_variants_set(df, parameters=None):
