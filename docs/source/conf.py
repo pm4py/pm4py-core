694916226fe3d193533d5df5125defd93f4829e1
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-11-25 13:40:16 +0100

    fix(pm4py): updated version number
    updated version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ project_copyright = 'Fraunhofer FIT'
 author = 'Fraunhofer FIT'
 
 # The short X.Y version
-version = '2.0.1'
+version = '2.1'
 # The full version, including alpha/beta/rc tags
-release = '2.0.1'
+release = '2.1.0'
 
 # -- General configuration ---------------------------------------------------
 
