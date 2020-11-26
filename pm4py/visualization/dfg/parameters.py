4689c8dbc2106ba49e123a95f62914eb876f17d7
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-11-26 07:02:53 +0100

    fix(pm4py): missing parameters in dfg visualization
    missing parameters in DFG visualization

--- pm4py/visualization/dfg/parameters.py
+++ pm4py/visualization/dfg/parameters.py
@@ -1,10 +1,12 @@
 from enum import Enum
-from pm4py.util.constants import PARAMETER_CONSTANT_ACTIVITY_KEY
+from pm4py.util import constants
 
 
 class Parameters(Enum):
-    ACTIVITY_KEY = PARAMETER_CONSTANT_ACTIVITY_KEY
+    ACTIVITY_KEY = constants.PARAMETER_CONSTANT_ACTIVITY_KEY
     FORMAT = "format"
     MAX_NO_EDGES_IN_DIAGRAM = "maxNoOfEdgesInDiagram"
     START_ACTIVITIES = "start_activities"
     END_ACTIVITIES = "end_activities"
+    TIMESTAMP_KEY = constants.PARAMETER_CONSTANT_TIMESTAMP_KEY
+    START_TIMESTAMP_KEY = constants.PARAMETER_CONSTANT_START_TIMESTAMP_KEY
