69723106a5649cd1f5b633346a454fb247048617
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-01-15 07:26:06 +0100

    feat(pm4py): possibility to return the visited elements
    possibility to return the visited elements

--- pm4py/simulation/playout/variants/basic_playout.py
+++ pm4py/simulation/playout/variants/basic_playout.py
@@ -1,19 +1,21 @@
 import datetime
 from copy import copy
+from enum import Enum
 from random import choice
 
 from pm4py.objects.log import log as log_instance
 from pm4py.objects.petri import semantics
+from pm4py.objects.petri.petrinet import PetriNet
+from pm4py.util import constants
 from pm4py.util import exec_utils
 from pm4py.util import xes_constants
-from enum import Enum
-from pm4py.util import constants
 
 
 class Parameters(Enum):
     ACTIVITY_KEY = constants.PARAMETER_CONSTANT_ACTIVITY_KEY
     TIMESTAMP_KEY = constants.PARAMETER_CONSTANT_TIMESTAMP_KEY
     CASE_ID_KEY = constants.PARAMETER_CONSTANT_CASEID_KEY
+    RETURN_VISITED_ELEMENTS = "return_visited_elements"
     NO_TRACES = "noTraces"
     MAX_TRACE_LENGTH = "maxTraceLength"
 
@@ -21,7 +23,7 @@ class Parameters(Enum):
 def apply_playout(net, initial_marking, no_traces=100, max_trace_length=100,
                   case_id_key=xes_constants.DEFAULT_TRACEID_KEY,
                   activity_key=xes_constants.DEFAULT_NAME_KEY, timestamp_key=xes_constants.DEFAULT_TIMESTAMP_KEY,
-                  final_marking=None):
+                  final_marking=None, return_visited_elements=False):
     """
     Do the playout of a Petrinet generating a log
 
@@ -46,12 +48,16 @@ def apply_playout(net, initial_marking, no_traces=100, max_trace_length=100,
     """
     # assigns to each event an increased timestamp from 1970
     curr_timestamp = 10000000
-    log = log_instance.EventLog()
+    all_visited_elements = []
+
     for i in range(no_traces):
-        trace = log_instance.Trace()
-        trace.attributes[case_id_key] = str(i)
+        visited_elements = []
+        visible_transitions_visited = []
+
         marking = copy(initial_marking)
-        while len(trace) < max_trace_length:
+        while len(visible_transitions_visited) < max_trace_length:
+            visited_elements.append(marking)
+
             if not semantics.enabled_transitions(net, marking):  # supports nets with possible deadlocks
                 break
             all_enabled_trans = semantics.enabled_transitions(net, marking)
@@ -61,15 +67,33 @@ def apply_playout(net, initial_marking, no_traces=100, max_trace_length=100,
                 trans = choice(list(all_enabled_trans))
             if trans is None:
                 break
+
+            visited_elements.append(trans)
             if trans.label is not None:
+                visible_transitions_visited.append(trans)
+
+            marking = semantics.execute(trans, net, marking)
+
+        all_visited_elements.append(tuple(visited_elements))
+
+    if return_visited_elements:
+        return all_visited_elements
+
+    log = log_instance.EventLog()
+
+    for index, visited_elements in enumerate(all_visited_elements):
+        trace = log_instance.Trace()
+        trace.attributes[case_id_key] = str(index)
+        for element in visited_elements:
+            if type(element) is PetriNet.Transition and element.label is not None:
                 event = log_instance.Event()
-                event[activity_key] = trans.label
+                event[activity_key] = element.label
                 event[timestamp_key] = datetime.datetime.fromtimestamp(curr_timestamp)
                 trace.append(event)
                 # increases by 1 second
                 curr_timestamp += 1
-            marking = semantics.execute(trans, net, marking)
         log.append(trace)
+
     return log
 
 
@@ -98,7 +122,8 @@ def apply(net, initial_marking, final_marking=None, parameters=None):
                                                xes_constants.DEFAULT_TIMESTAMP_KEY)
     no_traces = exec_utils.get_param_value(Parameters.NO_TRACES, parameters, 1000)
     max_trace_length = exec_utils.get_param_value(Parameters.MAX_TRACE_LENGTH, parameters, 1000)
+    return_visited_elements = exec_utils.get_param_value(Parameters.RETURN_VISITED_ELEMENTS, parameters, False)
 
     return apply_playout(net, initial_marking, max_trace_length=max_trace_length, no_traces=no_traces,
                          case_id_key=case_id_key, activity_key=activity_key, timestamp_key=timestamp_key,
-                         final_marking=final_marking)
+                         final_marking=final_marking, return_visited_elements=return_visited_elements)
