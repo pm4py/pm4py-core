043e85a8006f926ad19bda832c174c42b8855f37
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-02-15 12:28:02 +0100

    fix(pm4py): bug fix
    bug fix

--- pm4py/util/constants.py
+++ pm4py/util/constants.py
@@ -21,6 +21,7 @@ PARAMETER_KEY_CASE_ATTRIBUTE_PRFIX = 'case:'
 STOCHASTIC_DISTRIBUTION = "stochastic_distribution"
 LAYOUT_INFORMATION_PETRI = "layout_information_petri"
 PLACE_NAME_TAG = "place_name_tag"
+TRANS_NAME_TAG = "trans_name_tag"
 
 DEFAULT_VARIANT_SEP = ","
 DEFAULT_INDEX_KEY = "@@index"
