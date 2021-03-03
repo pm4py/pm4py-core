095fc3f87d08d02a58e0dd2c50f4003178df2b75
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2021-03-03 16:08:53 +0100

    minor fixes in simplified interface for discovery algorithms

--- pm4py/discovery.py
+++ pm4py/discovery.py
@@ -1,6 +1,11 @@
-from pm4py.objects.log.log import EventLog
+import warnings
 from typing import Tuple
-from pm4py.objects.petri.petrinet import PetriNet
+
+import deprecation
+
+from pm4py.objects.log.log import EventLog
+from pm4py.objects.petri.petrinet import PetriNet, Marking
+from pm4py.objects.process_tree.process_tree import ProcessTree
 
 
 def discover_dfg(log: EventLog) -> Tuple[dict, dict, dict]:
@@ -30,7 +35,7 @@ def discover_dfg(log: EventLog) -> Tuple[dict, dict, dict]:
     return dfg, start_activities, end_activities
 
 
-def discover_petri_net_alpha(log: EventLog) -> PetriNet:
+def discover_petri_net_alpha(log: EventLog) -> Tuple[PetriNet, Marking, Marking]:
     """
     Discovers a Petri net using the Alpha Miner
 
@@ -52,7 +57,7 @@ def discover_petri_net_alpha(log: EventLog) -> PetriNet:
     return alpha_miner.apply(log, variant=alpha_miner.Variants.ALPHA_VERSION_CLASSIC)
 
 
-def discover_petri_net_alpha_plus(log: EventLog) -> PetriNet:
+def discover_petri_net_alpha_plus(log: EventLog) -> Tuple[PetriNet, Marking, Marking]:
     """
     Discovers a Petri net using the Alpha+ algorithm
 
@@ -74,7 +79,7 @@ def discover_petri_net_alpha_plus(log: EventLog) -> PetriNet:
     return alpha_miner.apply(log, variant=alpha_miner.Variants.ALPHA_VERSION_PLUS)
 
 
-def discover_petri_net_inductive(log: EventLog, noise_threshold: float = 0.0) -> PetriNet:
+def discover_petri_net_inductive(log: EventLog, noise_threshold: float = 0.0) -> Tuple[PetriNet, Marking, Marking]:
     """
     Discovers a Petri net using the IMDFc algorithm
 
@@ -104,7 +109,7 @@ def discover_petri_net_inductive(log: EventLog, noise_threshold: float = 0.0) ->
 
 
 def discover_petri_net_heuristics(log: EventLog, dependency_threshold: float = 0.5, and_threshold: float = 0.65,
-                                  loop_two_threshold: float = 0.5) -> PetriNet:
+                                  loop_two_threshold: float = 0.5) -> Tuple[PetriNet, Marking, Marking]:
     """
     Discover a Petri net using the Heuristics Miner
 
@@ -135,7 +140,35 @@ def discover_petri_net_heuristics(log: EventLog, dependency_threshold: float = 0
         parameters.LOOP_LENGTH_TWO_THRESH: loop_two_threshold})
 
 
-def discover_tree_inductive(log: EventLog, noise_threshold: float = 0.0) -> PetriNet:
+def discover_process_tree_inductive(log: EventLog, noise_threshold: float = 0.0) -> ProcessTree:
+    """
+    Discovers a process tree using the IMDFc algorithm
+
+    Parameters
+    --------------
+    log
+        Event log
+    noise_threshold
+        Noise threshold (default: 0.0)
+
+    Returns
+    --------------
+    process_tree
+        Process tree object
+    """
+    from pm4py.algo.discovery.inductive import algorithm as inductive_miner
+    if noise_threshold > 0.0:
+        return inductive_miner.apply_tree(log, variant=inductive_miner.Variants.IMf, parameters={
+            inductive_miner.Variants.IMf.value.Parameters.NOISE_THRESHOLD: noise_threshold})
+    else:
+        return inductive_miner.apply_tree(log, variant=inductive_miner.Variants.IM, parameters={
+            inductive_miner.Variants.IM.value.Parameters.NOISE_THRESHOLD: noise_threshold})
+
+
+@deprecation.deprecated(deprecated_in='2.2.2', removed_in='2.3.0',
+                        details='discover_tree_inductive is deprecated, use discover_process_tree_inductive')
+def discover_tree_inductive(log: EventLog, noise_threshold: float = 0.0) -> ProcessTree:
+    warnings.warn('discover_tree_inductive is deprecated, use discover_process_tree_inductive', DeprecationWarning)
     """
     Discovers a process tree using the IMDFc algorithm
 
