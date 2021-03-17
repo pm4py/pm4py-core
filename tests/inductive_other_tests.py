d18a08558aaa9dcbec5d328409b860ca95ebe005
sebastiaan.van.zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2021-03-17 14:21:40 +0100

    Revert "Merge branch 'ft-835-hash-equals-petri-net-objects' into 'integration'"
    This reverts merge request !324

--- tests/inductive_other_tests.py
+++ tests/inductive_other_tests.py
@@ -36,27 +36,6 @@ class InductiveMinerOtherTests(unittest.TestCase):
         tree = inductive_miner.apply_tree_variants(pd_variants.get_variants_set(df),
                                                    variant=inductive_miner.IMd)
 
-    def test_ptree_in_pt(self):
-        import pm4py
-        from pm4py.objects.process_tree import bottomup
-        from pm4py.objects.petri.petrinet import PetriNet
-        import random
-        import uuid
-        log = pm4py.read_xes(os.path.join("input_data", "running-example.xes"))
-        tree = pm4py.discover_process_tree_inductive(log)
-        nodes = bottomup.get_bottomup_nodes(tree)
-        net = PetriNet()
-        c = 0
-        while True:
-            c += 1
-            if c % 2 == 1:
-                t = PetriNet.Transition(random.choice(nodes))
-            else:
-                t = PetriNet.Transition(str(uuid.uuid4()), None)
-            net.transitions.add(t)
-            if c % 10 == 0:
-                break
-
 
 if __name__ == "__main__":
     unittest.main()
