3187934e3e58caf04968117c04ee54f5b22e7556
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-04-17 13:41:27 +0200

    refactor(pm4py): moving DFG package
    moving DFG package

--- pm4py/algo/discovery/__init__.py
+++ pm4py/algo/discovery/__init__.py
@@ -1,4 +1,4 @@
-from pm4py.algo.discovery import dfg, alpha, heuristics, inductive, transition_system, log_skeleton, footprints, temporal_profile, minimum_self_distance
+from pm4py.algo.discovery import dfg, alpha, inductive, transition_system, log_skeleton, footprints, minimum_self_distance
 import pkgutil
 
 if pkgutil.find_loader("pandas"):
--- pm4py/objects/log/__init__.py
+++
@@ -1,1 +0,0 @@
-from pm4py.objects.log import importer, exporter, util, obj
