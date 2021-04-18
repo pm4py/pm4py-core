a71677caab77e3f0dbfde962a42a44a60297d3ab
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-04-18 11:44:53 +0200

    fix(pm4py): fixed the refactoring

--- pm4py/visualization/petri_net/variants/wo_decoration.py
+++ pm4py/visualization/petri_net/variants/wo_decoration.py
@@ -1,4 +1,16 @@
 from pm4py.visualization.petri_net.common import visualize
+from enum import Enum
+from pm4py.util.constants import PARAMETER_CONSTANT_ACTIVITY_KEY, PARAMETER_CONSTANT_TIMESTAMP_KEY
+
+
+class Parameters(Enum):
+    FORMAT = "format"
+    DEBUG = "debug"
+    RANKDIR = "set_rankdir"
+    ACTIVITY_KEY = PARAMETER_CONSTANT_ACTIVITY_KEY
+    TIMESTAMP_KEY = PARAMETER_CONSTANT_TIMESTAMP_KEY
+    AGGREGATION_MEASURE = "aggregationMeasure"
+    FONT_SIZE = "font_size"
 
 
 def apply(net, initial_marking, final_marking, log=None, aggregated_statistics=None, parameters=None):
