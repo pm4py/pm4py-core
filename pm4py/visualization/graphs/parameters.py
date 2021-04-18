ca7f00b18baf41c6a4d346062028c6413f63ce88
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-04-18 11:40:05 +0200

    refactor(pm4py): refactored the visualizations
    refactored the visualizations

--- pm4py/visualization/graphs/parameters.py
+++ pm4py/visualization/graphs/parameters.py
@@ -1,4 +1,9 @@
 from enum import Enum
+import warnings
+
+
+warnings.warn(
+    "pm4py.visualization.graphs.parameters is deprecated. please use the parameters inside the visualization variant.")
 
 
 class Parameters(Enum):
