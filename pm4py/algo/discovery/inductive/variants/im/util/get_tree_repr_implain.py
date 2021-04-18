636f7b06141e68a1a2a86e3420941b1da918237a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-04-18 12:06:22 +0200

    refactor(pm4py): parameters inductive miner
    parameters inductive miner

--- pm4py/algo/discovery/inductive/variants/im/util/get_tree_repr_implain.py
+++ pm4py/algo/discovery/inductive/variants/im/util/get_tree_repr_implain.py
@@ -2,7 +2,21 @@ from pm4py.algo.discovery.dfg.utils.dfg_utils import get_activities_self_loop
 from pm4py.objects.process_tree.obj import ProcessTree
 from pm4py.objects.process_tree.obj import Operator
 from pm4py.util import exec_utils, xes_constants
-from pm4py.algo.discovery.inductive.parameters import Parameters
+from pm4py.util import constants
+from enum import Enum
+
+
+class Parameters(Enum):
+    ACTIVITY_KEY = constants.PARAMETER_CONSTANT_ACTIVITY_KEY
+    START_TIMESTAMP_KEY = constants.PARAMETER_CONSTANT_START_TIMESTAMP_KEY
+    TIMESTAMP_KEY = constants.PARAMETER_CONSTANT_TIMESTAMP_KEY
+    CASE_ID_KEY = constants.PARAMETER_CONSTANT_CASEID_KEY
+    NOISE_THRESHOLD = "noiseThreshold"
+    EMPTY_TRACE_KEY = "empty_trace"
+    ONCE_PER_TRACE_KEY = "once_per_trace"
+    CONCURRENT_KEY = "concurrent"
+    STRICT_TAU_LOOP_KEY = "strict_tau_loop"
+    TAU_LOOP_KEY = "tau_loop"
 
 
 def get_transition(label):
--- pm4py/algo/discovery/inductive/variants/im_f/data_structures/subtree_infrequent.py
+++ pm4py/algo/discovery/inductive/variants/im_f/data_structures/subtree_infrequent.py
@@ -14,10 +14,24 @@ from pm4py.statistics.start_activities.log import get  as start_activities_get
 from pm4py.algo.discovery.inductive.util import parallel_cut_utils, detection_utils, cut_detection
 from pm4py.algo.discovery.inductive.variants.im_f import splitting_infrequent, fall_through_infrequent
 from pm4py.algo.discovery.inductive.variants.im.util import fall_through
-from pm4py.algo.discovery.inductive.parameters import Parameters
 from pm4py.util import exec_utils
 import pkgutil
 import logging
+from pm4py.util import constants
+from enum import Enum
+
+
+class Parameters(Enum):
+    ACTIVITY_KEY = constants.PARAMETER_CONSTANT_ACTIVITY_KEY
+    START_TIMESTAMP_KEY = constants.PARAMETER_CONSTANT_START_TIMESTAMP_KEY
+    TIMESTAMP_KEY = constants.PARAMETER_CONSTANT_TIMESTAMP_KEY
+    CASE_ID_KEY = constants.PARAMETER_CONSTANT_CASEID_KEY
+    NOISE_THRESHOLD = "noiseThreshold"
+    EMPTY_TRACE_KEY = "empty_trace"
+    ONCE_PER_TRACE_KEY = "once_per_trace"
+    CONCURRENT_KEY = "concurrent"
+    STRICT_TAU_LOOP_KEY = "strict_tau_loop"
+    TAU_LOOP_KEY = "tau_loop"
 
 
 class SubtreeInfrequent(object):
