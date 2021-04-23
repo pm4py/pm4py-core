c3b938765aff4e02801337f3114657f8de0dec07
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2021-04-23 11:30:36 +0200

    prepare for release 2.2.6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Fraunhofer FIT'
 # The short X.Y version
 version = '2.2'
 # The full version, including alpha/beta/rc tags
-release = '2.2.5'
+release = '2.2.6'
 
 # -- General configuration ---------------------------------------------------
 
--- pm4py/meta.py
+++ pm4py/meta.py
@@ -1,4 +1,4 @@
-VERSION = '2.2.5'
+VERSION = '2.2.6'
 
 __name__ = 'pm4py'
 __version__ = VERSION
