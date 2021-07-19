316d0b0b55756b123453989cf7a1e7fdfd97df40
alessandro.berti <alessandro.berti@fit.fraunhofer.de>
2021-07-19 11:26:28 +0000

    Coherence internal refactoring

--- pm4py/algo/simulation/tree_generator/simulator.py
+++ pm4py/algo/simulation/tree_generator/simulator.py
@@ -1,36 +1,5 @@
-from pm4py.algo.simulation.tree_generator.variants import basic, ptandloggenerator
-from enum import Enum
-from pm4py.util import exec_utils
+import warnings
 
+warnings.warn("Please use pm4py.algo.simulation.tree_generator.algorithm instead")
 
-class Variants(Enum):
-    BASIC = basic
-    PTANDLOGGENERATOR = ptandloggenerator
-
-
-BASIC = Variants.BASIC
-PTANDLOGGENERATOR = Variants.PTANDLOGGENERATOR
-DEFAULT_VARIANT = Variants.PTANDLOGGENERATOR
-
-VERSIONS = {Variants.BASIC, Variants.PTANDLOGGENERATOR}
-
-
-def apply(variant=DEFAULT_VARIANT, parameters=None):
-    """
-    Generate a process tree
-
-    Parameters
-    ------------
-    variant
-        Variant of the algorithm. Admitted values:
-            - Variants.BASIC
-            - Variants.PTANDLOGGENERATOR
-    parameters
-        Specific parameters of the algorithm
-
-    Returns
-    ------------
-    tree
-        Process tree
-    """
-    return exec_utils.get_variant(variant).apply(parameters=parameters)
+from pm4py.algo.simulation.tree_generator.algorithm import *
