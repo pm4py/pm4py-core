2ccd00dd5dbe9d72a8012032cfb1c5f2fea256dd
alessandro.berti <alessandro.berti@fit.fraunhofer.de>
2021-08-06 13:00:00 +0000

    Case termination statistics also for Pandas dataframes + Other statistics for Pandas dataframes

--- pm4py/statistics/traces/generic/pandas/case_arrival.py
+++ pm4py/statistics/traces/generic/pandas/case_arrival.py
@@ -56,3 +56,43 @@ def get_case_arrival_avg(df: pd.DataFrame, parameters: Optional[Dict[Union[str, 
     df_successive_rows = df_successive_rows.dropna(subset=['interlapsed_time'])
 
     return df_successive_rows['interlapsed_time'].mean()
+
+
+def get_case_dispersion_avg(df, parameters=None):
+    """
+    Gets the average time interlapsed between case ends
+
+    Parameters
+    --------------
+    df
+        Pandas dataframe
+    parameters
+        Parameters of the algorithm, including:
+            Parameters.TIMESTAMP_KEY -> attribute of the log to be used as timestamp
+
+    Returns
+    --------------
+    case_dispersion_avg
+        Average time interlapsed between the completion of cases
+    """
+    if parameters is None:
+        parameters = {}
+
+    caseid_glue = exec_utils.get_param_value(Parameters.CASE_ID_KEY, parameters, CASE_CONCEPT_NAME)
+    timest_key = exec_utils.get_param_value(Parameters.TIMESTAMP_KEY, parameters, DEFAULT_TIMESTAMP_KEY)
+
+    first_df = df.groupby(caseid_glue).last()
+
+    first_df = first_df.sort_values(timest_key)
+
+    first_df_shift = first_df.shift(-1)
+
+    first_df_shift.columns = [str(col) + '_2' for col in first_df_shift.columns]
+
+    df_successive_rows = pd.concat([first_df, first_df_shift], axis=1)
+    df_successive_rows['interlapsed_time'] = (
+            df_successive_rows[timest_key + '_2'] - df_successive_rows[timest_key]).astype('timedelta64[s]')
+
+    df_successive_rows = df_successive_rows.dropna(subset=['interlapsed_time'])
+
+    return df_successive_rows['interlapsed_time'].mean()
