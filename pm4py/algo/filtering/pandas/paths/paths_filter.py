3e2b3e5ff272f73c16ecb896e8219f83458043ef
alessandro.berti <alessandro.berti@fit.fraunhofer.de>
2021-09-03 14:28:04 +0000

    Paths performance filter

--- pm4py/algo/filtering/pandas/paths/paths_filter.py
+++ pm4py/algo/filtering/pandas/paths/paths_filter.py
@@ -12,6 +12,7 @@ from copy import copy
 import deprecation
 from typing import Optional, Dict, Any, Union, Tuple, List
 import pandas as pd
+import sys
 
 
 class Parameters(Enum):
@@ -20,6 +21,8 @@ class Parameters(Enum):
     TIMESTAMP_KEY = PARAMETER_CONSTANT_TIMESTAMP_KEY
     DECREASING_FACTOR = "decreasingFactor"
     POSITIVE = "positive"
+    MIN_PERFORMANCE = "min_performance"
+    MAX_PERFORMANCE = "max_performance"
 
 
 def apply(df: pd.DataFrame, paths: List[Tuple[str, str]], parameters: Optional[Dict[Union[str, Parameters], Any]] = None) -> pd.DataFrame:
@@ -45,11 +48,11 @@ def apply(df: pd.DataFrame, paths: List[Tuple[str, str]], parameters: Optional[D
     """
     if parameters is None:
         parameters = {}
-    paths = [path[0] + "," + path[1] for path in paths]
     case_id_glue = exec_utils.get_param_value(Parameters.CASE_ID_KEY, parameters, CASE_CONCEPT_NAME)
     attribute_key = exec_utils.get_param_value(Parameters.ATTRIBUTE_KEY, parameters, DEFAULT_NAME_KEY)
     timestamp_key = exec_utils.get_param_value(Parameters.TIMESTAMP_KEY, parameters, DEFAULT_TIMESTAMP_KEY)
     positive = exec_utils.get_param_value(Parameters.POSITIVE, parameters, True)
+    paths = [path[0] + "," + path[1] for path in paths]
     df = df.sort_values([case_id_glue, timestamp_key])
     filt_df = df[[case_id_glue, attribute_key]]
     filt_dif_shifted = filt_df.shift(-1)
@@ -57,6 +60,62 @@ def apply(df: pd.DataFrame, paths: List[Tuple[str, str]], parameters: Optional[D
     stacked_df = pd.concat([filt_df, filt_dif_shifted], axis=1)
     stacked_df["@@path"] = stacked_df[attribute_key] + "," + stacked_df[attribute_key + "_2"]
     stacked_df = stacked_df[stacked_df["@@path"].isin(paths)]
+    i1 = df.set_index(case_id_glue).index
+    i2 = stacked_df.set_index(case_id_glue).index
+    if positive:
+        ret = df[i1.isin(i2)]
+    else:
+        ret = df[~i1.isin(i2)]
+
+    ret.attrs = copy(df.attrs) if hasattr(df, 'attrs') else {}
+    return ret
+
+
+def apply_performance(df: pd.DataFrame, provided_path: Tuple[str, str], parameters: Optional[Dict[Union[str, Parameters], Any]] = None) -> pd.DataFrame:
+    """
+    Filters the cases of a dataframe where there is at least one occurrence of the provided path
+    occurring in the defined timedelta range.
+
+    Parameters
+    ----------
+    df
+        Dataframe
+    paths
+        Paths to filter on
+    parameters
+        Possible parameters of the algorithm, including:
+            Parameters.CASE_ID_KEY -> Case ID column in the dataframe
+            Parameters.ATTRIBUTE_KEY -> Attribute we want to filter
+            Parameters.TIMESTAMP_KEY -> Attribute identifying the timestamp in the log
+            Parameters.POSITIVE -> Specifies if the filter should be applied including traces (positive=True)
+            or excluding traces (positive=False)
+            Parameters.MIN_PERFORMANCE -> Minimal allowed performance of the provided path
+            Parameters.MAX_PERFORMANCE -> Maximal allowed performance of the provided path
+
+    Returns
+    ----------
+    df
+        Filtered dataframe
+    """
+    if parameters is None:
+        parameters = {}
+    case_id_glue = exec_utils.get_param_value(Parameters.CASE_ID_KEY, parameters, CASE_CONCEPT_NAME)
+    attribute_key = exec_utils.get_param_value(Parameters.ATTRIBUTE_KEY, parameters, DEFAULT_NAME_KEY)
+    timestamp_key = exec_utils.get_param_value(Parameters.TIMESTAMP_KEY, parameters, DEFAULT_TIMESTAMP_KEY)
+    positive = exec_utils.get_param_value(Parameters.POSITIVE, parameters, True)
+    provided_path = provided_path[0] + "," + provided_path[1]
+    min_performance = exec_utils.get_param_value(Parameters.MIN_PERFORMANCE, parameters, 0)
+    max_performance = exec_utils.get_param_value(Parameters.MAX_PERFORMANCE, parameters, sys.maxsize)
+    df = df.sort_values([case_id_glue, timestamp_key])
+    filt_df = df[[case_id_glue, attribute_key, timestamp_key]]
+    filt_dif_shifted = filt_df.shift(-1)
+    filt_dif_shifted.columns = [str(col) + '_2' for col in filt_dif_shifted.columns]
+    stacked_df = pd.concat([filt_df, filt_dif_shifted], axis=1)
+    stacked_df["@@path"] = stacked_df[attribute_key] + "," + stacked_df[attribute_key + "_2"]
+    stacked_df = stacked_df[stacked_df["@@path"] == provided_path]
+    stacked_df["@@timedelta"] = (stacked_df[timestamp_key + "_2"] - stacked_df[timestamp_key]).astype('timedelta64[s]')
+    stacked_df = stacked_df[stacked_df["@@timedelta"] >= min_performance]
+    stacked_df = stacked_df[stacked_df["@@timedelta"] <= max_performance]
     i1 = df.set_index(case_id_glue).index
     i2 = stacked_df.set_index(case_id_glue).index
     if positive:
