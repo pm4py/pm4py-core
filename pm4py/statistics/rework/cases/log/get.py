0feeb0c6cd4f129845059afc963e5580bcf66d0b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-09-07 14:21:31 +0200

    small fixes + added new examples to general examples script

--- pm4py/statistics/rework/cases/log/get.py
+++ pm4py/statistics/rework/cases/log/get.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Optional, Dict, Any, Union
 
-from pm4py.objects.log.log import EventLog
+from pm4py.objects.log.obj import EventLog
 from pm4py.util import exec_utils, constants, xes_constants
 
 
