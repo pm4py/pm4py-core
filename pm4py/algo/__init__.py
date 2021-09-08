d59f7b977a51b269c1b6355cb405afe3d0a89fe2
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-09-08 12:21:51 +0200

    fix(pm4py): fixing the internal imports
    fixing the internal imports

--- pm4py/algo/__init__.py
+++ pm4py/algo/__init__.py
@@ -1,2 +1,6 @@
-from pm4py.algo import discovery, conformance, reduction, analysis, evaluation, simulation, comparison, decision_mining, organizational_mining, transformation
+from pm4py.algo import discovery, conformance, reduction, analysis, evaluation, simulation, comparison, organizational_mining, transformation
 
+import pkgutil
+
+if pkgutil.find_loader("sklearn"):
+    from pm4py.algo import decision_mining
