51247c1ddd2a441f98c19d97517bd40d80659f03
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-09-26 05:45:16 +0200

    Revert "feat(pm4py): gamma random variable"
    This reverts commit 79e15e0e16c1234a972a3568c7b3ba5e1e06bc3c.

--- pm4py/objects/random_variables/__init__.py
+++ pm4py/objects/random_variables/__init__.py
@@ -1,1 +1,1 @@
-from pm4py.objects.random_variables import constant0, normal, uniform, exponential, random_variable, lognormal, gamma
+from pm4py.objects.random_variables import constant0, normal, uniform, exponential, random_variable
--- pm4py/objects/random_variables/gamma/__init__.py
+++
@@ -1,1 +0,0 @@
-from pm4py.objects.random_variables.gamma import random_variable
