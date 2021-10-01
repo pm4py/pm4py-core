044acb52aa2944dc88cee2d623cfcbc99e2a32fc
alessandro.berti <alessandro.berti@fit.fraunhofer.de>
2021-10-01 13:43:00 +0000

    FT 1264 DFG mining - Version with invisibles and no duplicate transitions

--- pm4py/objects/conversion/dfg/converter.py
+++ pm4py/objects/conversion/dfg/converter.py
@@ -1,10 +1,11 @@
-from pm4py.objects.conversion.dfg.variants import to_petri_net_activity_defines_place
+from pm4py.objects.conversion.dfg.variants import to_petri_net_activity_defines_place, to_petri_net_invisibles_no_duplicates
 from enum import Enum
 from pm4py.util import exec_utils
 
 
 class Variants(Enum):
     VERSION_TO_PETRI_NET_ACTIVITY_DEFINES_PLACE = to_petri_net_activity_defines_place
+    VERSION_TO_PETRI_NET_INVISIBLES_NO_DUPLICATES = to_petri_net_invisibles_no_duplicates
 
 
 DEFAULT_VARIANT = Variants.VERSION_TO_PETRI_NET_ACTIVITY_DEFINES_PLACE
