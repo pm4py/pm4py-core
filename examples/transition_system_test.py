8d1161796698f59017e773014ac68431d0c12a4b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-10-28 12:11:34 +0200

    updated example on transition systems
    updated example on transition systems

--- examples/transition_system_test.py
+++ examples/transition_system_test.py
@@ -8,9 +8,14 @@ def execute_script():
     # log_path = "C:/Users/bas/Documents/tue/svn/private/logs/ilp_test_2_abcd_acbd.xes"
     log_path = os.path.join("..", "tests", "input_data", "running-example.xes")
     log = xes_importer.apply(log_path)
-    ts = ts_discovery.apply(log)
+    ts = ts_discovery.apply(log, parameters={"include_data": True})
     viz = ts_vis.apply(ts, parameters={ts_vis.Variants.VIEW_BASED.value.Parameters.FORMAT: "svg"})
     ts_vis.view(viz)
+    for state in ts.states:
+        print(state.name, "ingoing=", len(state.data["ingoing_events"]), "outgoing=",
+              len(state.data["outgoing_events"]))
+    for trans in ts.transitions:
+        print(trans.name, trans.from_state.name, trans.to_state.name, "frequency=", len(trans.data["events"]))
 
 
 if __name__ == '__main__':
