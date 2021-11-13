f6f621986d2cb87bd7a52912bfab78dfad1b6560
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-11-13 06:43:59 +0100

    fix(pm4py): fixed inits
    fixed inits

--- pm4py/algo/discovery/__init__.py
+++ pm4py/algo/discovery/__init__.py
@@ -1,4 +1,4 @@
-from pm4py.algo.discovery import dfg, alpha, inductive, transition_system, log_skeleton, footprints, minimum_self_distance, performance_spectrum, temporal_profile, batches, heuristics
+from pm4py.algo.discovery import dfg, alpha, inductive, transition_system, log_skeleton, footprints, minimum_self_distance, performance_spectrum, temporal_profile, batches, heuristics, ocel
 import pkgutil
 
 if pkgutil.find_loader("pandas"):
