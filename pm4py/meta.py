6d7d102c60fbf697768dd81d647358486ea454df
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-03-27 10:23:12 +0200

    docs(pm4py): main index

--- pm4py/meta.py
+++ pm4py/meta.py
@@ -8,4 +8,26 @@ __maintainer_email__ = "pm4py@fit.fraunhofer.de"
 
 __doc__ = """
 Process Mining for Python (PM4Py)
+
+* Importing / Exporting Log Files
+    * `Importing Log Files`_
+    * `Exporting Log Files`_
+    * `Conversions`_
+* `Process Discovery`_
+* `Conformance Checking`_
+* `Statistics`_
+* `Filtering`_
+* `Machine Learning`_
+* `Simulation`_
+
+.. _Importing Log Files: pm4py.html#module-pm4py.read
+.. _Exporting Log Files: pm4py.html#module-pm4py.write
+.. _Conversions: pm4py.html#module-pm4py.convert
+.. _Process Discovery: pm4py.html#module-pm4py.discovery
+.. _Conformance Checking: pm4py.html#module-pm4py.conformance
+.. _Statistics: pm4py.html#module-pm4py.stats
+.. _Filtering: pm4py.html#module-pm4py.filtering
+.. _Machine Learning: pm4py.html#module-pm4py.ml
+.. _Simulation: pm4py.html#module-pm4py.sim
+
 """
