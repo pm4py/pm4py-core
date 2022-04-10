664719c0ec24c2f9081e8a2d8b759e1cfba767bd
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-04-10 17:40:41 +0200

    docs(pm4py): increased version number

--- pm4py/meta.py
+++ pm4py/meta.py
@@ -1,5 +1,5 @@
 __name__ = 'pm4py'
-VERSION = '2.2.20.1'
+VERSION = '2.2.20.2'
 __version__ = VERSION
 __doc__ = 'Process Mining for Python (PM4Py)'
 __author__ = 'Fraunhofer Institute for Applied Technology'
