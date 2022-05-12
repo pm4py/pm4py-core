78cb1d603f75a05db0144c0a941451d708558df0
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2022-05-12 14:48:39 +0200

    chore: prepare for pm4py 2.2.21

--- pm4py/meta.py
+++ pm4py/meta.py
@@ -1,5 +1,5 @@
 __name__ = 'pm4py'
-VERSION = '2.2.20.2'
+VERSION = '2.2.21'
 __version__ = VERSION
 __doc__ = 'Process Mining for Python (PM4Py)'
 __author__ = 'Fraunhofer Institute for Applied Technology'
