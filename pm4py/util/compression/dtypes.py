3ffac364112baad337a6c3309368aa6bf7deb486
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2022-07-19 09:25:05 +0200

    chore: full support for univariate and multivariate compression PMPY-1803

--- pm4py/util/compression/dtypes.py
+++ pm4py/util/compression/dtypes.py
@@ -1,16 +1,21 @@
 from typing import List, Tuple, Any
 
+
 class UnivariateCompressedLog(List[List[int]]):
     pass
 
-class MultivariateCompressedLog(List[List[Tuple[int]]]):
+
+class MultivariateCompressedLog(List[List[Tuple[Any]]]):
     pass
+
 
 class UnivariateLookupTable(List[Any]):
     pass
 
+
 class MultivariateLookupTable(List[List[Any]]):
     pass
+
 
 UCL = UnivariateCompressedLog
 MCL = MultivariateCompressedLog
