3c44322bd040bd3ce4c0bd2050c902ccb7f2648a
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2022-07-29 16:52:25 +0200

    PMPY-1839: add polars-based version of clean dfg calculation

--- pm4py/algo/discovery/dfg/variants/__init__.py
+++ pm4py/algo/discovery/dfg/variants/__init__.py
@@ -1,1 +1,1 @@
-from pm4py.algo.discovery.dfg.variants import native, performance, freq_triples, case_attributes, clean
+from pm4py.algo.discovery.dfg.variants import native, performance, freq_triples, case_attributes, clean, clean_polars
