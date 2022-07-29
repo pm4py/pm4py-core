f8bd7ffefa1c86d3c881a36902d2c0098c4814ee
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2022-07-29 17:22:41 +0200

    PMPY-1839: change node type of DFG to be ANY

--- pm4py/objects/dfg/obj.py
+++ pm4py/objects/dfg/obj.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List
+from typing import Tuple, List, Any
 
 
 class DirectlyFollowsGraph:
@@ -9,15 +9,15 @@ class DirectlyFollowsGraph:
         self._end_activities = list()
 
     @property
-    def graph(self) -> List[Tuple[str, str, int]]:
+    def graph(self) -> List[Tuple[Any, Any, int]]:
         return self._graph
 
     @property
-    def start_activities(self) -> List[Tuple[str, int]]:
+    def start_activities(self) -> List[Tuple[Any, int]]:
         return self._start_activities
 
     @property
-    def end_activities(self) -> List[Tuple[str, int]]:
+    def end_activities(self) -> List[Tuple[Any, int]]:
         return self._end_activities
 
 
