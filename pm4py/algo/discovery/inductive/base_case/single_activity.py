0828f84c54afa274fcf88156254b21d5c68ea620
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2022-08-05 08:51:12 +0200

    feat(PMPY-1799): implementation of base cases

---
+++ pm4py/algo/discovery/inductive/base_case/single_activity.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from pm4py.algo.discovery.inductive.base_case.abc import BaseCase
+from pm4py.objects.process_tree.obj import ProcessTree
+from pm4py.util.compression.dtypes import UCL
+from pm4py.util.compression import util as comut
+
+
+class SingleActivity(BaseCase[UCL]):
+    @classmethod
+    def applies(cls, obj=UCL) -> bool:
+        return len(list(filter(lambda t: len(t) == 1, obj))) == len(obj) and len(
+            comut.get_alphabet(obj)) == 1
+
+    @classmethod
+    def get_leaf(cls, obj=UCL) -> ProcessTree:
+        return ProcessTree(label=obj[0][0])
