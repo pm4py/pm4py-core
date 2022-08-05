d169d6116d53b9bb1955205728068fe3e4502287
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2022-08-05 08:52:20 +0200

    feat(PMPY-1799): define static classes for inductive miner cuts

--- pm4py/algo/discovery/inductive/cuts/concurrency.py
+++ pm4py/algo/discovery/inductive/cuts/concurrency.py
@@ -1,60 +1,82 @@
+from abc import ABC
 from itertools import product
+from typing import List, Collection, Any, Tuple, Optional, Generic
 
-import pm4py
+from pm4py.algo.discovery.inductive.cuts.abc import Cut, T
 from pm4py.algo.discovery.inductive.variants.im_clean import utils as im_utils
-from pm4py.objects.log.obj import EventLog
+from pm4py.objects.dfg import util as dfu
+from pm4py.objects.dfg.obj import DFG
+from pm4py.objects.process_tree.obj import Operator
+from pm4py.util.compression import util as comut
+from pm4py.util.compression.dtypes import UCL
 
 
-def detect(dfg, alphabet, start_activities, end_activities, msd=None):
-    groups = [{a} for a in alphabet]
-    if len(groups) == 0:
-        return None
-    for a, b in product(alphabet, alphabet):
-        if (a, b) not in dfg or (b, a) not in dfg:
-            groups = im_utils.__merge_groups_for_acts(a, b, groups)
-        elif msd is not None:
-            if (a in msd and b in msd[a]) or (b in msd and a in msd[b]):
+class ConcurrencyCut(ABC, Generic[T], Cut[T]):
+    @classmethod
+    def get_operator(cls) -> Operator:
+        return Operator.PARALLEL
+
+    @classmethod
+    def detect(cls, dfg: DFG, log: UCL = None) -> Optional[List[Collection[Any]]]:
+        start_activities = {a for (a, f) in dfg.start_activities}
+        end_activities = {a for (a, f) in dfg.end_activities}
+        alphabet = dfu.get_vertices(dfg)
+        msdw = comut.msdw(log, comut.msd(log)) if log is not None else None
+        groups = [{a} for a in alphabet]
+        if len(groups) == 0:
+            return None
+        for a, b in product(alphabet, alphabet):
+            if (a, b) not in dfg or (b, a) not in dfg:
                 groups = im_utils.__merge_groups_for_acts(a, b, groups)
+            elif msdw is not None:
+                if (a in msdw and b in msdw[a]) or (b in msdw and a in msdw[b]):
+                    groups = im_utils.__merge_groups_for_acts(a, b, groups)
 
-    groups = list(sorted(groups, key=lambda g: len(g)))
-    i = 0
-    while i < len(groups) and len(groups) > 1:
-        if len(groups[i].intersection(set(start_activities.keys()))) > 0 and len(groups[i].intersection(set(end_activities.keys()))) > 0:
-            i += 1
-            continue
-        group = groups[i]
-        del groups[i]
-        if i == 0:
-            groups[i].update(group)
-        else:
-            groups[i - 1].update(group)
+        groups = list(sorted(groups, key=lambda g: len(g)))
+        i = 0
+        while i < len(groups) and len(groups) > 1:
+            if len(groups[i].intersection(start_activities)) > 0 and len(
+                    groups[i].intersection(end_activities)) > 0:
+                i += 1
+                continue
+            group = groups[i]
+            del groups[i]
+            if i == 0:
+                groups[i].update(group)
+            else:
+                groups[i - 1].update(group)
 
-    return groups if len(groups) > 1 else None
+        return groups if len(groups) > 1 else None
 
 
-def project(log, groups):
-    return [[list(filter(lambda e: e in g, t)) for t in log] for g in groups]    
+class ConcurrencyLogCut(ConcurrencyCut[UCL]):
 
-def project_dfg(dfg_sa_ea_actcount, groups):
-    dfgs = []
-    skippable = []
-    for gind, g in enumerate(groups):
-        start_activities = {}
-        end_activities = {}
-        activities = {}
-        paths_frequency = {}
-        for act in dfg_sa_ea_actcount.start_activities:
-            if act in g:
-                start_activities[act] = dfg_sa_ea_actcount.start_activities[act]
-        for act in dfg_sa_ea_actcount.end_activities:
-            if act in g:
-                end_activities[act] = dfg_sa_ea_actcount.end_activities[act]
-        for act in dfg_sa_ea_actcount.act_count:
-            if act in g:
-                activities[act] = dfg_sa_ea_actcount.act_count[act]
-        for arc in dfg_sa_ea_actcount.dfg:
-            if arc[0] in g and arc[1] in g:
-                paths_frequency[arc] = dfg_sa_ea_actcount.dfg[arc]
-        dfgs.append(im_utils.DfgSaEaActCount(paths_frequency, start_activities, end_activities, activities))
-        skippable.append(False)
-    return [dfgs, skippable]
+    @classmethod
+    def project(cls, obj: UCL, groups: List[Collection[Any]]) -> List[UCL]:
+        l = []
+        for g in groups:
+            ucl = UCL()
+            ucl.append([list(filter(lambda e: e in g, t)) for t in obj])
+            l.append(ucl)
+        return l
+
+
+class ConcurrencyDFGCut(ConcurrencyCut[DFG]):
+
+    @classmethod
+    def project(cls, obj: DFG, groups: List[Collection[Any]]) -> Tuple[List[DFG], List[bool]]:
+        dfgs = []
+        skippable = []
+        for g in groups:
+            dfn = DFG()
+            for (a, f) in obj.start_activities:
+                if a in g:
+                    dfn.start_activities.append((a, f))
+            for (a, f) in obj.end_activities:
+                if a in g:
+                    dfn.end_activities.append((a, f))
+            for (a, b, f) in obj.graph:
+                if a in g and b in g:
+                    dfn.graph.append((a, b, f))
+            skippable.append(False)
+        return dfgs, skippable
