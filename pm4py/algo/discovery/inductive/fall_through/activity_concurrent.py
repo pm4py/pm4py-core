32ad5867ee45193e427361ee41084e2cecb173e1
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2022-08-09 13:05:03 +0200

    fix(PMPY-1799): fix cyclic import for activity concurrent fall through

--- pm4py/algo/discovery/inductive/fall_through/activity_concurrent.py
+++ pm4py/algo/discovery/inductive/fall_through/activity_concurrent.py
@@ -1,7 +1,10 @@
 from typing import Optional, Tuple, List
 
-from pm4py.algo.discovery.inductive.fall_through.abc import FallThrough, T
-from pm4py.algo.discovery.inductive.variants.im import IM
+from pm4py.algo.discovery.inductive.cuts.concurrency import ConcurrencyLogCut
+from pm4py.algo.discovery.inductive.cuts.loop import LoopLogCut
+from pm4py.algo.discovery.inductive.cuts.sequence import SequenceLogCut
+from pm4py.algo.discovery.inductive.cuts.xor import ExclusiveChoiceLogCut
+from pm4py.algo.discovery.inductive.fall_through.abc import FallThrough
 from pm4py.objects.process_tree.obj import ProcessTree, Operator
 from pm4py.util.compression import util as comut
 from pm4py.util.compression.dtypes import UCL
@@ -15,11 +18,20 @@ class ActivityConcurrent(FallThrough[UCL]):
         cc = set()
         for a in candidates:
             l_alt = [list(filter(lambda e: e != a, t)) for t in log]
-            cut = IM().find_cut(l_alt)
+            cut = cls._find_cut(l_alt)
             if cut is not None:
                 cc.add(a)
                 break
         return cc
+
+    @classmethod
+    def _find_cut(cls, log: UCL) -> Optional[Tuple[ProcessTree, List[UCL]]]:
+        # TODO: use some factory method to obtain the applicable cuts automatically
+        dfg = comut.discover_dfg(log)
+        g = ExclusiveChoiceLogCut.apply(log, dfg)
+        g = SequenceLogCut.apply(log, dfg) if g is None else g
+        g = ConcurrencyLogCut.apply(log, dfg) if g is None else g
+        return LoopLogCut.apply(log, dfg) if g is None else g
 
     @classmethod
     def holds(cls, log: UCL) -> bool:
