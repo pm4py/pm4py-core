5fc3e663e9b12a8464b9f8927d9f296c972b6e53
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2022-08-09 13:20:09 +0200

    feat(PMPY-1799): account for start/end activities in alphabet of dfg; add transitive relations

--- pm4py/objects/dfg/util.py
+++ pm4py/objects/dfg/util.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from typing import Dict, Collection, Any
+from typing import Dict, Collection, Any, Tuple
 
 from pm4py.objects.dfg.obj import DFG
 
@@ -14,6 +14,8 @@ def get_vertices(dfg: DFG) -> Collection[Any]:
     """
     alphabet = set()
     [alphabet.update({a, b}) for (a, b, f) in dfg.graph]
+    [alphabet.update({a}) for (a, f) in dfg.start_activities]
+    [alphabet.update({a}) for (a, f) in dfg.end_activities]
     return alphabet
 
 
@@ -77,3 +79,62 @@ def get_sink_vertices(dfg: DFG) -> Collection[Any]:
     outgoing = get_outgoing_arcs(dfg)
     [ends.add(a) for a in outgoing if len(outgoing[a]) == 0]
     return ends
+
+
+def get_transitive_relations(dfg: DFG) -> Tuple[Dict[Any, Collection[Any]], Dict[Any, Collection[Any]]]:
+    '''
+    Computes the full transitive relations in both directions (all activities reachable from a given activity and all
+    activities that can reach the activity)
+
+    :param dfg: ``DFG`` object
+
+    :rtype: ``Tuple[Dict[Any, Collection[Any]], Dict[Any, Collection[Any]]] first argument maps an activity on all other
+    activities that are able to reach the activity ('transitive pre set')
+        second argument maps an activity on all other activities that it can reach (transitively) ('transitive post set')
+    '''
+    alph = get_vertices(dfg)
+    pre = {a: set() for a in alph}
+    post = {a: set() for a in alph}
+    if len(dfg.graph) > 0:
+        q = [(a, b) for (a, b, f) in dfg.graph]
+        while len(q) > 0:
+            s, t = q.pop(0)
+            post[s].add(t)
+            pre[t].add(s)
+            post[s].update(post[t])
+            pre[t].update(pre[s])
+            for a, b, f in dfg.graph:
+                if b == s and not post[s].issubset(post[a]):
+                    post[a].update(post[s])
+                    q.append((a, b))
+                if a == t and not pre[t].issubset(pre[b]):
+                    pre[b].update(pre[t])
+                    q.append((a, b))
+    return pre, post
+
+
+def get_vertex_frequencies(dfg: DFG) -> Counter[Any]:
+    '''
+    Computes the number of times a vertex in the dfg is visited.
+    The number equals the number of occurrences in the underlying log and is computed by summing up the incoming
+    arc frequency and the number of starts in the vertex. The value is equal to the number of outgoing arcs combined
+    with the number of endings of the vertex.
+    '''
+    c = Counter()
+    for v in get_vertices(dfg):
+        c[v] = 0
+    for (a, b, f) in dfg.graph:
+        c[v] += f
+    for (a, f) in dfg.start_activities:
+        c[v] += f
+    return c
+
+
+def as_nx_graph(dfg: DFG):
+    import networkx as nx
+
+    nx_graph = nx.DiGraph()
+    nx_graph.add_nodes_from(get_vertices(dfg))
+    for a, b, f in dfg.graph:
+        nx_graph.add_edge(a, b)
+    return nx_graph
