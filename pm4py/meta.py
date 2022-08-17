c0e81d4b09a962bd16e5524fa9291cf56b17cc5a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-08-17 10:52:13 +0200

    fix(pm4py): updating version in meta

--- pm4py/meta.py
+++ pm4py/meta.py
@@ -1,5 +1,5 @@
 __name__ = 'pm4py'
-VERSION = '2.2.26'
+VERSION = '2.2.27'
 __version__ = VERSION
 __doc__ = 'Process Mining for Python (PM4Py)'
 __author__ = 'Fraunhofer Institute for Applied Technology'
