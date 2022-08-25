308e86713758af80a0d5a91894046ad1b7b3264a
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2022-08-25 11:01:32 +0200

    feat(PMPY-1799): push cut/bc/ft detection to the factory

--- pm4py/algo/discovery/inductive/cuts/factory.py
+++ pm4py/algo/discovery/inductive/cuts/factory.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional, Tuple, TypeVar
 
 from pm4py.algo.discovery.inductive.cuts.abc import Cut
 from pm4py.algo.discovery.inductive.cuts.concurrency import ConcurrencyCutUVCL, ConcurrencyCutDFG
@@ -7,12 +7,16 @@ from pm4py.algo.discovery.inductive.cuts.sequence import StrictSequenceCutUVCL, 
 from pm4py.algo.discovery.inductive.cuts.xor import ExclusiveChoiceCutUVCL, ExclusiveChoiceCutDFG
 from pm4py.algo.discovery.inductive.dtypes.im_ds import IMDataStructure, IMDataStructureUVCL, IMDataStructureDFG
 from pm4py.algo.discovery.inductive.variants.instances import IMInstance
+from pm4py.objects.process_tree.obj import ProcessTree
+
+T = TypeVar('T', bound=IMDataStructure)
+S = TypeVar('S', bound=Cut)
 
 
 class CutFactory:
 
     @classmethod
-    def get_cuts(cls, obj: IMDataStructure, inst: IMInstance) -> List[Cut]:
+    def get_cuts(cls, obj: T, inst: IMInstance) -> List[S]:
         if inst is IMInstance.IM:
             if type(obj) is IMDataStructureUVCL:
                 return [ExclusiveChoiceCutUVCL, StrictSequenceCutUVCL, ConcurrencyCutUVCL, LoopCutUVCL]
@@ -20,3 +24,11 @@ class CutFactory:
             if type(obj) is IMDataStructureDFG:
                 return [ExclusiveChoiceCutDFG, StrictSequenceCutDFG, ConcurrencyCutDFG, LoopCutDFG]
         return list()
+
+    @classmethod
+    def find_cut(cls, obj: IMDataStructure, inst: IMInstance) -> Optional[Tuple[ProcessTree, List[T]]]:
+        for c in CutFactory.get_cuts(obj, inst):
+            r = c.apply(obj)
+            if r is not None:
+                return r
+        return None
