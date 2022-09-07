d06aad52bace64d6d7c1bf01ce75d211227a602b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-09-07 10:55:01 +0200

    fix(pm4py): fixed flower detection

--- pm4py/algo/discovery/inductive/fall_through/flower.py
+++ pm4py/algo/discovery/inductive/fall_through/flower.py
@@ -2,11 +2,13 @@ from collections import Counter
 from multiprocessing import Pool, Manager
 from typing import Optional, Tuple, List
 
-from pm4py.algo.discovery.inductive.dtypes.im_ds import IMDataStructureUVCL
+from pm4py.algo.discovery.inductive.dtypes.im_ds import IMDataStructureUVCL, IMDataStructureDFG
 from pm4py.algo.discovery.inductive.fall_through.abc import FallThrough
 from pm4py.algo.discovery.inductive.fall_through.empty_traces import EmptyTracesUVCL
 from pm4py.objects.process_tree.obj import ProcessTree, Operator
 from pm4py.util.compression import util as comut
+from pm4py.objects.dfg.obj import DFG
+from pm4py.algo.discovery.inductive.dtypes.im_dfg import InductiveDFG
 
 
 class FlowerModelUVCL(FallThrough[IMDataStructureUVCL]):
@@ -19,6 +21,38 @@ class FlowerModelUVCL(FallThrough[IMDataStructureUVCL]):
     def apply(cls, obj: IMDataStructureUVCL, pool: Pool = None, manager: Manager = None) -> Optional[
         Tuple[ProcessTree, List[IMDataStructureUVCL]]]:
         log = obj.data_structure
-        logs = [IMDataStructureUVCL(Counter({(a,): 1})) for a in comut.get_alphabet(log)]
-        logs.append(IMDataStructureUVCL(Counter()))
-        return ProcessTree(operator=Operator.LOOP), logs
+        loop = ProcessTree(operator=Operator.LOOP)
+        xor = ProcessTree(operator=Operator.XOR)
+        skip = ProcessTree()
+        loop.children.append(xor)
+        loop.children.append(skip)
+        xor.parent = loop
+        skip.parent = loop
+        for a in comut.get_alphabet(log):
+            act_leaf = ProcessTree(label=a)
+            act_leaf.parent = xor
+            xor.children.append(act_leaf)
+        return loop, []
+
+
+class FlowerModelDFG(FallThrough[IMDataStructureDFG]):
+    @classmethod
+    def holds(cls, obj: IMDataStructureDFG) -> bool:
+        return True
+
+    @classmethod
+    def apply(cls, obj: IMDataStructureDFG, pool: Pool = None, manager: Manager = None) -> Optional[
+        Tuple[ProcessTree, List[IMDataStructureDFG]]]:
+        activities = set(obj.dfg.start_activities).union(set(obj.dfg.end_activities)).union(set(x[0] for x in obj.dfg.graph)).union(set(x[1] for x in obj.dfg.graph))
+        loop = ProcessTree(operator=Operator.LOOP)
+        xor = ProcessTree(operator=Operator.XOR)
+        skip = ProcessTree()
+        loop.children.append(xor)
+        loop.children.append(skip)
+        xor.parent = loop
+        skip.parent = loop
+        for a in activities:
+            act_leaf = ProcessTree(label=a)
+            act_leaf.parent = xor
+            xor.children.append(act_leaf)
+        return loop, []
