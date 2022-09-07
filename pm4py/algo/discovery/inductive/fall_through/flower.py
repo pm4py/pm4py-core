6f9f2709f663c8ae9a0597f534faee4b2f697c63
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-09-07 12:02:33 +0200

    fix(pm4py): proper fixing flower fallthrough

--- pm4py/algo/discovery/inductive/fall_through/flower.py
+++ pm4py/algo/discovery/inductive/fall_through/flower.py
@@ -6,6 +6,9 @@ from pm4py.algo.discovery.inductive.fall_through.abc import FallThrough
 from pm4py.algo.discovery.inductive.fall_through.empty_traces import EmptyTracesUVCL
 from pm4py.objects.process_tree.obj import ProcessTree, Operator
 from pm4py.util.compression import util as comut
+from pm4py.util.compression.dtypes import UVCL
+from pm4py.objects.dfg.obj import DFG
+from pm4py.algo.discovery.inductive.dtypes.im_dfg import InductiveDFG
 
 
 class FlowerModelUVCL(FallThrough[IMDataStructureUVCL]):
@@ -18,18 +21,13 @@ class FlowerModelUVCL(FallThrough[IMDataStructureUVCL]):
     def apply(cls, obj: IMDataStructureUVCL, pool: Pool = None, manager: Manager = None) -> Optional[
         Tuple[ProcessTree, List[IMDataStructureUVCL]]]:
         log = obj.data_structure
-        loop = ProcessTree(operator=Operator.LOOP)
-        xor = ProcessTree(operator=Operator.XOR)
-        skip = ProcessTree()
-        loop.children.append(xor)
-        loop.children.append(skip)
-        xor.parent = loop
-        skip.parent = loop
+        uvcl_do = UVCL()
         for a in comut.get_alphabet(log):
-            act_leaf = ProcessTree(label=a)
-            act_leaf.parent = xor
-            xor.children.append(act_leaf)
-        return loop, []
+            uvcl_do[(a,)] = 1
+        uvcl_redo = UVCL()
+        im_uvcl_do = IMDataStructureUVCL(uvcl_do)
+        im_uvcl_redo = IMDataStructureUVCL(uvcl_redo)
+        return ProcessTree(operator=Operator.LOOP), [im_uvcl_do, im_uvcl_redo]
 
 
 class FlowerModelDFG(FallThrough[IMDataStructureDFG]):
@@ -41,15 +39,12 @@ class FlowerModelDFG(FallThrough[IMDataStructureDFG]):
     def apply(cls, obj: IMDataStructureDFG, pool: Pool = None, manager: Manager = None) -> Optional[
         Tuple[ProcessTree, List[IMDataStructureDFG]]]:
         activities = set(obj.dfg.start_activities).union(set(obj.dfg.end_activities)).union(set(x[0] for x in obj.dfg.graph)).union(set(x[1] for x in obj.dfg.graph))
-        loop = ProcessTree(operator=Operator.LOOP)
-        xor = ProcessTree(operator=Operator.XOR)
-        skip = ProcessTree()
-        loop.children.append(xor)
-        loop.children.append(skip)
-        xor.parent = loop
-        skip.parent = loop
+        dfg_do = DFG()
         for a in activities:
-            act_leaf = ProcessTree(label=a)
-            act_leaf.parent = xor
-            xor.children.append(act_leaf)
-        return loop, []
+            dfg_do.start_activities[a] = 1
+            dfg_do.end_activities[a] = 1
+        dfg_redo = DFG()
+        im_dfg_do = IMDataStructureDFG(InductiveDFG(dfg_do))
+        im_dfg_redo = IMDataStructureDFG(InductiveDFG(dfg_redo))
+        return ProcessTree(operator=Operator.LOOP), [im_dfg_do, im_dfg_redo]
+
