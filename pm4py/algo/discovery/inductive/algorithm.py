aba60ea1ba5c85ecafd45dad01749eb38774dfdf
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-09-07 11:55:22 +0200

    feat(pm4py): inductive miner application on variants

--- pm4py/algo/discovery/inductive/algorithm.py
+++ pm4py/algo/discovery/inductive/algorithm.py
@@ -16,6 +16,7 @@ from pm4py.util import constants
 from pm4py.util import exec_utils
 from pm4py.util import xes_constants as xes_util
 from pm4py.util.compression import util as comut
+from pm4py.util.compression.dtypes import UVCL
 import warnings
 
 
@@ -31,7 +32,7 @@ class Variants(Enum):
     IMd = IMInstance.IMd
 
 
-def apply(obj: Union[EventLog, pd.DataFrame, DFG], parameters: Optional[Dict[Any, Any]] = None, variant=Variants.IM) -> ProcessTree:
+def apply(obj: Union[EventLog, pd.DataFrame, DFG, UVCL], parameters: Optional[Dict[Any, Any]] = None, variant=Variants.IM) -> ProcessTree:
     if parameters is None:
         parameters = {}
     if type(obj) not in [EventLog, pd.DataFrame, DFG]:
@@ -39,8 +40,11 @@ def apply(obj: Union[EventLog, pd.DataFrame, DFG], parameters: Optional[Dict[Any
     ack = exec_utils.get_param_value(Parameters.ACTIVITY_KEY, parameters, xes_util.DEFAULT_NAME_KEY)
     tk = exec_utils.get_param_value(Parameters.TIMESTAMP_KEY, parameters, xes_util.DEFAULT_TIMESTAMP_KEY)
     cidk = exec_utils.get_param_value(Parameters.CASE_ID_KEY, parameters, pmutil.constants.CASE_CONCEPT_NAME)
-    if type(obj) in [EventLog, pd.DataFrame]:
-        uvcl = comut.get_variants(comut.project_univariate(obj, key=ack, df_glue=cidk, df_sorting_criterion_key=tk))
+    if type(obj) in [EventLog, pd.DataFrame, UVCL]:
+        if type(obj) in [EventLog, pd.DataFrame]:
+            uvcl = comut.get_variants(comut.project_univariate(obj, key=ack, df_glue=cidk, df_sorting_criterion_key=tk))
+        else:
+            uvcl = obj
         if variant is Variants.IM:
             im = IMUVCL()
             return im.apply(IMDataStructureUVCL(uvcl))
@@ -48,7 +52,7 @@ def apply(obj: Union[EventLog, pd.DataFrame, DFG], parameters: Optional[Dict[Any
             imd = IMD()
             idfg = InductiveDFG(dfg=comut.discover_dfg_uvcl(uvcl), skip=() in uvcl)
             return imd.apply(IMDataStructureDFG(idfg))
-    if type(obj) is DFG:
+    elif type(obj) is DFG:
         if variant is not Variants.IMd:
             warnings.warn('Inductive Miner Variant requested for DFG artefact is not IMD, resorting back to IMD')
         imd = IMD()
