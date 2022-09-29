949fd7a4ef6c662fa99dbb76c217adfb960cda3d
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-09-29 14:24:45 +0200

    feat(pm4py): new command line utilities

--- pm4py/cli.py
+++ pm4py/cli.py
@@ -6,70 +6,73 @@ from pathlib import Path
 import traceback
 import pandas as pd
 
-
 methods = {
     "ConvertToXES": {"inputs": [".csv"], "output_extension": ".xes",
-                          "method": lambda x: pm4py.write_xes(
-                              pm4py.convert_to_event_log(pm4py.format_dataframe(pd.read_csv(x[0]))), x[1])},
+                     "method": lambda x: pm4py.write_xes(
+                         pm4py.convert_to_event_log(pm4py.format_dataframe(pd.read_csv(x[0]))), x[1])},
     "ConvertToCSV": {"inputs": [".xes"], "output_extension": ".csv",
                      "method": lambda x: pm4py.convert_to_dataframe(pm4py.read_xes(x[0])).to_csv(x[1], index=False)},
     "ConvertPNMLtoBPMN": {"inputs": [".pnml"], "output_extension": ".bpmn",
-                           "method": lambda x: pm4py.write_bpmn(pm4py.convert_to_bpmn(*pm4py.read_pnml(x[0])), x[1])},
+                          "method": lambda x: pm4py.write_bpmn(pm4py.convert_to_bpmn(*pm4py.read_pnml(x[0])), x[1])},
     "ConvertPNMLtoPTML": {"inputs": [".pnml"], "output_extension": ".ptml",
-                          "method": lambda x: pm4py.write_ptml(pm4py.convert_to_process_tree(*pm4py.read_pnml(x[0])), x[1])},
+                          "method": lambda x: pm4py.write_ptml(pm4py.convert_to_process_tree(*pm4py.read_pnml(x[0])),
+                                                               x[1])},
     "ConvertPTMLtoPNML": {"inputs": [".ptml"], "output_extension": ".pnml",
-                          "method": lambda x: pm4py.write_pnml(*pm4py.convert_to_petri_net(pm4py.read_ptml(x[0])), x[1])},
+                          "method": lambda x: pm4py.write_pnml(*pm4py.convert_to_petri_net(pm4py.read_ptml(x[0])),
+                                                               x[1])},
     "ConvertPTMLtoBPMN": {"inputs": [".ptml"], "output_extension": ".bpmn",
                           "method": lambda x: pm4py.write_bpmn(pm4py.convert_to_bpmn(pm4py.read_ptml(x[0])), x[1])},
     "ConvertBPMNtoPNML": {"inputs": [".bpmn"], "output_extension": ".pnml",
-                          "method": lambda x: pm4py.write_pnml(*pm4py.convert_to_petri_net(pm4py.read_bpmn(x[0])), x[1])},
+                          "method": lambda x: pm4py.write_pnml(*pm4py.convert_to_petri_net(pm4py.read_bpmn(x[0])),
+                                                               x[1])},
     "ConvertDFGtoPNML": {"inputs": [".dfg"], "output_extension": ".pnml",
-                         "method": lambda x: pm4py.write_pnml(*pm4py.convert_to_petri_net(*pm4py.read_dfg(x[0])), x[1])},
+                         "method": lambda x: pm4py.write_pnml(*pm4py.convert_to_petri_net(*pm4py.read_dfg(x[0])),
+                                                              x[1])},
     "DiscoverPetriNetAlpha": {"inputs": [".xes"], "output_extension": ".pnml",
-                              "method": lambda x: pm4py.write_pnml(*pm4py.discover_petri_net_alpha(read_log(x[0])),
+                              "method": lambda x: pm4py.write_pnml(*pm4py.discover_petri_net_alpha(__read_log(x[0])),
                                                                    x[1])},
     "DiscoverPetriNetInductive": {"inputs": [".xes"], "output_extension": ".pnml",
                                   "method": lambda x: pm4py.write_pnml(
-                                      *pm4py.discover_petri_net_inductive(read_log(x[0])),
+                                      *pm4py.discover_petri_net_inductive(__read_log(x[0])),
                                       x[1])},
     "DiscoverPetriNetHeuristics": {"inputs": [".xes"], "output_extension": ".pnml",
                                    "method": lambda x: pm4py.write_pnml(
-                                       *pm4py.discover_petri_net_heuristics(read_log(x[0])),
+                                       *pm4py.discover_petri_net_heuristics(__read_log(x[0])),
                                        x[1])},
     "DiscoverBPMNInductive": {"inputs": [".xes"], "output_extension": ".bpmn",
                               "method": lambda x: pm4py.write_bpmn(
-                                  pm4py.discover_bpmn_inductive(read_log(x[0])),
+                                  pm4py.discover_bpmn_inductive(__read_log(x[0])),
                                   x[1])},
     "DiscoverProcessTreeInductive": {"inputs": [".xes"], "output_extension": ".ptml",
-                              "method": lambda x: pm4py.write_ptml(
-                                  pm4py.discover_process_tree_inductive(read_log(x[0])),
-                                  x[1])},
+                                     "method": lambda x: pm4py.write_ptml(
+                                         pm4py.discover_process_tree_inductive(__read_log(x[0])),
+                                         x[1])},
     "DiscoverDFG": {"inputs": [".xes"], "output_extension": ".dfg",
-                    "method": lambda x: pm4py.write_dfg(*pm4py.discover_dfg(read_log(x[0])), x[1])},
+                    "method": lambda x: pm4py.write_dfg(*pm4py.discover_dfg(__read_log(x[0])), x[1])},
     "ConformanceDiagnosticsTBR": {"inputs": [".xes", ".pnml"], "output_extension": ".txt",
                                   "method": lambda x: open(x[2], "w").write(str(
-                                      pm4py.conformance_diagnostics_token_based_replay(read_log(x[0]),
+                                      pm4py.conformance_diagnostics_token_based_replay(__read_log(x[0]),
                                                                                        *pm4py.read_pnml(x[1]))))},
     "ConformanceDiagnosticsAlignments": {"inputs": [".xes", ".pnml"], "output_extension": ".txt",
-                                  "method": lambda x: open(x[2], "w").write(str(
-                                      pm4py.conformance_diagnostics_alignments(read_log(x[0]),
-                                                                                       *pm4py.read_pnml(x[1]))))},
-    "FitnessTBR": {"inputs": [".xes", ".pnml"], "output_extension": ".txt",
-                                  "method": lambda x: open(x[2], "w").write(str(
-                                      pm4py.fitness_token_based_replay(read_log(x[0]),
-                                                                                       *pm4py.read_pnml(x[1]))))},
-    "FitnessAlignments": {"inputs": [".xes", ".pnml"], "output_extension": ".txt",
                                          "method": lambda x: open(x[2], "w").write(str(
-                                             pm4py.fitness_alignments(read_log(x[0]),
+                                             pm4py.conformance_diagnostics_alignments(__read_log(x[0]),
                                                                                       *pm4py.read_pnml(x[1]))))},
-    "PrecisionTBR": {"inputs": [".xes", ".pnml"], "output_extension": ".txt",
+    "FitnessTBR": {"inputs": [".xes", ".pnml"], "output_extension": ".txt",
                    "method": lambda x: open(x[2], "w").write(str(
-                       pm4py.precision_token_based_replay(read_log(x[0]),
+                       pm4py.fitness_token_based_replay(__read_log(x[0]),
                                                         *pm4py.read_pnml(x[1]))))},
-    "PrecisionAlignments": {"inputs": [".xes", ".pnml"], "output_extension": ".txt",
+    "FitnessAlignments": {"inputs": [".xes", ".pnml"], "output_extension": ".txt",
                           "method": lambda x: open(x[2], "w").write(str(
-                              pm4py.precision_alignments(read_log(x[0]),
+                              pm4py.fitness_alignments(__read_log(x[0]),
                                                        *pm4py.read_pnml(x[1]))))},
+    "PrecisionTBR": {"inputs": [".xes", ".pnml"], "output_extension": ".txt",
+                     "method": lambda x: open(x[2], "w").write(str(
+                         pm4py.precision_token_based_replay(__read_log(x[0]),
+                                                            *pm4py.read_pnml(x[1]))))},
+    "PrecisionAlignments": {"inputs": [".xes", ".pnml"], "output_extension": ".txt",
+                            "method": lambda x: open(x[2], "w").write(str(
+                                pm4py.precision_alignments(__read_log(x[0]),
+                                                           *pm4py.read_pnml(x[1]))))},
     "SaveVisDFG": {"inputs": [".dfg"], "output_extension": ".png",
                    "method": lambda x: pm4py.save_vis_dfg(*pm4py.read_dfg(x[0]), x[1])},
     "SaveVisPNML": {"inputs": [".pnml"], "output_extension": ".png",
@@ -79,22 +82,64 @@ methods = {
     "SaveVisPTML": {"inputs": [".ptml"], "output_extension": ".png",
                     "method": lambda x: pm4py.save_vis_process_tree(pm4py.read_ptml(x[0]), x[1])},
     "SaveVisDottedChart": {"inputs": [".xes", None, None, None], "output_extension": ".png",
-                           "method": lambda x: pm4py.save_vis_dotted_chart(read_log(x[0]), x[4], attributes=[x[1], x[2], x[3]])},
+                           "method": lambda x: pm4py.save_vis_dotted_chart(__read_log(x[0]), x[4],
+                                                                           attributes=[x[1], x[2], x[3]])},
     "SaveVisTransitionSystem": {"inputs": [".xes"], "output_extension": ".png",
-                                "method": lambda x: pm4py.save_vis_transition_system(pm4py.discover_transition_system(read_log(x[0])), x[1])},
-    "SaveVisTrie": {"inputs": [".xes"], "output_extension": ".png", "method": lambda x: pm4py.save_vis_prefix_tree(pm4py.discover_prefix_tree(read_log(x[0])), x[1])},
-    "SaveVisEventsDistribution": {"inputs": [".xes", None], "output_extension": ".png", "method": lambda x: pm4py.save_vis_events_distribution_graph(read_log(x[0]), x[2], distr_type=x[1])},
-    "SaveVisEventsPerTime": {"inputs": [".xes"], "output_extension": ".png", "method": lambda x: pm4py.save_vis_events_per_time_graph(read_log(x[0]), x[1])}
+                                "method": lambda x: pm4py.save_vis_transition_system(
+                                    pm4py.discover_transition_system(__read_log(x[0])), x[1])},
+    "SaveVisTrie": {"inputs": [".xes"], "output_extension": ".png",
+                    "method": lambda x: pm4py.save_vis_prefix_tree(pm4py.discover_prefix_tree(__read_log(x[0])), x[1])},
+    "SaveVisEventsDistribution": {"inputs": [".xes", None], "output_extension": ".png",
+                                  "method": lambda x: pm4py.save_vis_events_distribution_graph(__read_log(x[0]), x[2],
+                                                                                               distr_type=x[1])},
+    "SaveVisEventsPerTime": {"inputs": [".xes"], "output_extension": ".png",
+                             "method": lambda x: pm4py.save_vis_events_per_time_graph(__read_log(x[0]), x[1])},
+    "GenerateProcessTree": {"inputs": [None], "output_extension": ".ptml", "method": lambda x: pm4py.write_ptml(
+        pm4py.generate_process_tree(parameters={"min": int(x[0]), "max": int(x[0]), "mode": int(x[0])}), x[1])},
+    "PNMLplayout": {"inputs": [".pnml"], "output_extension": ".xes",
+                    "method": lambda x: pm4py.write_xes(pm4py.play_out(*pm4py.read_pnml(x[0])), x[1])},
+    "PTMLplayout": {"inputs": [".ptml"], "output_extension": ".xes",
+                    "method": lambda x: pm4py.write_xes(pm4py.play_out(pm4py.read_ptml(x[0])), x[1])},
+    "DFGplayout": {"inputs": [".dfg"], "output_extension": ".dfg",
+                   "method": lambda x: pm4py.write_xes(pm4py.play_out(*pm4py.read_dfg(x[0])), x[1])},
+    "SaveVisSNA": {"inputs": [".xes", None], "output_extension": ".png",
+                   "method": lambda x: pm4py.save_vis_sna(__apply_sna(__read_log(x[0]), x[1]), x[2])},
+    "SaveVisCaseDuration": {"inputs": [".xes"], "output_extension": ".png",
+                            "method": lambda x: pm4py.save_vis_case_duration_graph(__read_log(x[0]), x[1])},
+    "FilterVariantsTopK": {"inputs": [".xes", None], "output_extension": ".xes",
+                           "method": lambda x: pm4py.write_xes(pm4py.filter_variants_top_k(__read_log(x[0]), int(x[1])),
+                                                               x[2])},
+    "FilterVariantsCoverage": {"inputs": [".xes", None], "output_extension": ".xes",
+                               "method": lambda x: pm4py.write_xes(
+                                   pm4py.filter_variants_by_coverage_percentage(__read_log(x[0]), float(x[1])), x[2])},
+    "FilterCasePerformance": {"inputs": [".xes", None, None], "output_extension": ".xes",
+                              "method": lambda x: pm4py.write_xes(
+                                  pm4py.filter_case_performance(__read_log(x[0]), min_performance=float(x[1]),
+                                                                max_performance=float(x[2])), x[3])},
+    "FilterTimeRange": {"inputs": [".xes", None, None], "output_extension": ".xes",
+                        "method": lambda x: pm4py.write_xes(
+                            pm4py.filter_time_range(__read_log(x[0]), x[1] + " 00:00:00", x[2] + " 23:59:59"), x[3])}
 }
 
 
-def read_log(log_path):
+def __read_log(log_path):
     if "xes" in log_path.lower():
         return pm4py.read_xes(log_path)
     elif "csv" in log_path.lower():
         dataframe = pd.read_csv(log_path)
         dataframe = pm4py.format_dataframe(dataframe)
         return dataframe
+
+
+def __apply_sna(log, method, **kwargs):
+    if method == "handover":
+        return pm4py.discover_handover_of_work_network(log, **kwargs)
+    elif method == "working_together":
+        return pm4py.discover_working_together_network(log, **kwargs)
+    elif method == "similar_activities":
+        return pm4py.discover_activity_based_resource_similarity(log, **kwargs)
+    elif method == "subcontracting":
+        return pm4py.discover_subcontracting_network(log, **kwargs)
 
 
 def __get_output_name(inp_list, idx, method_name, extension):
@@ -126,8 +171,10 @@ def cli_interface():
         else:
             if not os.path.exists(sys.argv[j]):
                 os.mkdir(sys.argv[j])
-            outputs = [[os.path.join(sys.argv[j], __get_output_name(inputs[z], z, method_name, method["output_extension"]))] for z in
-                       range(len(inputs))]
+            outputs = [
+                [os.path.join(sys.argv[j], __get_output_name(inputs[z], z, method_name, method["output_extension"]))]
+                for z in
+                range(len(inputs))]
         method_tuples = [(*inputs[i], *outputs[i]) for i in range(len(inputs))]
         for i in range(len(method_tuples)):
             try:
@@ -137,7 +184,7 @@ def cli_interface():
             except:
                 traceback.print_exc()
     else:
-        raise Exception("the provided method ("+method_name+") does not exist in the CLI.")
+        raise Exception("the provided method (" + method_name + ") does not exist in the CLI.")
 
 
 if __name__ == "__main__":
