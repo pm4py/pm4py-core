f3bfdad87cfda15b76ecf2f1bad482c77b5b98e5
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2022-09-29 09:23:18 +0200

    feat(pm4py): added features to CLI

--- pm4py/cli.py
+++ pm4py/cli.py
@@ -13,6 +13,18 @@ methods = {
                               pm4py.convert_to_event_log(pm4py.format_dataframe(pd.read_csv(x[0]))), x[1])},
     "ConvertToCSV": {"inputs": [".xes"], "output_extension": ".csv",
                      "method": lambda x: pm4py.convert_to_dataframe(pm4py.read_xes(x[0])).to_csv(x[1], index=False)},
+    "ConvertPNMLtoBPMN": {"inputs": [".pnml"], "output_extension": ".bpmn",
+                           "method": lambda x: pm4py.write_bpmn(pm4py.convert_to_bpmn(*pm4py.read_pnml(x[0])), x[1])},
+    "ConvertPNMLtoPTML": {"inputs": [".pnml"], "output_extension": ".ptml",
+                          "method": lambda x: pm4py.write_ptml(pm4py.convert_to_process_tree(*pm4py.read_pnml(x[0])), x[1])},
+    "ConvertPTMLtoPNML": {"inputs": [".ptml"], "output_extension": ".pnml",
+                          "method": lambda x: pm4py.write_pnml(*pm4py.convert_to_petri_net(pm4py.read_ptml(x[0])), x[1])},
+    "ConvertPTMLtoBPMN": {"inputs": [".ptml"], "output_extension": ".bpmn",
+                          "method": lambda x: pm4py.write_bpmn(pm4py.convert_to_bpmn(pm4py.read_ptml(x[0])), x[1])},
+    "ConvertBPMNtoPNML": {"inputs": [".bpmn"], "output_extension": ".pnml",
+                          "method": lambda x: pm4py.write_pnml(*pm4py.convert_to_petri_net(pm4py.read_bpmn(x[0])), x[1])},
+    "ConvertDFGtoPNML": {"inputs": [".dfg"], "output_extension": ".pnml",
+                         "method": lambda x: pm4py.write_pnml(*pm4py.convert_to_petri_net(*pm4py.read_dfg(x[0])), x[1])},
     "DiscoverPetriNetAlpha": {"inputs": [".xes"], "output_extension": ".pnml",
                               "method": lambda x: pm4py.write_pnml(*pm4py.discover_petri_net_alpha(read_log(x[0])),
                                                                    x[1])},
@@ -32,6 +44,8 @@ methods = {
                               "method": lambda x: pm4py.write_ptml(
                                   pm4py.discover_process_tree_inductive(read_log(x[0])),
                                   x[1])},
+    "DiscoverDFG": {"inputs": [".xes"], "output_extension": ".dfg",
+                    "method": lambda x: pm4py.write_dfg(*pm4py.discover_dfg(read_log(x[0])), x[1])},
     "ConformanceDiagnosticsTBR": {"inputs": [".xes", ".pnml"], "output_extension": ".txt",
                                   "method": lambda x: open(x[2], "w").write(str(
                                       pm4py.conformance_diagnostics_token_based_replay(read_log(x[0]),
@@ -56,6 +70,21 @@ methods = {
                           "method": lambda x: open(x[2], "w").write(str(
                               pm4py.precision_alignments(read_log(x[0]),
                                                        *pm4py.read_pnml(x[1]))))},
+    "SaveVisDFG": {"inputs": [".dfg"], "output_extension": ".png",
+                   "method": lambda x: pm4py.save_vis_dfg(*pm4py.read_dfg(x[0]), x[1])},
+    "SaveVisPNML": {"inputs": [".pnml"], "output_extension": ".png",
+                    "method": lambda x: pm4py.save_vis_petri_net(*pm4py.read_pnml(x[0]), x[1])},
+    "SaveVisBPMN": {"inputs": [".bpmn"], "output_extension": ".png",
+                    "method": lambda x: pm4py.save_vis_bpmn(pm4py.read_bpmn(x[0]), x[1])},
+    "SaveVisPTML": {"inputs": [".ptml"], "output_extension": ".png",
+                    "method": lambda x: pm4py.save_vis_process_tree(pm4py.read_ptml(x[0]), x[1])},
+    "SaveVisDottedChart": {"inputs": [".xes", None, None, None], "output_extension": ".png",
+                           "method": lambda x: pm4py.save_vis_dotted_chart(read_log(x[0]), x[4], attributes=[x[1], x[2], x[3]])},
+    "SaveVisTransitionSystem": {"inputs": [".xes"], "output_extension": ".png",
+                                "method": lambda x: pm4py.save_vis_transition_system(pm4py.discover_transition_system(read_log(x[0])), x[1])},
+    "SaveVisTrie": {"inputs": [".xes"], "output_extension": ".png", "method": lambda x: pm4py.save_vis_prefix_tree(pm4py.discover_prefix_tree(read_log(x[0])), x[1])},
+    "SaveVisEventsDistribution": {"inputs": [".xes", None], "output_extension": ".png", "method": lambda x: pm4py.save_vis_events_distribution_graph(read_log(x[0]), x[2], distr_type=x[1])},
+    "SaveVisEventsPerTime": {"inputs": [".xes"], "output_extension": ".png", "method": lambda x: pm4py.save_vis_events_per_time_graph(read_log(x[0]), x[1])}
 }
 
 
@@ -82,9 +111,9 @@ def cli_interface():
         inputs = []
         for i in range(len(method["inputs"])):
             ci = sys.argv[2 + i]
-            if not os.path.exists(ci):
-                raise Exception("the provided path (" + ci + ") does not exist.")
             if os.path.isdir(ci):
+                if not os.path.exists(ci):
+                    raise Exception("the provided path (" + ci + ") does not exist.")
                 files = os.listdir(ci)
                 inputs.append([os.path.join(ci, f) for f in files if
                                os.path.isfile(os.path.join(ci, f)) and f.endswith(method["inputs"][i])])
