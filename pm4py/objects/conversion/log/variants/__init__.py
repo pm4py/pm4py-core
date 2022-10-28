e96912b3308719a9f6074253c372e33bdc8548a6
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2022-10-28 14:18:05 +0200

    feat(pm4py): conversion event log to NetworkX DiGraph

--- pm4py/objects/conversion/log/variants/__init__.py
+++ pm4py/objects/conversion/log/variants/__init__.py
@@ -1,1 +1,1 @@
-from pm4py.objects.conversion.log.variants import to_data_frame, to_event_stream, to_event_log, df_to_event_log_1v, df_to_event_log_nv
+from pm4py.objects.conversion.log.variants import to_data_frame, to_event_stream, to_event_log, df_to_event_log_1v, df_to_event_log_nv, to_nx
