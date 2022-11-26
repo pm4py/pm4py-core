37e7e92db973497b23b30610a005512bd2d3268a
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2022-11-26 11:30:54 +0100

    fix(pm4py): fixed examples

--- examples/enrich_log_with_align.py
+++ examples/enrich_log_with_align.py
@@ -2,10 +2,11 @@ import pm4py
 import os
 from pm4py.algo.conformance.alignments.petri_net import algorithm as alignments
 from pm4py.algo.conformance.alignments.petri_net.utils import log_enrichment
+from pm4py.objects.log.importer.xes import importer as xes_importer
 
 
 def execute_script():
-    log = pm4py.read_xes(os.path.join("..", "tests", "input_data", "running-example.xes"))
+    log = xes_importer.apply(os.path.join("..", "tests", "input_data", "running-example.xes"))
     filtered_log = pm4py.filter_variants_top_k(log, 1)
     net, im, fm = pm4py.discover_petri_net_inductive(filtered_log)
     aligned_traces = alignments.apply(log, net, im, fm, parameters={"ret_tuple_as_trans_desc": True})
