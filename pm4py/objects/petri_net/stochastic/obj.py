b45d031b1a9408ba18f19bf8dda6d8261897ded7
Sebastiaan J. van Zelst <sebastiaan.van.zelst@fit.fraunhofer.de>
2022-11-28 16:58:30 +0100

    fix(PMPY-1967): fix formatting for stochastic petri nets

--- pm4py/objects/petri_net/stochastic/obj.py
+++ pm4py/objects/petri_net/stochastic/obj.py
@@ -1,19 +1,19 @@
-from pm4py.objects.petri_net.obj import PetriNet
-from typing import Collection, Dict, Any
+from typing import Any, Collection, Dict
 
-import deprecation
+from pm4py.objects.petri_net.obj import PetriNet
+
 
 class StochasticPetriNet(PetriNet):
 
     class Transition(PetriNet.Transition):
-        def __init__(self, name: str, label: str = None, in_arcs: Collection[PetriNet.Arc]=None, out_arcs:Collection[PetriNet.Arc]=None, weight:float =1.0, properties: Dict[str, Any]=None):
+        def __init__(self, name: str, label: str = None, in_arcs: Collection[PetriNet.Arc] = None, out_arcs: Collection[PetriNet.Arc] = None, weight: float = 1.0, properties: Dict[str, Any] = None):
             super().__init__(name, label, in_arcs, out_arcs, properties)
             self.__weight = weight
 
         def __set_weight(self, weight: float):
             self.__weight = weight
-            
+
         def __get_weight(self) -> float:
             return self.__weight
-        
+
         weight = property(__get_weight, __set_weight)
