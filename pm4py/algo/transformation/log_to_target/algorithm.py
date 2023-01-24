ddf2a5aa9bde090101cd2db11d7163d72ad73f70
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-01-24 08:38:18 +0100

    feat(pm4py): log to target vectors (for ML purposes)

---
+++ pm4py/algo/transformation/log_to_target/algorithm.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from pm4py.algo.transformation.log_to_target.variants import next_activity, next_time, remaining_time
+from pm4py.objects.log.obj import EventLog, EventStream
+import pandas as pd
+from typing import Union, Dict, Optional, Any
+from pm4py.util import exec_utils
+
+
+class Variants(Enum):
+    NEXT_ACTIVITY = next_activity
+    NEXT_TIME = next_time
+    REMAINING_TIME = remaining_time
+
+
+def apply(log: Union[EventLog, EventStream, pd.DataFrame], variant=None, parameters: Optional[Dict[Any, Any]] = None):
+    if variant is None:
+        raise Exception("please provide the variant between: Variants.NEXT_ACTIVITY, Variants.NEXT_TIME, Variants.REMAINING_TIME")
+    return exec_utils.get_variant(variant).apply(log, parameters=parameters)
---
+++ pm4py/algo/transformation/log_to_target/variants/__init__.py
@@ -0,0 +1,1 @@
+from pm4py.algo.transformation.log_to_target.variants import next_activity, next_time, remaining_time
