4598639afa0cc8ecf60475b22cf3c37dccb4fc83
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-02-08 08:21:25 +0100

    fix(pm4py): internal changes (to Gviz visualization methods)

--- pm4py/visualization/common/save.py
+++ pm4py/visualization/common/save.py
@@ -14,7 +14,7 @@ def save(gviz, output_file_path):
     output_file_path
         Path where the GraphViz output should be saved
     """
-    format = os.path.splitext(output_file_path)[1][1:]
+    format = os.path.splitext(output_file_path)[1][1:].lower()
     is_dot_installed = dot_util.check_dot_installed()
 
     render = gviz.render(cleanup=True)
