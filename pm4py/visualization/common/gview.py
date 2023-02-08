11c4154f49df76e4690a9e2228e7dfc947e0d2a5
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-02-08 08:08:15 +0100

    feat(pm4py): check_dot_installed utility

--- pm4py/visualization/common/gview.py
+++ pm4py/visualization/common/gview.py
@@ -43,6 +43,23 @@ def matplotlib_view(gviz):
     plt.imshow(img)
     plt.show()
 
+def check_dot_installed():
+    """
+    Check if Graphviz's dot is installed correctly in the system
+
+    Returns
+    -------
+    boolean
+        Boolean telling if Graphviz's dot is installed correctly
+    """
+    import subprocess
+
+    try:
+        val = subprocess.run('dot -V', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return val.returncode == 0
+    except:
+        return False
+
 
 def serialize_dot(gviz: Digraph) -> bytes:
     """
