b39781c36a700ee9a0ac3501c3014e2465b23aed
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-02-27 07:52:25 +0100

    feat(pm4py): cost-based DFG discovery + visualization + refactoring

--- pm4py/visualization/dfg/variants/frequency.py
+++ pm4py/visualization/dfg/variants/frequency.py
@@ -1,22 +1,15 @@
-import tempfile
-from copy import copy
-
-from graphviz import Digraph
-
 from pm4py.statistics.attributes.log import get as attr_get
 from pm4py.objects.dfg.utils import dfg_utils
 from pm4py.util import xes_constants as xes
-from pm4py.visualization.common.utils import *
 from pm4py.util import exec_utils
 from pm4py.statistics.sojourn_time.log import get as soj_time_get
 from enum import Enum
 from pm4py.util import constants
-from typing import Optional, Dict, Any, Union, Tuple
-from pm4py.objects.log.obj import EventLog, EventStream
-from pm4py.util import typing
+from typing import Optional, Dict, Any, Tuple
 import graphviz
 from pm4py.objects.log.obj import EventLog
 from collections import Counter
+from pm4py.visualization.dfg.util import dfg_gviz
 
 
 class Parameters(Enum):
@@ -29,209 +22,6 @@ class Parameters(Enum):
     START_TIMESTAMP_KEY = constants.PARAMETER_CONSTANT_START_TIMESTAMP_KEY
     FONT_SIZE = "font_size"
     BGCOLOR = "bgcolor"
-
-
-def get_min_max_value(dfg):
-    """
-    Gets min and max value assigned to edges
-    in DFG graph
-
-    Parameters
-    -----------
-    dfg
-        Directly follows graph
-
-    Returns
-    -----------
-    min_value
-        Minimum value in directly follows graph
-    max_value
-        Maximum value in directly follows graph
-    """
-    min_value = 9999999999
-    max_value = -1
-
-    for edge in dfg:
-        if dfg[edge] < min_value:
-            min_value = dfg[edge]
-        if dfg[edge] > max_value:
-            max_value = dfg[edge]
-
-    return min_value, max_value
-
-
-def assign_penwidth_edges(dfg):
-    """
-    Assign penwidth to edges in directly-follows graph
-
-    Parameters
-    -----------
-    dfg
-        Direcly follows graph
-
-    Returns
-    -----------
-    penwidth
-        Graph penwidth that edges should have in the direcly follows graph
-    """
-    penwidth = {}
-    min_value, max_value = get_min_max_value(dfg)
-    for edge in dfg:
-        v0 = dfg[edge]
-        v1 = get_arc_penwidth(v0, min_value, max_value)
-        penwidth[edge] = str(v1)
-
-    return penwidth
-
-
-def get_activities_color(activities_count):
-    """
-    Get frequency color for attributes
-
-    Parameters
-    -----------
-    activities_count
-        Count of attributes in the log
-
-    Returns
-    -----------
-    activities_color
-        Color assigned to attributes in the graph
-    """
-    activities_color = {}
-
-    min_value, max_value = get_min_max_value(activities_count)
-
-    for ac in activities_count:
-        v0 = activities_count[ac]
-        """transBaseColor = int(
-            255 - 100 * (v0 - min_value) / (max_value - min_value + 0.00001))
-        transBaseColorHex = str(hex(transBaseColor))[2:].upper()
-        v1 = "#" + transBaseColorHex + transBaseColorHex + "FF"""
-
-        v1 = get_trans_freq_color(v0, min_value, max_value)
-
-        activities_color[ac] = v1
-
-    return activities_color
-
-
-def graphviz_visualization(activities_count, dfg, image_format="png", measure="frequency",
-                           max_no_of_edges_in_diagram=100000, start_activities=None, end_activities=None, soj_time=None,
-                            font_size="12", bgcolor=constants.DEFAULT_BGCOLOR):
-    """
-    Do GraphViz visualization of a DFG graph
-
-    Parameters
-    -----------
-    activities_count
-        Count of attributes in the log (may include attributes that are not in the DFG graph)
-    dfg
-        DFG graph
-    image_format
-        GraphViz should be represented in this format
-    measure
-        Describes which measure is assigned to edges in direcly follows graph (frequency/performance)
-    max_no_of_edges_in_diagram
-        Maximum number of edges in the diagram allowed for visualization
-    start_activities
-        Start activities of the log
-    end_activities
-        End activities of the log
-    soj_time
-        For each activity, the sojourn time in the log
-
-    Returns
-    -----------
-    viz
-        Digraph object
-    """
-    if start_activities is None:
-        start_activities = {}
-    if end_activities is None:
-        end_activities = {}
-
-    filename = tempfile.NamedTemporaryFile(suffix='.gv')
-    viz = Digraph("", filename=filename.name, engine='dot', graph_attr={'bgcolor': bgcolor})
-
-    # first, remove edges in diagram that exceeds the maximum number of edges in the diagram
-    dfg_key_value_list = []
-    for edge in dfg:
-        dfg_key_value_list.append([edge, dfg[edge]])
-    # more fine grained sorting to avoid that edges that are below the threshold are
-    # undeterministically removed
-    dfg_key_value_list = sorted(dfg_key_value_list, key=lambda x: (x[1], x[0][0], x[0][1]), reverse=True)
-    dfg_key_value_list = dfg_key_value_list[0:min(len(dfg_key_value_list), max_no_of_edges_in_diagram)]
-    dfg_allowed_keys = [x[0] for x in dfg_key_value_list]
-    dfg_keys = list(dfg.keys())
-    for edge in dfg_keys:
-        if edge not in dfg_allowed_keys:
-            del dfg[edge]
-
-    # calculate edges penwidth
-    penwidth = assign_penwidth_edges(dfg)
-    activities_in_dfg = set()
-    activities_count_int = copy(activities_count)
-
-    for edge in dfg:
-        activities_in_dfg.add(edge[0])
-        activities_in_dfg.add(edge[1])
-
-    # assign attributes color
-    activities_color = get_activities_color(activities_count_int)
-
-    # represent nodes
-    viz.attr('node', shape='box')
-
-    if len(activities_in_dfg) == 0:
-        activities_to_include = sorted(list(set(activities_count_int)))
-    else:
-        # take unique elements as a list not as a set (in this way, nodes are added in the same order to the graph)
-        activities_to_include = sorted(list(set(activities_in_dfg)))
-
-    activities_map = {}
-
-    for act in activities_to_include:
-        if "frequency" in measure and act in activities_count_int:
-            viz.node(str(hash(act)), act + " (" + str(activities_count_int[act]) + ")", style='filled',
-                     fillcolor=activities_color[act], fontsize=font_size)
-            activities_map[act] = str(hash(act))
-        else:
-            viz.node(str(hash(act)), act + " (" + human_readable_stat(soj_time[act]) + ")", fontsize=font_size)
-            activities_map[act] = str(hash(act))
-
-    # make edges addition always in the same order
-    dfg_edges = sorted(list(dfg.keys()))
-
-    # represent edges
-    for edge in dfg_edges:
-        if "frequency" in measure:
-            label = str(dfg[edge])
-        else:
-            label = human_readable_stat(dfg[edge])
-        viz.edge(str(hash(edge[0])), str(hash(edge[1])), label=label, penwidth=str(penwidth[edge]), fontsize=font_size)
-
-    start_activities_to_include = [act for act in start_activities if act in activities_map]
-    end_activities_to_include = [act for act in end_activities if act in activities_map]
-
-    if start_activities_to_include:
-        viz.node("@@startnode", "<&#9679;>", shape='circle', fontsize="34")
-        for act in start_activities_to_include:
-            label = str(start_activities[act]) if isinstance(start_activities, dict) else ""
-            viz.edge("@@startnode", activities_map[act], label=label, fontsize=font_size)
-
-    if end_activities_to_include:
-        # <&#9632;>
-        viz.node("@@endnode", "<&#9632;>", shape='doublecircle', fontsize="32")
-        for act in end_activities_to_include:
-            label = str(end_activities[act]) if isinstance(end_activities, dict) else ""
-            viz.edge(activities_map[act], "@@endnode", label=label, fontsize=font_size)
-
-    viz.attr(overlap='false')
-
-    viz.format = image_format.replace("html", "plain-ext")
-
-    return viz
 
 
 def apply(dfg: Dict[Tuple[str, str], int], log: EventLog = None, parameters: Optional[Dict[Any, Any]] = None, activities_count : Dict[str, int] = None, soj_time: Dict[str, float] = None) -> graphviz.Digraph:
@@ -289,7 +79,7 @@ def apply(dfg: Dict[Tuple[str, str], int], log: EventLog = None, parameters: Opt
         else:
             soj_time = {key: 0 for key in activities}
 
-    return graphviz_visualization(activities_count, dfg, image_format=image_format, measure="frequency",
+    return dfg_gviz.graphviz_visualization(activities_count, dfg, image_format=image_format, measure="frequency",
                                   max_no_of_edges_in_diagram=max_no_of_edges_in_diagram,
                                   start_activities=start_activities, end_activities=end_activities, soj_time=soj_time,
                                   font_size=font_size, bgcolor=bgcolor)
