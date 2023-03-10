0a1c6f9c6e0ff45a0e732978589ed17513899be8
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2023-03-10 09:17:33 +0100

    fix(pm4py): fixed init dependency on simpy

--- pm4py/algo/analysis/__init__.py
+++ pm4py/algo/analysis/__init__.py
@@ -1,5 +1,1 @@
-from pm4py.algo.analysis import extended_marking_equation, marking_equation, workflow_net
-
-import pkgutil
-if pkgutil.find_loader("simpy"):
-    from pm4py.algo.analysis import woflan
+from pm4py.algo.analysis import extended_marking_equation, marking_equation, workflow_net, woflan
