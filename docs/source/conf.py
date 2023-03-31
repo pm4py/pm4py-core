f2d1f1a328b694955dc77ff5b556e31336c96fce
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2023-03-31 11:13:23 +0200

    bumped version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Fraunhofer FIT'
 # The short X.Y version
 version = '2.7'
 # The full version, including alpha/beta/rc tags
-release = '2.7.1'
+release = '2.7.2'
 
 # -- General configuration ---------------------------------------------------
 
