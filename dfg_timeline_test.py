10b787e96f3c92abe227482d89c5dff1e3d8c891
har-leen-kaur1 <57240580+Har-leen-kaur@users.noreply.github.com>
2023-03-31 11:54:43 +0200

    rename file

--- dfg_timeline_test.py
+++ dfg_timeline_test.py
@@ -1,4 +1,3 @@
-#import pandas as pd
 import pm4py
 
 
@@ -6,6 +5,4 @@ file1 = "C:/Users/harleenkaur/Documents/thesis/event logs/Artificial - Loan Proc
 log = pm4py.read_xes(file1)
 #dfg, start_activities, end_activities = pm4py.discover_dfg_typed(log)  
 #pm4py.view_dfg(dfg, start_activities, end_activities)  
-
-mydfg = pm4py.discover_timeline_dfg(log)
-
+dfg = pm4py.discover_timeline_dfg(log)
