eaf2fbc01e028aa268b60a5c1ba9c034a797c30f
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-04-03 14:33:25 +0200

    feat(pm4py): other NLP/LLM abstractions

---
+++ pm4py/algo/querying/openai/net_to_descr.py
@@ -0,0 +1,44 @@
+from typing import Optional, Dict, Any
+from pm4py.objects.petri_net.obj import PetriNet, Marking
+from enum import Enum
+from pm4py.util import exec_utils
+
+
+class Parameters(Enum):
+    RESPONSE_HEADER = "response_header"
+
+
+def apply(net: PetriNet, im: Marking, fm: Marking, parameters: Optional[Dict[Any, Any]] = None) -> str:
+    """
+    Provides the description of an accepting Petri net
+
+    Parameters
+    --------------
+    net
+        Petri net
+    im
+        Initial marking
+    fm
+        Final marking
+    parameters
+        Possible parameters of the algorithm, including:
+        - Parameters.INCLUDE_HEADER => includes the header
+
+    Returns
+    --------------
+    stru
+        String representation of the given accepting Petri net
+    """
+    if parameters is None:
+        parameters = {}
+
+    include_header = exec_utils.get_param_value(Parameters.RESPONSE_HEADER, parameters, True)
+
+    ret = ["\n"]
+    if include_header:
+        ret.append("If I have a Petri net:\n")
+    ret.append(repr(net))
+    ret.append("\ninitial marking: "+repr(im))
+    ret.append("final marking: "+repr(fm))
+
+    return "\n".join(ret)
