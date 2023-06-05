89dc613eb110e7ba377007472755033b93eb5f07
vco <vco@di.ku.dk>
2023-06-05 11:37:47 +0200

    after merging tims semantics

--- pm4py/objects/dcr/semantics.py
+++ pm4py/objects/dcr/semantics.py
@@ -14,11 +14,55 @@ def execute(e, dcr, cmd_print=False):
             print(f'[!] Event {e} not enabled!') if cmd_print else None
             return False
     else:
-        # TODO: make time pass x duration
         print(f'[!] Event {e} does not exist!') if cmd_print else None
         return False
 
+def time_step(time, dcr, dict_exe):
+    deadline = find_next_deadline(dcr)
+    if deadline == None or deadline - time >= 0:
+        for e in dcr['marking']['pendingDeadline']:
+            dcr['marking']['pendingDeadline'][e] =  max(dcr['marking']['pendingDeadline'][e] - time, 0)
+        for e in dcr['marking']['executed']:
+            dcr['marking']['executedTime'][e] = min(dcr['marking']['executedTime'][e] + time, dict_exe[e])
+        return (True, time)
+    else:
+        print('[!] The time step is not allowed, you are gonna miss a deadline')
+        return (False, time)
+    
+def find_next_deadline(dcr):
+    nextDeadline = None
+    for e in dcr['marking']['pendingDeadline']:
+        if (nextDeadline == None and e not in dcr ['marking']['included']):
+            continue
+        if (nextDeadline == None and e in dcr['marking']['included']) or (dcr['marking']['pendingDeadline'][e] < nextDeadline and e in dcr['marking']['included']):
+            nextDeadline = dcr['marking']['pendingDeadline'][e]
+    return nextDeadline
 
+def find_next_delay(dcr):
+    nextDelay = None
+    for e in dcr['conditionsForDelays']:
+        for (e_prime, k) in dcr['conditionsForDelays'][e]:
+            if e_prime in dcr['marking']['executed'] and e_prime in dcr['marking']['included']:
+                delay = k - dcr['marking']['executedTime'][e_prime]
+                if delay > 0 and (nextDelay == None or delay < nextDelay):
+                    nextDelay = delay
+    return nextDelay
+
+def create_max_executed_time_dict(dcr):
+    d = {}
+    for e in dcr['events']:
+        d[e] = max_executed_time(e, dcr)
+    return d
+
+def max_executed_time(event, dcr):
+    maxDelay = 0
+    for e in dcr['conditionsForDelays']:
+        for (e_prime, k) in dcr['conditionsForDelays'][e]:
+            if e_prime == event:
+                if k > maxDelay:
+                    maxDelay = k
+    return maxDelay
+        
 def is_accepting(dcr):
     pend_incl = dcr['marking']['pending'].intersection(dcr['marking']['included'])
     return len(pend_incl) == 0
@@ -26,6 +70,7 @@ def is_accepting(dcr):
 
 def is_enabled(e, dcr):
     return e in enabled(dcr)
+
 def enabled(dcr):
     res = deepcopy(dcr['marking']['included'])
     for e in dcr['conditionsFor']:
@@ -33,7 +78,22 @@ def enabled(dcr):
             for e_prime in dcr['conditionsFor'][e]:
                 if e_prime in dcr['marking']['included'] and e_prime not in dcr['marking']['executed']:
                     res.discard(e)
+                    
+    for e in dcr['conditionsForDelays']:
+        if e in res:
+            for (e_prime, k) in dcr['conditionsForDelays'][e]:
+                if e_prime in dcr['marking']['included'] and e_prime not in dcr['marking']['executed']:
+                    res.discard(e)
+                elif e_prime in dcr['marking']['included'] and e_prime in dcr['marking']['executed']:
+                    if dcr['marking']['executedTime'][e_prime] < k:
+                        res.discard(e)
+                        
     # TODO: extend with milestones
+    for e in dcr['milestonesFor']:
+        if e in res:
+            for e_prime in dcr['milestonesFor'][e]:
+                if e_prime in dcr['marking']['included'] and e_prime in dcr['marking']['pending']:
+                    res.discard(e)
     return res
 
 def weak_execute(e, dcr):
@@ -45,6 +105,10 @@ def weak_execute(e, dcr):
     '''
     dcr['marking']['pending'].discard(e)
     dcr['marking']['executed'].add(e)
+    dcr['marking']['executedTime'][e] = 0
+    
+    if e in dcr['marking']['pendingDeadline']:
+        dcr['marking']['pendingDeadline'].pop(e) 
     if e in dcr['excludesTo']:
         for e_prime in dcr['excludesTo'][e]:
             dcr['marking']['included'].discard(e_prime)
@@ -53,6 +117,10 @@ def weak_execute(e, dcr):
             dcr['marking']['included'].add(e_prime)
     if e in dcr['responseTo']:
         for e_prime in dcr['responseTo'][e]:
+            dcr['marking']['pending'].add(e_prime)
+    if e in dcr['responseToDeadlines']:
+        for (e_prime, k) in dcr['responseToDeadlines'][e]:
+            dcr['marking']['pendingDeadline'][e_prime] = k
             dcr['marking']['pending'].add(e_prime)
     # TODO: extend with milestones
 
