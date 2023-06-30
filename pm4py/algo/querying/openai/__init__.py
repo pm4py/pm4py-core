6ccb9b9808b9c348d43bc521822dbbdc444ca6aa
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-06-30 15:33:13 +0200

    refactor(pm4py): added deprecation warning

--- pm4py/algo/querying/openai/__init__.py
+++ pm4py/algo/querying/openai/__init__.py
@@ -1,2 +1,5 @@
 from pm4py.algo.querying.llm.abstractions import *
 from pm4py.algo.querying.llm.connectors import openai as perform_query
+
+import warnings
+warnings.warn("the pm4py.algo.querying.openai package is deprecated. Please use pm4py.algo.querying.llm.abstractions instead.")
