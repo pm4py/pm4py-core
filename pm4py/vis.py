84687c003576d037f318b1f8bc6bb432f5bccb98
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-06-30 08:17:30 +0200

    docs(pm4py): improving docstrings
    improving docstrings

--- pm4py/vis.py
+++ pm4py/vis.py
@@ -36,7 +36,7 @@ def view_petri_net(petri_net: PetriNet, initial_marking: Optional[Marking] = Non
     :param format: Format of the output picture (if html is provided, GraphvizJS is used to render the visualization in an HTML page)
     :param bgcolor: Background color of the visualization (default: white)
     :param decorations: Decorations (color, label) associated to the elements of the Petri net
-    :param debug: debug mode (show place and transition's names)
+    :param debug: Boolean enabling/disabling the debug mode (show place and transition's names)
 
     .. code-block:: python3
 
@@ -63,7 +63,7 @@ def save_vis_petri_net(petri_net: PetriNet, initial_marking: Marking, final_mark
     :param file_path: Destination path
     :param bgcolor: Background color of the visualization (default: white)
     :param decorations: Decorations (color, label) associated to the elements of the Petri net
-    :param debug: debug mode (show place and transition's names)
+    :param debug: Boolean enabling/disabling the debug mode (show place and transition's names)
 
     .. code-block:: python3
 
