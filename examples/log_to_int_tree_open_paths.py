00fb9f742868362a7935ecc115a808846d628e74
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-07-12 13:32:25 +0200

    refactor(pm4py): refactoring log to intervals + simplified interface (for more queue-related examples)

--- examples/log_to_int_tree_open_paths.py
+++ examples/log_to_int_tree_open_paths.py
@@ -11,6 +11,15 @@ def execute_script():
     # read the detailed information about the source and target event of each path
     print(tree[1319616410])
 
+    # builds a new tree considering only the intervals going from an event with activity
+    # 'T06 Determine necessity of stop advice'
+    # to an event with activity 'T02 Check confirmation of receipt'
+    tree = log_to_interval_tree.apply(log, variant=log_to_interval_tree.Variants.OPEN_PATHS, parameters={"filter_activity_couple": ("T06 Determine necessity of stop advice", "T02 Check confirmation of receipt")})
+    # see how many paths are open at the timestamp 1319616410
+    print(len(tree[1319616410]))
+    # read the detailed information about the source and target event of each path
+    print(tree[1319616410])
+
 
 if __name__ == "__main__":
     execute_script()
