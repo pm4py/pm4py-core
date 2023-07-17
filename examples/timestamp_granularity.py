5275a64b0551aa753e13cb5beb56eebe481ca347
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-07-17 07:04:15 +0200

    docs(pm4py): improved example

--- examples/timestamp_granularity.py
+++ examples/timestamp_granularity.py
@@ -10,7 +10,7 @@ def execute_script():
     # prints the original timestamp column of the dataframe
     print(dataframe["time:timestamp"])
 
-    # Here are some common options that you can use as a frequency:
+    # Here are some common options that you can use as a granularity:
     #
     # 'D': Day
     # 'H': Hour
@@ -29,6 +29,12 @@ def execute_script():
 
     # prints the new timestamp column of the dataframe
     print(dataframe["time:timestamp"])
+
+    # for completeness, we report some alternatives methods in Pandas to do the same (casting on the minute):
+    #
+    # dataframe["time:timestamp"] = dataframe["time:timestamp"].apply(lambda x: x.replace(second=0, microsecond=0))
+    #
+    # dataframe["time:timestamp"] = dataframe["time:timestamp"].dt.round('min')
 
 
 if __name__ == "__main__":
