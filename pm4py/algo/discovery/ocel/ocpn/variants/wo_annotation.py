d8f6f4c2c796405df5ada170289f7468fb3bfdd5
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-08-01 08:38:37 +0200

    feat(pm4py): TBR diagnostics in OCPN discovery and visualization

--- pm4py/algo/discovery/ocel/ocpn/variants/wo_annotation.py
+++ pm4py/algo/discovery/ocel/ocpn/variants/wo_annotation.py
@@ -8,7 +8,9 @@ from collections import Counter
 from typing import Optional, Dict, Any
 from pm4py.objects.dfg.obj import DFG
 from pm4py.objects.conversion.process_tree import converter as tree_converter
+from pm4py.algo.conformance.tokenreplay import algorithm as token_based_replay
 from pm4py.objects.ocel.util import flattening
+from copy import copy
 
 
 class Parameters(Enum):
@@ -16,6 +18,7 @@ class Parameters(Enum):
     OBJECT_TYPE = ocel_constants.PARAM_OBJECT_TYPE
     INDUCTIVE_MINER_VARIANT = "inductive_miner_variant"
     DOUBLE_ARC_THRESHOLD = "double_arc_threshold"
+    DIAGNOSTICS_WITH_TBR = "diagnostics_with_token_based_replay"
 
 
 def apply(ocel: OCEL, parameters: Optional[Dict[Any, Any]] = None) -> Dict[str, Any]:
@@ -35,17 +38,48 @@ def apply(ocel: OCEL, parameters: Optional[Dict[Any, Any]] = None) -> Dict[str, 
         - Parameters.OBJECT_TYPE => the object type attribute to be used
         - Parameters.DOUBLE_ARC_THRESHOLD => the threshold for the attribution of the "double arc", as
         described in the paper.
+        - Parameters.DIAGNOSTICS_WITH_TBR => performs token-based replay and stores the result in the return dict
 
     Returns
     -----------------
     ocpn
-        Object-centric Petri net model, as a dictionary of properties.
+        Object-centric Petri net model, presented as a dictionary of properties:
+        - activities: complete set of activities derived from the object-centric event log
+        - object_types: complete set of object types derived from the object-centric event log
+        - edges: dictionary connecting each object type to a set of directly-followed arcs between activities (expressed as tuples,
+                  e.g., (act1, act2)). Every pair of activities is linked to some sets:
+                - event_pairs: the tuples of event identifiers where the directly-follows arc occurs
+                - total_objects: set of tuples containing two event and one object identifier, uniquely identifying an
+                                  occurrence of the arc.
+        - activities_indep: dictionary linking each activity, regardless of the object type, to some sets:
+            - events: the event identifiers where the activity occurs
+            - unique_objects: the object identifiers where the activity occurs
+            - total_objects: the tuples of event and object identifiers where the activity occurs.
+        - activities_ot: dictionary linking each object type to another dictionary, where the activities are linked to some sets:
+            - events: the event identifiers where the activity occurs (with at least one object of the given object type)
+            - unique_objects: the object identifiers of the given object type where the activity occurs
+            - total_objects: the tuples of event and object identifiers where the activity occurs.
+        - start_activities: dictionary linking each object type to another dictionary, where the start activities
+                            of the given object type are linked to some sets:
+            - events: the event identifiers where the start activity occurs (with at least one object of the given object type)
+            - unique_objects: the object identifiers of the given object type where the start activity occurs
+            - total_objects: the tuples of event and object identifiers where the start activity occurs.
+        - end_activities: dictionary linking each object type to another dictionary, where the end activities
+                          of the given object type are linked to some sets:
+            - events: the event identifiers where the end activity occurs (with at least one object of the given object type)
+            - unique_objects: the object identifiers of the given object type where the end activity occurs
+            - total_objects: the tuples of event and object identifiers where the end activity occurs.
+        - petri_nets: the accepted Petri nets (Petri net + initial marking + final marking) discovered by the process discovery algorithm
+        - double_arcs_on_activity: dictionary linking each object type to another dictionary, where each arc of the Petri net
+                                    is linked to a boolean (True if it is a double arc)
+        - tbr_results: the results of the token-based replay operation (if required)
     """
     if parameters is None:
         parameters = {}
 
     double_arc_threshold = exec_utils.get_param_value(Parameters.DOUBLE_ARC_THRESHOLD, parameters, 0.0)
     inductive_miner_variant = exec_utils.get_param_value(Parameters.INDUCTIVE_MINER_VARIANT, parameters, "im")
+    diagnostics_with_tbr = exec_utils.get_param_value(Parameters.DIAGNOSTICS_WITH_TBR, parameters, False)
 
     ocpn = ocdfg_discovery.apply(ocel, parameters=parameters)
 
@@ -87,6 +121,26 @@ def apply(ocel: OCEL, parameters: Optional[Dict[Any, Any]] = None) -> Dict[str, 
             flat_log = flattening.flatten(ocel, ot, parameters=parameters)
             process_tree = inductive_miner.apply(flat_log, parameters=parameters)
             petri_net = tree_converter.apply(process_tree, parameters=parameters)
+            if diagnostics_with_tbr:
+                tbr_parameters = copy(parameters)
+                tbr_parameters["enable_pltr_fitness"] = True
+                replayed_traces, place_fitness_per_trace, transition_fitness_per_trace, notexisting_activities_in_model = token_based_replay.apply(
+                    flat_log, petri_net[0], petri_net[1], petri_net[2], parameters=tbr_parameters)
+                place_diagnostics = {place: {"m": 0, "r": 0, "c": 0, "p": 0} for place in place_fitness_per_trace}
+                trans_count = {trans: 0 for trans in petri_net[0].transitions}
+                # computes the missing, remaining, consumed, and produced tokens per place.
+                for place, res in place_fitness_per_trace.items():
+                    place_diagnostics[place]['m'] += res['m']
+                    place_diagnostics[place]['r'] += res['r']
+                    place_diagnostics[place]['c'] += res['c']
+                    place_diagnostics[place]['p'] += res['p']
+
+                # counts the number of times a transition has been fired during the replay.
+                for trace in replayed_traces:
+                    for trans in trace['activated_transitions']:
+                        trans_count[trans] += 1
+
+                tbr_results[ot] = (place_diagnostics, trans_count)
             petri_nets[ot] = petri_net
 
     ocpn["petri_nets"] = petri_nets
