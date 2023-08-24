a3fb5b873ea9ca2bd51842d6bfe3d544472a8bbb
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2023-08-24 07:33:40 +0200

    feat(pm4py): added example and docstring

--- pm4py/visualization/networkx/variants/digraph.py
+++ pm4py/visualization/networkx/variants/digraph.py
@@ -19,6 +19,21 @@ def apply(G: nx.DiGraph, parameters: Optional[Dict[Any, Any]] = None) -> graphvi
     """
     Creates a Graphviz Digraph from a NetworkX DiGraph object.
 
+    Minimum viable example:
+
+        import pm4py
+        from pm4py.visualization.networkx import visualizer as nx_to_gv_vis
+
+        log = pm4py.read_xes("../tests/input_data/running-example.xes")
+        # gets an 'event graph' where events, cases and their relationships
+        # are represented in a graph (NetworkX DiGraph)
+        event_graph = pm4py.convert_log_to_networkx(log)
+
+        # visualize the NX DiGraph using Graphviz
+        gviz = nx_to_gv_vis.apply(event_graph, parameters={"format": "svg"})
+        nx_to_gv_vis.view(gviz)
+
+
     Parameters
     ---------------
     G
