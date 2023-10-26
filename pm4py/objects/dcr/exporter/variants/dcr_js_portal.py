04c991b8fb4e8edca7281f6961a2d7fab83c9ae7
Simon <simonhermansen5dk@gmail.com>
2023-10-26 18:08:57 +0200

    Updated visualisation of DCR graphs in dcr-js portal

--- pm4py/objects/dcr/exporter/variants/dcr_js_portal.py
+++ pm4py/objects/dcr/exporter/variants/dcr_js_portal.py
@@ -28,8 +28,19 @@ def export_dcr_xml(dcr, output_file_name, dcr_title):
     included = etree.SubElement(marking, "included")
     pendingResponse = etree.SubElement(marking, "pendingResponses")
 
-    xcoord = 0
-    ycoord = 0
+    xcoord = {}
+    ycoord = {}
+    x = 0
+    y = 0
+
+    for event in dcr['events']:
+        xcoord[event] = x
+        ycoord[event] = y
+        x += 300
+        
+        if x > 1200:
+            x = 0
+            y += 300
 
     for event in dcr['events']:
         xml_event = etree.SubElement(events, "event")
@@ -37,8 +48,8 @@ def export_dcr_xml(dcr, output_file_name, dcr_title):
         xml_event_custom = etree.SubElement(xml_event, "custom")
         xml_visual = etree.SubElement(xml_event_custom, "visualization")
         xml_location = etree.SubElement(xml_visual, "location")
-        xml_location.set("xLoc", str(xcoord))
-        xml_location.set("yLoc", str(ycoord))
+        xml_location.set("xLoc", str(xcoord[event]))
+        xml_location.set("yLoc", str(ycoord[event]))
         xml_size = etree.SubElement(xml_visual, "size")
         xml_size.set("width", "130")
         xml_size.set("height", "150")
@@ -47,20 +58,22 @@ def export_dcr_xml(dcr, output_file_name, dcr_title):
         xml_labelMapping = etree.SubElement(labelMappings, "labelMapping")
         xml_labelMapping.set("eventId", event)
         xml_labelMapping.set("labelId", event)
-        xcoord += 130
 
         for event_prime in dcr['events']:
             if event in dcr["conditionsFor"] and event_prime in dcr["conditionsFor"][event]:
                 xml_condition = etree.SubElement(conditions, "condition")
-                xml_condition.set("sourceId", event_prime)
-                xml_condition.set("targetId", event)
+                xml_condition.set("sourceId", event)
+                xml_condition.set("targetId", event_prime)
                 xml_condition_custom = etree.SubElement(xml_condition, "custom")
                 xml_waypoints = etree.SubElement(xml_condition_custom, "waypoints")
                 xml_waypoint = etree.SubElement(xml_waypoints, "waypoint")
-                xml_waypoint.set("x", str(xcoord))
-                xml_waypoint.set("y", str(75))
+                xml_waypoint.set("x", str(xcoord[event]+65))
+                xml_waypoint.set("y", str(ycoord[event]+75))
+                xml_waypoint = etree.SubElement(xml_waypoints, "waypoint")
+                xml_waypoint.set("x", str(xcoord[event_prime]+65))
+                xml_waypoint.set("y", str(ycoord[event_prime]+75))
                 xml_custom_id = etree.SubElement(xml_condition_custom, "id")
-                xml_custom_id.set("id", "Relation_" + event_prime + "_" + event)
+                xml_custom_id.set("id", "Relation_" + event + "_" + event_prime + "_condition")
             if event in dcr["responseTo"] and event_prime in dcr["responseTo"][event]:
                 xml_response = etree.SubElement(responses, "response")
                 xml_response.set("sourceId", event)
@@ -68,10 +81,13 @@ def export_dcr_xml(dcr, output_file_name, dcr_title):
                 xml_response_custom = etree.SubElement(xml_response, "custom")
                 xml_waypoints = etree.SubElement(xml_response_custom, "waypoints")
                 xml_waypoint = etree.SubElement(xml_waypoints, "waypoint")
-                xml_waypoint.set("x", str(xcoord))
-                xml_waypoint.set("y", str(75))
+                xml_waypoint.set("x", str(xcoord[event]+65))
+                xml_waypoint.set("y", str(ycoord[event]+75))
+                xml_waypoint = etree.SubElement(xml_waypoints, "waypoint")
+                xml_waypoint.set("x", str(xcoord[event_prime]+65))
+                xml_waypoint.set("y", str(ycoord[event_prime]+75))
                 xml_custom_id = etree.SubElement(xml_response_custom, "id")
-                xml_custom_id.set("id", "Relation_" + event + "_" + event_prime)
+                xml_custom_id.set("id", "Relation_" + event + "_" + event_prime + "_response")
             if event in dcr["includesTo"] and event_prime in dcr["includesTo"][event]:
                 xml_include = etree.SubElement(includes, "include")
                 xml_include.set("sourceId", event)
@@ -79,10 +95,13 @@ def export_dcr_xml(dcr, output_file_name, dcr_title):
                 xml_include_custom = etree.SubElement(xml_include, "custom")
                 xml_waypoints = etree.SubElement(xml_include_custom, "waypoints")
                 xml_waypoint = etree.SubElement(xml_waypoints, "waypoint")
-                xml_waypoint.set("x", str(xcoord))
-                xml_waypoint.set("y", str(75))
+                xml_waypoint.set("x", str(xcoord[event]+65))
+                xml_waypoint.set("y", str(ycoord[event]+75))
+                xml_waypoint = etree.SubElement(xml_waypoints, "waypoint")
+                xml_waypoint.set("x", str(xcoord[event_prime]+65))
+                xml_waypoint.set("y", str(ycoord[event_prime]+75))
                 xml_custom_id = etree.SubElement(xml_include_custom, "id")
-                xml_custom_id.set("id", "Relation_" + event + "_" + event_prime)
+                xml_custom_id.set("id", "Relation_" + event + "_" + event_prime + "_include")
             if event in dcr["excludesTo"] and event_prime in dcr["excludesTo"][event]:
                 xml_exclude = etree.SubElement(excludes, "exclude")
                 xml_exclude.set("sourceId", event)
@@ -90,11 +109,13 @@ def export_dcr_xml(dcr, output_file_name, dcr_title):
                 xml_exclude_custom = etree.SubElement(xml_exclude, "custom")
                 xml_waypoints = etree.SubElement(xml_exclude_custom, "waypoints")
                 xml_waypoint = etree.SubElement(xml_waypoints, "waypoint")
-                xml_waypoint.set("x", str(xcoord))
-                xml_waypoint.set("y", str(75))
+                xml_waypoint.set("x", str(xcoord[event]+65))
+                xml_waypoint.set("y", str(ycoord[event]+75))
+                xml_waypoint = etree.SubElement(xml_waypoints, "waypoint")
+                xml_waypoint.set("x", str(xcoord[event_prime]+65))
+                xml_waypoint.set("y", str(ycoord[event_prime]+75))
                 xml_custom_id = etree.SubElement(xml_exclude_custom, "id")
-                xml_custom_id.set("id", "Relation_" + event + "_" + event_prime)
-            xcoord += 25
+                xml_custom_id.set("id", "Relation_" + event + "_" + event_prime + "_exclude")
 
         if event in dcr['marking']['executed']:
             marking_exec = etree.SubElement(executed, "event")
@@ -105,10 +126,6 @@ def export_dcr_xml(dcr, output_file_name, dcr_title):
         if event in dcr['marking']['pending']:
             marking_pend = etree.SubElement(pendingResponse, "event")
             marking_pend.set("id", event)
-
-        if xcoord > 2000:
-            xcoord = 0
-            ycoord += 175
 
     tree = etree.ElementTree(root)
     tree.write(output_file_name, pretty_print=True, xml_declaration=True, encoding="utf-8", standalone="yes")
--- tests/dcr_tests.py
+++ tests/dcr_tests.py
@@ -15,37 +15,47 @@ class TestDcr(unittest.TestCase):
     #TODO: create a dict DCR graph and the same graph in the portal
     # it has to have: all relations + self relations + time
 
-    # def test_exporter_to_portal(self):
-    #     event_log_file = os.path.join("input_data", "sepsis", "data", "Sepsis Cases - Event Log.xes")
-    #     dcrxml_file_export = os.path.join("test_output_data", "sepsis_dcr_portal.xml")
-    #     log = pm4py.read_xes(event_log_file)
-    #     dcr, _ = disc_alg.apply(log)
-    #     export_apply(dcr_graph=dcr,path=dcrxml_file_export,variant=dcr_exporter.XML_DCR_PORTAL, dcr_title='xml_2_dcr_portal')
+    def test_exporter_to_portal(self):
+        event_log_file = os.path.join("input_data", "sepsis", "data", "Sepsis Cases - Event Log.xes")
+        dcrxml_file_export = os.path.join("test_output_data", "sepsis_dcr_portal.xml")
+        log = pm4py.read_xes(event_log_file)
+        dcr, _ = disc_alg.apply(log)
+        export_apply(dcr_graph=dcr,path=dcrxml_file_export,variant=dcr_exporter.XML_DCR_PORTAL, dcr_title='xml_2_dcr_portal')
 
-    # def test_importer_from_portal(self):
-    #     dcr = import_apply(os.path.join("test_output_data", "sepsis_dcr_portal.xml"))
-    #     self.assertIsNotNone(dcr)
-
-    # def test_exporter_to_xml_simple(self):
-    #     event_log_file = os.path.join("input_data", "sepsis", "data", "Sepsis Cases - Event Log.xes")
-    #     dcrxml_file_export = os.path.join("test_output_data", "sepsis_dcr_simple.xml")
-    #     log = pm4py.read_xes(event_log_file)
-    #     dcr, _ = disc_alg.apply(log)
-    #     export_apply(dcr_graph=dcr,path=dcrxml_file_export,variant=dcr_exporter.XML_SIMPLE)
-
-    # def test_import_export_portal(self):
-    #     dcr = import_apply(os.path.join("test_output_data", "sepsis_dcr_portal.xml"))
-    #     path = os.path.join("test_output_data", "sepsis_dcr_portal_exported.xml")
-    #     export_apply(dcr_graph=dcr,path=path,variant=dcr_exporter.XML_DCR_PORTAL, dcr_title='sepsis_dcr_portal_exported_xml')
-    #     dcr_imported_after_export = import_apply(path)
-    #     self.assertEqual(len(dcr), len(dcr_imported_after_export))
-    #     os.remove(path)
-
-    def test_dcr_js_portal(self):
+    def test_importer_from_portal(self):
         dcr = import_apply(os.path.join("test_output_data", "sepsis_dcr_portal.xml"))
-        path = "C:/Users/herma/Downloads/sepsis_dcr_js_portal.xml"
-        export_apply(dcr_graph=dcr,path=path,variant=dcr_exporter.DCR_JS_PORTAL, dcr_title='sepsis_dcr_portal_exported_xml')
+        self.assertIsNotNone(dcr)
 
+    def test_exporter_to_xml_simple(self):
+        event_log_file = os.path.join("input_data", "sepsis", "data", "Sepsis Cases - Event Log.xes")
+        dcrxml_file_export = os.path.join("test_output_data", "sepsis_dcr_simple.xml")
+        log = pm4py.read_xes(event_log_file)
+        dcr, _ = disc_alg.apply(log)
+        export_apply(dcr_graph=dcr,path=dcrxml_file_export,variant=dcr_exporter.XML_SIMPLE)
+
+    def test_import_export_portal(self):
+        dcr = import_apply(os.path.join("test_output_data", "sepsis_dcr_portal.xml"))
+        path = os.path.join("test_output_data", "sepsis_dcr_portal_exported.xml")
+        export_apply(dcr_graph=dcr,path=path,variant=dcr_exporter.XML_DCR_PORTAL, dcr_title='sepsis_dcr_portal_exported_xml')
+        dcr_imported_after_export = import_apply(path)
+        self.assertEqual(len(dcr), len(dcr_imported_after_export))
+        os.remove(path)
+
+    def test_xml_dcr_portal_to_dcr_js_portal(self):
+        dcr = import_apply(os.path.join("test_output_data", "sepsis_dcr_portal.xml"))
+        path = os.path.join("test_output_data", "sepsis_dcr_js_portal.xml")
+        export_apply(dcr_graph=dcr,path=path,variant=dcr_exporter.DCR_JS_PORTAL, dcr_title='sepsis_dcr_js_portal')
+
+    def test_xes_to_dcr_js_portal(self):
+        event_log_file = os.path.join("input_data", "running-example.xes")
+        dcrxml_file_export = os.path.join("test_output_data", "running-example_dcr_portal.xml")
+        log = pm4py.read_xes(event_log_file)
+        dcr, _ = disc_alg.apply(log)
+        export_apply(dcr_graph=dcr,path=dcrxml_file_export,variant=dcr_exporter.XML_DCR_PORTAL, dcr_title='running-example_dcr_portal')
+        dcr_imported_after_export = import_apply(dcrxml_file_export)
+        path = os.path.join("test_output_data", "running-example_dcr_js_portal.xml")
+        export_apply(dcr_graph=dcr_imported_after_export,path=path,variant=dcr_exporter.DCR_JS_PORTAL, dcr_title='running-example_dcr_js_portal')
+        os.remove(dcrxml_file_export)
 
 if __name__ == '__main__':
     unittest.main()
