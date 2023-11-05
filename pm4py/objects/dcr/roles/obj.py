59a8e1f895eec37c5ac552b3be1d9fae5360421d
RagnarLaki <ragnarlaki@gmail.com>
2023-11-05 21:19:45 +0100

    Update obj.py

--- pm4py/objects/dcr/roles/obj.py
+++ pm4py/objects/dcr/roles/obj.py
@@ -5,26 +5,33 @@ class RoleDCR_Graph(object):
     """
     A class representing a Role-based DCR graph.
 
-    This class wraps around a DCR graph structure and extends it with role-based features, such as principals,
-    roles, and role assignments. It provides a way to integrate roles into the DCR model and to compute role-based
-    constraints as part of the graph.
+    This class wraps around a DCR graph structure, extending it with role-based features such as principals,
+    roles, role assignments, and principals assignments. It provides an interface to integrate roles into the
+    DCR model and to compute role-based constraints as part of the graph.
 
     Attributes
     ----------
-    __principals : Set[str]
+    
+    self.__g : DCRGraph
+        The underlying DCR graph structure.
+    self.__principals : Set[str]
         A set of principal identifiers within the graph.
-    __roles : Set[str]
+    self.__roles : Set[str]
         A set of role identifiers within the graph.
-    __roleAssignments : Dict[str, Set[str]]
+    self.__roleAssignments : Dict[str, Set[str]]
         A dictionary where keys are activity identifiers and values are sets of roles assigned to those activities.
+    self.__principalsAssignment : Dict[str, Set[str]]
+        A dictionary where keys are activity identifiers and values are sets of principals assigned to those activities.
 
     Methods
     -------
-    getConstraints():
+    
+    getConstraints() -> int:
         Computes the total number of constraints in the DCR graph, including those derived from role assignments.
 
     Parameters
     ----------
+    
     g : DCRGraph
         The underlying DCR graph structure.
     template : dict, optional
@@ -38,14 +45,14 @@ class RoleDCR_Graph(object):
         "principals": {"principal1", "principal2"},\n
         "roles": {"role1", "role2"},\n
         "roleAssignments": {"activity1": {"role1"}},\n
-        "readRoleAssignments": {"activity1": {"role2"}}\n
+        "principalsAssignments": {"activity1": {"principal1"}}\n
     })\n
 
     \nAccess role-based attributes\n
     principals = role_graph.principals\n
     roles = role_graph.roles\n
     role_assignments = role_graph.roleAssignments\n
-    read_role_assignments = role_graph.readRoleAssignments\n
+    principals_assignment = role_graph.principalsAssignment\n
 
     \nCompute the number of constraints\n
     total_constraints = role_graph.getConstraints()\n
