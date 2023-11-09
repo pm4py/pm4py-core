eef2ef15c84e8d249a808ad3be8dfee16d3af17c
Scones111 <jonas.lykke.kjeldsen@gmail.com>
2023-11-09 10:45:55 +0100

    updated the properties to be lower case

--- pm4py/algo/conformance/dcr/rules/include.py
+++ pm4py/algo/conformance/dcr/rules/include.py
@@ -23,8 +23,8 @@ class CheckInclude(CheckFrame):
             List of updated deviation if any were detected
         """
         if event not in graph.marking.included:
-            for event_prime in graph.includesTo:
-                if event in graph.includesTo[event_prime]:
+            for event_prime in graph.includes:
+                if event in graph.includes[event_prime]:
                     if ['includeViolation', (event_prime, event)] not in deviations:
                         deviations.append(('includeViolation', (event_prime, event)))
         return deviations
