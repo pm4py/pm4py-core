2faff12b26c451b2c5dc64313ef7d7b6bae62592
Scones111 <jonas.lykke.kjeldsen@gmail.com>
2023-11-09 19:50:59 +0100

    reverted xml simple back, works as intented now

--- pm4py/objects/dcr/exporter/variants/xml_simple.py
+++ pm4py/objects/dcr/exporter/variants/xml_simple.py
@@ -1,16 +1,17 @@
 from lxml import etree
 
 
-def export_dcr_graph(dcr, root, parents_dict=None, replace_whitespace=''):
+def export_dcr_graph(dcr, root, parents_dict=None):
+    # Writes the DCR graph to the XML file
     for event in dcr['events']:
         xml_event = etree.SubElement(root, "events")
         xml_event_id = etree.SubElement(xml_event, "id")
-        xml_event_id.text = event.replace(' ', replace_whitespace)
+        xml_event_id.text = event
         xml_event_label = etree.SubElement(xml_event, "label")
-        xml_event_label.text = event.replace(' ', replace_whitespace)
+        xml_event_label.text = event
         if parents_dict and event in parents_dict:
             xml_event_parent = etree.SubElement(xml_event, "parent")
-            xml_event_parent.text = parents_dict[event].replace(' ', replace_whitespace)
+            xml_event_parent.text = parents_dict[event]
 
         for event_prime in dcr['events']:
             if event in dcr["conditionsFor"] and event_prime in dcr["conditionsFor"][event]:
@@ -18,9 +19,9 @@ def export_dcr_graph(dcr, root, parents_dict=None, replace_whitespace=''):
                 xml_type = etree.SubElement(xml_condition, "type")
                 xml_type.text = "condition"
                 xml_source = etree.SubElement(xml_condition, "source")
-                xml_source.text = event_prime.replace(' ', replace_whitespace)
+                xml_source.text = event_prime
                 xml_target = etree.SubElement(xml_condition, "target")
-                xml_target.text = event.replace(' ', replace_whitespace)
+                xml_target.text = event
                 if 'conditionsForDelays' in dcr.__dict__.keys() and event in dcr['conditionsForDelays'] \
                         and event_prime in dcr['conditionsForDelays'][event]:
                     time = dcr['conditionsForDelays'][event][event_prime]
@@ -32,9 +33,9 @@ def export_dcr_graph(dcr, root, parents_dict=None, replace_whitespace=''):
                 xml_type = etree.SubElement(xml_response, "type")
                 xml_type.text = "response"
                 xml_source = etree.SubElement(xml_response, "source")
-                xml_source.text = event.replace(' ', replace_whitespace)
+                xml_source.text = event
                 xml_target = etree.SubElement(xml_response, "target")
-                xml_target.text = event_prime.replace(' ', replace_whitespace)
+                xml_target.text = event_prime
                 if 'responseToDeadlines' in dcr.__dict__.keys() and event in dcr['responseToDeadlines'] \
                         and event_prime in dcr['responseToDeadlines'][event]:
                     time = dcr['responseToDeadlines'][event][event_prime]
@@ -46,34 +47,36 @@ def export_dcr_graph(dcr, root, parents_dict=None, replace_whitespace=''):
                 xml_type = etree.SubElement(xml_include, "type")
                 xml_type.text = "include"
                 xml_source = etree.SubElement(xml_include, "source")
-                xml_source.text = event.replace(' ', replace_whitespace)
+                xml_source.text = event
                 xml_target = etree.SubElement(xml_include, "target")
-                xml_target.text = event_prime.replace(' ', replace_whitespace)
+                xml_target.text = event_prime
             if event in dcr["excludesTo"] and event_prime in dcr["excludesTo"][event]:
                 xml_exclude = etree.SubElement(root, "rules")
                 xml_type = etree.SubElement(xml_exclude, "type")
                 xml_type.text = "exclude"
                 xml_source = etree.SubElement(xml_exclude, "source")
-                xml_source.text = event.replace(' ', replace_whitespace)
+                xml_source.text = event
                 xml_target = etree.SubElement(xml_exclude, "target")
-                xml_target.text = event_prime.replace(' ', replace_whitespace)
-            if event in dcr["milestonesFor"] and event_prime in dcr["milestonesFor"][event]:
-                xml_exclude = etree.SubElement(root, "rules")
-                xml_type = etree.SubElement(xml_exclude, "type")
-                xml_type.text = "milestone"
-                xml_source = etree.SubElement(xml_exclude, "source")
-                xml_source.text = event.replace(' ', replace_whitespace)
-                xml_target = etree.SubElement(xml_exclude, "target")
-                xml_target.text = event_prime.replace(' ', replace_whitespace)
-            if "noResponseTo" in dcr and event in dcr["noResponseTo"] and event_prime in dcr["noResponseTo"][event]:
-                xml_exclude = etree.SubElement(root, "rules")
-                xml_type = etree.SubElement(xml_exclude, "type")
-                xml_type.text = "coresponse"
-                xml_source = etree.SubElement(xml_exclude, "source")
-                xml_source.text = event.replace(' ', replace_whitespace)
-                xml_target = etree.SubElement(xml_exclude, "target")
-                xml_target.text = event_prime.replace(' ', replace_whitespace)
+                xml_target.text = event_prime
+        # TODO: include in the future when the DCR graph obj.py is updated
+            # if dcr['milestonesFor'] is not None and event in dcr["milestonesFor"] and event_prime in dcr["milestonesFor"][event]:
+            #     xml_exclude = etree.SubElement(root, "rules")
+            #     xml_type = etree.SubElement(xml_exclude, "type")
+            #     xml_type.text = "milestone"
+            #     xml_source = etree.SubElement(xml_exclude, "source")
+            #     xml_source.text = event
+            #     xml_target = etree.SubElement(xml_exclude, "target")
+            #     xml_target.text = event_prime
+            # if "noResponseTo" in dcr and event in dcr["noResponseTo"] and event_prime in dcr["noResponseTo"][event]:
+            #     xml_exclude = etree.SubElement(root, "rules")
+            #     xml_type = etree.SubElement(xml_exclude, "type")
+            #     xml_type.text = "coresponse"
+            #     xml_source = etree.SubElement(xml_exclude, "source")
+            #     xml_source.text = event
+            #     xml_target = etree.SubElement(xml_exclude, "target")
+            #     xml_target.text = event_prime
 
+        # TODO: ask Morten how to export the marking with XML simple
         # if event in dcr['marking']['executed']:
         #     marking_exec = etree.SubElement(executed, "event")
         #     marking_exec.set("id", event)
@@ -85,7 +88,7 @@ def export_dcr_graph(dcr, root, parents_dict=None, replace_whitespace=''):
         #     marking_pend.set("id",event)
 
 
-def export_dcr_xml(dcr, output_file_name, dcr_title='DCR from pm4py', dcr_description=None,replace_whitespace=''):
+def export_dcr_xml(dcr, output_file_name, dcr_title='DCR from pm4py', dcr_description=None):
     '''
     Writes a DCR graph object to disk in the ``.xml`` file format (exported as ``.xml`` file).
     The file can be imported and visualised in the DCR solutions portal (https://dcrgraphs.net/)
@@ -117,21 +120,22 @@ def export_dcr_xml(dcr, output_file_name, dcr_title='DCR from pm4py', dcr_descri
     role_description = etree.SubElement(role, "description")
     role_description.text = "Dummy user"
 
-    if 'subprocesses' in dcr:
-        parents_dict = {}
-        for sp_name, sp_events in dcr['subprocesses'].items():
-            xml_event = etree.SubElement(root, "events")
-            xml_event_id = etree.SubElement(xml_event, "id")
-            xml_event_id.text = sp_name
-            xml_event_label = etree.SubElement(xml_event, "label")
-            xml_event_label.text = sp_name
-            xml_event_type = etree.SubElement(xml_event, "type")
-            xml_event_type.text = "subprocess"  # TODO: try "nesting" if subprocess doesn't work
-            for sp_event in sp_events:
-                parents_dict[sp_event] = sp_name
-        export_dcr_graph(dcr, root, parents_dict, replace_whitespace=replace_whitespace)
-    else:
-        export_dcr_graph(dcr, root, None, replace_whitespace=replace_whitespace)
+# TODO: include in the future when the DCR graph is updated with subprocesses
+    # if 'subprocesses' in dcr:
+    #     parents_dict = {}
+    #     for sp_name, sp_events in dcr['subprocesses'].items():
+    #         xml_event = etree.SubElement(root, "events")
+    #         xml_event_id = etree.SubElement(xml_event, "id")
+    #         xml_event_id.text = sp_name
+    #         xml_event_label = etree.SubElement(xml_event, "label")
+    #         xml_event_label.text = sp_name
+    #         xml_event_type = etree.SubElement(xml_event, "type")
+    #         xml_event_type.text = "subprocess"  # TODO: try "nesting" if subprocess doesn't work
+    #         for sp_event in sp_events:
+    #             parents_dict[sp_event] = sp_name
+    #     export_dcr_graph(dcr, root, parents_dict)
+    # else:
+    export_dcr_graph(dcr, root, None)
 
     tree = etree.ElementTree(root)
     tree.write(output_file_name, pretty_print=True)
