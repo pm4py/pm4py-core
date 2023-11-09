0ef449bac40df1a0d22d24b805a5ec1465067e10
Scones111 <jonas.lykke.kjeldsen@gmail.com>
2023-11-09 20:25:15 +0100

    removed our own rst files, and added the original configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,10 +16,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath('../../pm4py/algo/conformance/dcr'))
-sys.path.insert(0, os.path.abspath('../../pm4py/algo/conformance/alignments/dcr'))
-sys.path.insert(0, os.path.abspath('../../pm4py/algo/discovery/dcr_discover'))
-sys.path.insert(0, os.path.abspath('../../pm4py/objects/dcr'))
+
 # -- Project information -----------------------------------------------------
 
 project = 'pm4py'
@@ -52,7 +49,6 @@ napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_ivar = True
 autodoc_member_order = 'bysource'
-autodoc_inherit_docstring = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -76,8 +72,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patternss = ['tests', 'scripts', '.rst']
-
+exclude_patterns = ['tests', 'scripts']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
--- docs/source/install.rst
+++ docs/source/install.rst
@@ -1,18 +1,32 @@
 Installation
 ============
 
-This project is an extension of PM4Py that has already developed the code, in which one can install the this extension pm4py-dcr via a docker file
+pip
+---
+
+To use ``pm4py`` on any OS, install it using ``pip``:
+
+.. code-block:: console
+
+   (.venv) $ pip install pm4py
+
+``pmp4y`` uses the ``Graphviz`` library for rendering visualizations.
+Please install `Graphviz <https://graphviz.org/download/>`_.
+
+After installation, GraphViz is located in the ``program files`` directory.
+The ``bin\`` folder of the GraphViz directory needs to be added manually to the ``system path``.
+In order to do so, please follow `this instruction <https://stackoverflow.com/questions/44272416/how-to-add-a-folder-to-path-environment-variable-in-windows-10-with-screensho>`_.
 
 Docker
 ------
-To install pm4py-dcr via Docker, use:
+To install pm4py via Docker, use:
 
 .. code-block:: console
 
    $ docker pull pm4py/pm4py-core:latest
 
-To run pm4py-dcr via docker, use:
+To run pm4py via docker, use:
 
 .. code-block:: console
 
-   $ docker run -it pm4py/pm4py-dcr:latest bash
+   $ docker run -it pm4py/pm4py-core:latest bash
