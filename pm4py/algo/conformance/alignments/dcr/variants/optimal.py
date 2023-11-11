fc84cd4fd54cc9c49c2f9e4fd68d7407947415dd
Scones111 <jonas.lykke.kjeldsen@gmail.com>
2023-11-11 10:58:25 +0100

    added examples, fixed optimal to produce similar output to petri net alignment such that the notation is coherent

--- pm4py/algo/conformance/alignments/dcr/variants/optimal.py
+++ pm4py/algo/conformance/alignments/dcr/variants/optimal.py
@@ -110,6 +110,7 @@ class LogAlignment:
             aligned_traces = aligned_traces + trace_alignment.perform_alignment()
         return aligned_traces
 
+
 class TraceAlignment:
     """
     The TraceAlignment class provides a simplified interface to perform optimal alignment for DCR graphs,
@@ -195,7 +196,6 @@ class TraceAlignment:
         self.result[Outputs.BEST_WORST_COST.value] = fitness_bwc[1]
 
 
-
 class Parameters(Enum):
     """
     Enumeration that defines keys and constants for various parameters used in the alignment process.
@@ -235,6 +235,7 @@ class Outputs(Enum):
     GLOBAL_MIN = "global_min"
     ALIGN_FITNESS = 'fitness'
     BEST_WORST_COST = "bwc"
+
 
 class Performance:
     def __init__(self, alignment, graph_handler, trace_handler):
@@ -405,6 +406,7 @@ class DCRGraphHandler:
             return curr_graph
         return new_graph
 
+
 class Alignment:
     def __init__(self, graph_handler: DCRGraphHandler, trace_handler: TraceHandler, parameters: Optional[Dict] = None):
         """
@@ -484,7 +486,7 @@ class Alignment:
         if state_representation not in self.visited_states:
             self.visited_states.add(state_representation)
             new_moves = moves + [new_move]
-            heappush(self.open_set,(new_cost, new_graph, new_trace, str(new_graph), new_moves))
+            heappush(self.open_set, (new_cost, new_graph, new_trace, str(new_graph), new_moves))
 
     def get_new_state(self, curr_cost, curr_marking, curr_trace, event, move_type):
         """
@@ -585,7 +587,6 @@ class Alignment:
 
         return self.global_min
 
-
     def apply_trace(self, parameters=None):
         """
         Applies the alignment algorithm to a trace in order to find an optimal alignment
@@ -620,18 +621,19 @@ class Alignment:
 
         visited, closed, cost, self.final_alignment, final_cost = 0, 0, 0, None, float('inf')
         self.open_set.append(
-            (cost, deepcopy(self.graph_handler.graph.marking), self.trace_handler.trace, str(self.graph_handler.graph.marking), []))
+            (cost, deepcopy(self.graph_handler.graph.marking), self.trace_handler.trace,
+             str(self.graph_handler.graph.marking), []))
 
         # perform while loop to iterate through all states
         while self.open_set:
             current = heappop(self.open_set)
             visited += 1
             result = self.process_current_state(current)
-            #if the state has already been visited, and associated cost with the state is lower than skip
+            # if the state has already been visited, and associated cost with the state is lower than skip
             if self.skip_current(result) and result is not None:
                 continue
             curr_cost, curr_trace, state_repr, moves = result[0], result[2], result[3], result[4]
-            #if curr_cost is greater than final cost, no reason to explore this branch
+            # if curr_cost is greater than final cost, no reason to explore this branch
             if curr_cost > final_cost:
                 continue
             self.update_closed_and_visited_sets(curr_cost, state_repr)
@@ -647,9 +649,9 @@ class Alignment:
         return self.construct_results(visited, closed, final_cost)
 
     def skip_current(self, result):
-        #if state is visited, and cost is the same skip
+        # if state is visited, and cost is the same skip
         curr_cost, state_repr = result[0], result[3]
-        visitCost = self.closed_set.get(state_repr,float("inf"))
+        visitCost = self.closed_set.get(state_repr, float("inf"))
         return visitCost <= curr_cost and visitCost is not float("inf")
 
     def perform_moves(self, curr_cost, current, moves):
@@ -724,7 +726,8 @@ class Alignment:
             Outputs.GLOBAL_MIN.value: self.global_min,
         }
 
-def apply(trace_or_log: Union[pd.DataFrame,EventLog,Trace], graph: DcrGraph, parameters=None):
+
+def apply(trace_or_log: Union[pd.DataFrame, EventLog, Trace], graph: DcrGraph, parameters=None):
     """
     Applies an alignment operation on a given trace or log against a specified DCR graph.
 
@@ -776,11 +779,13 @@ def get_diagnostics_dataframe(log: EventLog, conf_result: List[Dict[str, Any]], 
     case_id_key = exec_utils.get_param_value(Parameters.CASE_ID_KEY, parameters,
                                              xes_constants.DEFAULT_TRACEID_KEY)
 
-    import pandas as pd
     diagn_stream = []
     for index in range(len(log)):
         case_id = log[index].attributes[case_id_key]
+
+        cost = conf_result[index][Outputs.COST.value]
         align_fitness = conf_result[index][Outputs.ALIGN_FITNESS.value]
-        diagn_stream.append({"case_id": case_id, "align_fitness": align_fitness})
+        is_fit = align_fitness == 1.0
+        diagn_stream.append({"case_id": case_id, "cost": cost, "fitness": align_fitness, "is_fit": is_fit})
 
     return pd.DataFrame(diagn_stream)
