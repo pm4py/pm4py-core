29d96f254e39087e3456d1e5aaae7cf93e40fc49
humam.kourani <humam.kourani@fit.fraunhofer.de>
2023-12-14 06:17:54 +0000

    POWL ISJournal updates

--- pm4py/algo/discovery/powl/inductive/variants/im_maximal.py
+++ pm4py/algo/discovery/powl/inductive/variants/im_maximal.py
@@ -1,16 +1,16 @@
 from typing import Optional, Tuple, List, Dict, Any
 
-from pm4py.algo.discovery.powl.inductive.variants.brute_force.factory import CutFactoryPOWLBruteForce
+from pm4py.algo.discovery.powl.inductive.variants.maximal.factory import CutFactoryPOWLMaximal
 from pm4py.algo.discovery.powl.inductive.variants.im_tree import IMBasePOWL, T
 from pm4py.algo.discovery.powl.inductive.variants.powl_discovery_varaints import POWLDiscoveryVariant
 from pm4py.objects.powl.obj import POWL
 
 
-class POWLInductiveMinerBruteForce(IMBasePOWL):
+class POWLInductiveMinerMaximalOrder(IMBasePOWL):
 
     def instance(self) -> POWLDiscoveryVariant:
-        return POWLDiscoveryVariant.BRUTE_FORCE
+        return POWLDiscoveryVariant.MAXIMAL
 
     def find_cut(self, obj: T, parameters: Optional[Dict[str, Any]] = None) -> Optional[Tuple[POWL, List[T]]]:
-        res = CutFactoryPOWLBruteForce.find_cut(obj, parameters=parameters)
+        res = CutFactoryPOWLMaximal.find_cut(obj, parameters=parameters)
         return res
