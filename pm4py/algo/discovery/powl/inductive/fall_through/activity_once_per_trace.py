c2278110acbbbf36e8fe5055e0b76f867e833da4
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2024-02-07 07:41:36 +0100

    fix(pm4py): bug fix POWL package

--- pm4py/algo/discovery/powl/inductive/fall_through/activity_once_per_trace.py
+++ pm4py/algo/discovery/powl/inductive/fall_through/activity_once_per_trace.py
@@ -1,6 +1,28 @@
+from collections import Counter
+from typing import Optional, Tuple, List, Any, Dict
+
+from pm4py.algo.discovery.inductive.dtypes.im_ds import IMDataStructureUVCL
 from pm4py.algo.discovery.inductive.fall_through.activity_once_per_trace import ActivityOncePerTraceUVCL
 from pm4py.algo.discovery.powl.inductive.fall_through.activity_concurrent import POWLActivityConcurrentUVCL
+from pm4py.objects.powl.obj import StrictPartialOrder
+from pm4py.objects.process_tree.obj import ProcessTree, Operator
 
 
 class POWLActivityOncePerTraceUVCL(ActivityOncePerTraceUVCL, POWLActivityConcurrentUVCL):
-    pass
+    @classmethod
+    def apply(cls, obj: IMDataStructureUVCL,
+              pool=None,
+              manager=None,
+              parameters: Optional[Dict[str, Any]] = None) -> Optional[Tuple[StrictPartialOrder, List[IMDataStructureUVCL]]]:
+
+        candidate = cls._get_candidate(obj, pool, manager, parameters)
+        if candidate is None:
+            return None
+        log = obj.data_structure
+        l_a = Counter()
+        l_other = Counter()
+        for t in log:
+            l_a.update({tuple(filter(lambda e: e == candidate, t)): log[t]})
+            l_other.update({tuple(filter(lambda e: e != candidate, t)): log[t]})
+        children = [IMDataStructureUVCL(l_a), IMDataStructureUVCL(l_other)]
+        return StrictPartialOrder(children), children
