95682c59b8486e389a32dbbfa1604d5c38505147
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2024-03-05 15:14:03 +0100

    feat(pm4py): taking DagreJS HTML in static file

--- pm4py/visualization/bpmn/variants/dagrejs.py
+++ pm4py/visualization/bpmn/variants/dagrejs.py
@@ -5,6 +5,7 @@ from enum import Enum
 from copy import copy
 import shutil
 import tempfile
+import importlib.resources
 
 
 class Parameters(Enum):
@@ -12,6 +13,13 @@ class Parameters(Enum):
     IFRAME_WIDTH = "iframe_width"
     IFRAME_HEIGHT = "iframe_height"
     LOCAL_JUPYTER_FILE_NAME = "local_jupyter_file_name"
+
+
+def get_html_file_contents():
+    file_path = 'pm4py/static/myfile.html'
+    resource_path = importlib.resources.path("pm4py.visualization.bpmn.util", "dagrejs_base.html")
+    with open(resource_path, 'r') as html_file:
+        return html_file.read()
 
 
 def apply(bpmn_graph: BPMN, parameters: Optional[Dict[Any, Any]] = None) -> str:
@@ -39,287 +47,13 @@ def apply(bpmn_graph: BPMN, parameters: Optional[Dict[Any, Any]] = None) -> str:
     from pm4py.objects.bpmn.exporter.variants import etree as bpmn_xml_exporter
     export_parameters = copy(parameters)
     xml_stri = bpmn_xml_exporter.get_xml_string(bpmn_graph, parameters=export_parameters)
+    xml_stri = xml_stri.decode(encoding).replace("\t", "").replace("\r", "").replace("\n", "")
 
     F = tempfile.NamedTemporaryFile(suffix=".html")
     F.close()
 
     F = open(F.name, "w", encoding=encoding)
-    F.write("""
-<html>
-<head>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.16.0/d3.min.js" charset="utf-8"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/dagre-d3/0.6.1/dagre-d3.min.js"></script>
-<script type="text/javascript" src="https://unpkg.com/bpmn-js@17.0.2/dist/bpmn-navigated-viewer.production.min.js"></script>
-</head>
-<body>
-	<div style="visibility: hidden; position: absolute;">
-	<svg width="90%" height="90%">
-	  <g/>
-	</svg>
-	</div>
-	<div id="canvas" style="height: 90%; width: 90%; padding: 0; margin: 0; position: absolute"></div>
-    <div style="visibility: hidden">
-      <div style="position: fixed; top: 0px; left: 0px">
-        <svg id="internalSvg" style="width: 0%; height: 0%"></svg>
-      </div>
-      <div
-        id="internalCanvas"
-        style="
-          height: 0%;
-          width: 0%;
-          padding: 0;
-          margin: 0;
-          top: 0px;
-          left: 0px;
-          position: fixed;
-        "
-      ></div>
-    </div>
-<script type="text/javascript">
-class CustomWaypoint {
-    dictio;
-    $name;
-    $model;
-    $type;
-    $attrs;
-    $parent;
-    $descriptor;
-    x;
-    y;
-
-    constructor() {
-        this.dictio = {};
-    }
-
-    get(varname) {
-        return this.dictio[varname];
-    }
-
-    set(varname, varvalue) {
-        this.dictio[varname] = varvalue;
-    }
-}
-
-function renderGraph(
-    iterativelyReachedNodes,
-    nodes,
-    edgesDict,
-    nodesep,
-    edgesep,
-    ranksep,
-    targetDivDagre,
-    desideredWidth = null,
-    desideredHeight = null
-) {
-    var g = new dagreD3.graphlib.Graph().setGraph({});
-
-    for (let n of iterativelyReachedNodes) {
-        if (!n.$type.toLowerCase().endsWith("flow")) {
-            let name = "" + n.name;
-            let isProperName = true;
-            if (name.length == 0) {
-                name = n.id;
-                isProperName = false;
-            }
-            if (name == "start" || name == "end") {
-                isProperName = false;
-            }
-            if (isProperName && desideredWidth != null) {
-                g.setNode(n.id, {
-                    label: n.name.replaceAll(" ", "\\n"),
-                    width: desideredWidth,
-                    height: desideredHeight,
-                });
-            } else if (desideredWidth != null) {
-                g.setNode(n.id, {
-                    label: n.name.replaceAll(" ", "\\n"),
-                    width: Math.min(desideredWidth, desideredHeight) * 0.28,
-                    height: Math.min(desideredWidth, desideredHeight) * 0.28,
-                });
-            } else {
-                g.setNode(n.id, {
-                    label: n.name.replaceAll(" ", "\\n"),
-                });
-            }
-        }
-    }
-
-    for (let n of nodes) {
-        if (n.$type.toLowerCase().endsWith("flow")) {
-            let source = n.sourceRef.id;
-            let target = n.targetRef.id;
-            edgesDict[source + "@" + target] = n.id;
-            g.setEdge(source, target, {
-                label: ""
-            });
-        }
-    }
-
-    g.graph().rankDir = "LR";
-    g.graph().nodesep = nodesep;
-    g.graph().edgesep = edgesep;
-    g.graph().ranksep = ranksep;
-
-    let render = new dagreD3.render();
-    let svg = d3.select("#" + targetDivDagre);
-    let inner = svg.append("g");
-    render(inner, g);
-
-    return g;
-}
-
-async function bpmnLayoutWithDagre(
-    xmlString
-) {
-    let targetDivFirstBpmn = "internalCanvas";
-    let targetDivDagre = "internalSvg";
-    let nodesep = 30;
-    let edgesep = 30;
-    let ranksep = 85;
-
-    let bpmnViewer = new BpmnJS({
-        container: "#" + targetDivFirstBpmn,
-    });
-
-    await bpmnViewer.importXML(xmlString);
-
-    let nodes = bpmnViewer._definitions.rootElements[0].flowElements;
-    let graphical = bpmnViewer._definitions.diagrams[0].plane.planeElement;
-    let graphicalDict = {};
-    let edgesDict = {};
-    let i = 0;
-    while (i < graphical.length) {
-        graphicalDict[graphical[i].bpmnElement.id] = i;
-        i++;
-    }
-
-    let toVisit = [];
-    let iterativelyReachedNodes = [];
-
-    for (let n of nodes) {
-        if (n.$type.toLowerCase().endsWith("startevent")) {
-            toVisit.push(n);
-            break;
-        }
-    }
-
-    while (toVisit.length > 0) {
-        let el = toVisit.pop();
-        if (!iterativelyReachedNodes.includes(el)) {
-            iterativelyReachedNodes.push(el);
-        }
-        if (el.outgoing != null) {
-            for (let out of el.outgoing) {
-                if (!iterativelyReachedNodes.includes(out.targetRef)) {
-                    toVisit.push(out.targetRef);
-                }
-            }
-        }
-    }
-
-    let g = renderGraph(
-        iterativelyReachedNodes,
-        nodes,
-        edgesDict,
-        nodesep,
-        edgesep,
-        ranksep,
-        targetDivDagre
-    );
-
-    let desideredWidth = 0;
-    let desideredHeight = 0;
-
-    for (let nodeId in g._nodes) {
-        let node = g._nodes[nodeId];
-        let elemStr = node.elem.innerHTML;
-        let width = parseInt(elemStr.split('width="')[1].split('"')[0]);
-        let height = parseInt(elemStr.split('height="')[1].split('"')[0]);
-        desideredWidth = Math.max(desideredWidth, width);
-        desideredHeight = Math.max(desideredHeight, height);
-    }
-
-    g = renderGraph(
-        iterativelyReachedNodes,
-        nodes,
-        edgesDict,
-        nodesep,
-        edgesep,
-        ranksep,
-        targetDivDagre,
-        desideredWidth * 1.7,
-        desideredHeight * 0.87
-    );
-
-    for (let nodeId in g._nodes) {
-        let node = g._nodes[nodeId];
-        let elemStr = node.elem.innerHTML;
-        let width = parseInt(elemStr.split('width="')[1].split('"')[0]);
-        let height = parseInt(elemStr.split('height="')[1].split('"')[0]);
-        graphical[graphicalDict[nodeId]].bounds.x = node.x - width / 2.0;
-        graphical[graphicalDict[nodeId]].bounds.y = node.y - height / 2.0;
-        graphical[graphicalDict[nodeId]].bounds.height = height;
-        graphical[graphicalDict[nodeId]].bounds.width = width;
-    }
-
-    for (let edgeId in g._edgeLabels) {
-        let graphEdgeObj = g._edgeObjs[edgeId];
-        graphEdgeObj = graphEdgeObj.v + "@" + graphEdgeObj.w;
-        let graphEdge = g._edgeLabels[edgeId];
-        let edge = g._edgeLabels[edgeId];
-        let graphicalElement = graphical[graphicalDict[edgesDict[graphEdgeObj]]];
-        let referenceWaypoint = graphicalElement.waypoint[0];
-        graphicalElement.waypoint = [];
-        for (let p of edge.points) {
-            let waypoint = new CustomWaypoint();
-            waypoint.$type = referenceWaypoint.$type;
-            waypoint.x = p.x;
-            waypoint.y = p.y;
-            waypoint.$parent = referenceWaypoint.$parent;
-            waypoint.$attrs = referenceWaypoint.$attrs;
-            waypoint.$descriptor = referenceWaypoint.$descriptor;
-            waypoint.$model = referenceWaypoint.$model;
-            waypoint.set("x", p.x);
-            waypoint.set("y", p.y);
-            graphicalElement.waypoint.push(waypoint);
-        }
-    }
-
-    let xmlContent = await bpmnViewer.saveXML();
-
-    return xmlContent.xml;
-}
-
-async function renderBpmn(xmlString) {
-    var bpmnViewer = new BpmnJS({
-        container: '#canvas'
-    });
-
-    await bpmnViewer.importXML(xmlString);
-
-    // access viewer components
-    var canvas = bpmnViewer.get('canvas');
-
-    canvas.zoom('fit-viewport');
-}
-
-async function layoutAndRender(xmlString) {
-        bpmnLayoutWithDagre(xmlString).then(function(data) {
-            renderBpmn(data);
-        });
-    }
-</script>
-	<script type="text/javascript">
-	let xmlString = '""")
-    F.write(xml_stri.decode(encoding).replace("\t", "").replace("\r", "").replace("\n", ""))
-    F.write("';\n")
-    F.write("""
-	layoutAndRender(xmlString);
-	</script>
-</body>
-</html>
-    """)
+    F.write(get_html_file_contents().replace("REPLACE", xml_stri))
     F.close()
 
     return F.name
